1b0beafff9b8bd5f6bc70dd7c22548f9b719293c
Ox Cart <ox.to.a.cart@gmail.com>
2020-09-30 12:35:09 -0500

    Factored mobilesdk into a submodule and enabled dynamic dnsgrab address

--- app/src/main/java/org/getlantern/lantern/vpn/Provider.java
+++ app/src/main/java/org/getlantern/lantern/vpn/Provider.java
@@ -4,7 +4,7 @@ import android.net.VpnService;
 
 // A provider provides the implementation of VPN internals.
 interface Provider {
-  void run(final VpnService vpnService, final VpnService.Builder builder, final String socksAddr) throws Exception;
+  void run(final VpnService vpnService, final VpnService.Builder builder, final String socksAddr, final String dnsGrabAddr) throws Exception;
 
   void stop() throws Exception;
 }
