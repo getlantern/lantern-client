52b8a22a2c6164915d83d2e5b262f94be8e6d0fa
atavism <todd@lana.io>
2020-10-17 11:43:16 -0700

    enable view binding and apply updates to top bar fragment

--- app/src/main/java/org/getlantern/lantern/fragment/TopBarFragment.kt
+++ app/src/main/java/org/getlantern/lantern/fragment/TopBarFragment.kt
@@ -1,26 +1,49 @@
 package org.getlantern.lantern.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-//import androidx.databinding.DataBindingUtil
+import android.widget.ImageView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.core.view.GravityCompat
 
 import org.getlantern.lantern.R
-//import org.getlantern.lantern.databinding.FragmentTopBarBinding
+import org.getlantern.lantern.fragment.TopBarFragment as TopBarFragment
 
+private const val TAG = "TopBarFragment"
 
-class TopBarFragment:Fragment()
-{
+class TopBarFragment : Fragment() {
+
+  companion object {
+    @JvmStatic
+    fun newInstance(): TopBarFragment {
+      return TopBarFragment()
+    }
+  }
 
   //private lateinit var binding: FragmentTopBarBinding
+  private lateinit var menuIcon: ImageView
 
+  // Called when the fragment instantiates its UI view
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.top_bar, container, false)
-    /*binding = DataBindingUtil.inflate(
-      inflater, R.layout.top_bar, container, false)
-    return binding.root*/
+    val view = inflater.inflate(R.layout.top_bar, container, false)
+    
+    menuIcon = view.findViewById(R.id.menuIcon)
+
+    return view
   }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    menuIcon.setOnClickListener {
+      Log.d(TAG, "Menu icon clicked")
+      var drawerLayout = requireActivity().findViewById(R.id.drawerLayout)
+      drawerLayout.openDrawer(GravityCompat.START)
+    }
+  }
+
 }
