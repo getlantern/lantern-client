28e04426764ce9e539b8906d2ed56e52448d7c48
Ox Cart <ox.to.a.cart@gmail.com>
2021-01-13 08:40:09 -0600

    Pass correct timezone to server, freeze bandwidth at point in time, use short format for date and time

--- app/src/main/java/org/getlantern/lantern/model/Bandwidth.kt
+++ app/src/main/java/org/getlantern/lantern/model/Bandwidth.kt
@@ -1,5 +1,8 @@
 package org.getlantern.lantern.model
 
+import java.text.DateFormat
+import java.util.*
+
 class Bandwidth(
         val percent: Long, // [0, 100]
         val remaining: Long, // in MB
@@ -9,9 +12,21 @@ class Bandwidth(
 
     val used: Long
         get() = allowed - remaining
+    val expiresAtString: String
+
+    init {
+        // the ttlSeconds from Go is relative to the system timezone, adjust accordingly for correct
+        // display
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.SECOND, ttlSeconds.toInt())
+        calendar[Calendar.SECOND] = 0
+
+        val format = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+        expiresAtString = format.format(calendar.time);
+    }
 
     override fun toString(): String {
-        return String.format("Bandwidth update: %d/%d (%d). TTL: %d (seconds)",
-                remaining, allowed, percent, ttlSeconds)
+        return String.format("Bandwidth update: %d/%d (%d). TTL: %d (seconds) Expires At: %s",
+                remaining, allowed, percent, ttlSeconds, expiresAtString)
     }
 }
