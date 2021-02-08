17ee185d7b7c5e07b7e035892a86e3270e344c59
Ox Cart <ox.to.a.cart@gmail.com>
2021-02-08 07:55:03 -0600

    Playing around with signal

---
+++ app/src/test/java/org/getlantern/SignalTest.kt
@@ -0,0 +1,39 @@
+package org.getlantern
+
+import okhttp3.OkHttpClient
+import org.getlantern.mobilesdk.model.LoConf
+import org.getlantern.mobilesdk.model.LoConfCallback
+import org.getlantern.mobilesdk.util.HttpClient
+import org.junit.Assert
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SignalTest {
+    @Test
+    fun testFetchLoconf() {
+        val httpClient = OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
+        val client = HttpClient(httpClient)
+
+        val success = AtomicBoolean()
+        val _success = success as java.lang.Object
+        val cb = LoConfCallback { it ->
+            success.set(true)
+            synchronized(success) { _success.notifyAll() }
+        }
+
+        LoConf.fetch(client, "https://raw.githubusercontent.com/getlantern/loconf/master/messages.json", cb)
+        synchronized(success) {
+            try {
+                _success.wait(8000)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+        Assert.assertTrue(success.get())
+    }
+}
