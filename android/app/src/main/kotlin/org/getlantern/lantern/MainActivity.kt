8beec5b509670194e61f7d0e51ffd198af3cf2e6
Markhor Sarwar <markhor@getlantern.org>
2021-03-10 06:46:12 +0700

    Split the Model class into multiple models, and migrate from im-memory data source to using observable model

--- android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
@@ -1,88 +1,38 @@
 package org.getlantern.lantern
 
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.EventChannel.EventSink
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.StandardMethodCodec
 import io.lantern.isimud.model.Messaging
-import io.lantern.isimud.model.Model
-import io.lantern.isimud.model.ProtobufMessageCodec
-import kotlinx.collections.immutable.persistentListOf
+import io.lantern.isimud.model.MessagingModel
+import io.lantern.isimud.model.VpnModel
 
 
-class MainActivity: FlutterActivity(), EventChannel.StreamHandler {
+class MainActivity : FlutterActivity() {
+
+    private lateinit var messagingModel: MessagingModel
+    private lateinit var vpnModel: VpnModel
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-         // Prepare channels
-        EventChannel(getFlutterEngine()?.dartExecutor, updatesChannel, StandardMethodCodec(ProtobufMessageCodec())).setStreamHandler(this)
+        messagingModel = MessagingModel(flutterEngine)
+        vpnModel = VpnModel(flutterEngine)
 
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, methodChannel, StandardMethodCodec(ProtobufMessageCodec())).setMethodCallHandler {
-            // Note: this method is invoked on the main thread.
-            call, result ->
-            when(call.method) {
-                "get" -> {
-                    val path = call.arguments<String>()
-                    result.success(model.get(path))
-                }
-                "getRange" -> {
-                    val path = call.argument<String>("path")
-                    val start = call.argument<Int>("start")
-                    val count = call.argument<Int>("count")
-                    result.success(model.getRange<Any>(path!!, start!!, count!!))
-                }
-                "getRangeDetails" -> {
-                    val path = call.argument<String>("path")
-                    val detailsPrefix = call.argument<String>("detailsPrefix")
-                    val start = call.argument<Int>("start")
-                    val count = call.argument<Int>("count")
-                    result.success(model.getRangeDetails<Any>(path!!, detailsPrefix!!, start!!, count!!))
-                }
-                "put" -> {
-                    val path = call.argument<String>("path")
-                    val value = call.argument<Any>("value")
-                    model.put(path!!, value!!)
-                    result.success(null)
-                }
-                else -> result.notImplemented()
-            }
-        }
-    }
-
-    override fun onListen(arguments: Any?, events: EventSink?) {
-        var args = arguments as Map<String, String>
-        val subscriberID = args["subscriberID"] as Int
-        val path = args["path"] as String
-        val detailsPrefix = args["detailsPrefix"]
-        if (detailsPrefix != null) {
-            model.subscribeDetails(subscriberID, path, detailsPrefix as String) { path: String, newValue: List<Any?> ->
-                events?.success(mapOf("subscriberID" to subscriberID, "newValue" to newValue))
-            }
-        } else {
-            model.subscribe(subscriberID, path) { path: String, newValue: Any? ->
-                events?.success(mapOf("subscriberID" to subscriberID, "newValue" to newValue))
-            }
-        }
-    }
-
-    override fun onCancel(arguments: Any?) {
-        if (arguments == null) {
-            return;
-        }
-        var args = arguments as Map<String, String>
-        val subscriberID = args["subscriberID"] as Int
-        val path = args["path"] as String
-        model.unsubscribe(subscriberID, path)
-    }
-
-    companion object {
-        private val model = Model()
-
-        private const val methodChannel = "methodChannel"
-        private const val updatesChannel = "updatesChannel"
+//        val handler = Handler(Looper.getMainLooper())
+//        handler.postDelayed({
+//            messagingModel.observableModel.mutate { tx ->
+//                tx.put(
+//                    "/contact/0",
+//                    Messaging.Contact.newBuilder()
+//                        .setUserID("0")
+//                        .setName("This is a new name")
+//                        .build()
+//                )
+//            }
+//        }, 3000)
     }
 
 //    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
