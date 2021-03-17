eea7f8e5b765d11bc5aa6c2346e64b15dd6a469e
Markhor Sarwar <markhor@getlantern.org>
2021-03-17 16:36:31 +0700

    Make flutterEngine in Model optional, and make SessionManager depends on VpnModel instead of directly have access to the observable model

--- android/app/src/main/kotlin/io/lantern/isimud/model/VpnModel.kt
+++ android/app/src/main/kotlin/io/lantern/isimud/model/VpnModel.kt
@@ -11,8 +11,8 @@ import java.io.File
  * Created by DoNguyen on 10/3/21.
  */
 class VpnModel(
-        flutterEngine: FlutterEngine,
-        private var switchLanternHandler: ((vpnOn: Boolean) -> Unit)? = null,
+    flutterEngine: FlutterEngine? = null,
+    private var switchLanternHandler: ((vpnOn: Boolean) -> Unit)? = null,
 ) : Model("vpn", flutterEngine, vpnObservableModel) {
 
     companion object {
@@ -21,12 +21,12 @@ class VpnModel(
         const val PATH_BANDWIDTH = "/bandwidth"
 
         val vpnObservableModel = ObservableModel.build(
-                ctx = LanternApp.getAppContext(),
-                filePath = File(
-                        File(LanternApp.getAppContext().filesDir, ".lantern"),
-                        "vpn_db"
-                ).absolutePath,
-                password = "password" // TODO: make the password random and save it as an encrypted preference
+            ctx = LanternApp.getAppContext(),
+            filePath = File(
+                File(LanternApp.getAppContext().filesDir, ".lantern"),
+                "vpn_db"
+            ).absolutePath,
+            password = "password" // TODO: make the password random and save it as an encrypted preference
         )
 
         init {
@@ -46,23 +46,39 @@ class VpnModel(
         }
     }
 
-    private fun switchLantern(value: Boolean) {
-        switchLanternHandler?.invoke(value)
-    }
-
-    fun setVpnOn(vpnOn: Boolean) {
-        val vpnStatus = if (vpnOn) "connected" else "disconnected"
-        observableModel.mutate { tx ->
-            tx.put(PATH_VPN_STATUS, vpnStatus)
-        }
+    fun isConnectedToVpn(): Boolean {
+        val vpnStatus = vpnStatus()
+        return vpnStatus == "connected" || vpnStatus == "disconnecting"
     }
 
     private fun vpnStatus(): String {
         return observableModel.get(PATH_VPN_STATUS) ?: ""
     }
 
-    fun isConnectedToVpn(): Boolean {
-        val vpnStatus = vpnStatus()
-        return vpnStatus == "connected" || vpnStatus == "disconnecting"
+    private fun switchLantern(value: Boolean) {
+        switchLanternHandler?.invoke(value)
+    }
+
+    fun setVpnOn(vpnOn: Boolean) {
+        val vpnStatus = if (vpnOn) "connected" else "disconnected"
+        saveVpnStatus(vpnStatus)
+    }
+
+    fun saveVpnStatus(vpnStatus: String) {
+        observableModel.mutate { tx ->
+            tx.put(PATH_VPN_STATUS, vpnStatus)
+        }
+    }
+
+    fun saveServerInfo(serverInfo: Vpn.ServerInfo) {
+        observableModel.mutate { tx ->
+            tx.put(PATH_SERVER_INFO, serverInfo)
+        }
+    }
+
+    fun saveBandwidth(bandwidth: Vpn.Bandwidth) {
+        observableModel.mutate { tx ->
+            tx.put(PATH_BANDWIDTH, bandwidth)
+        }
     }
 }
