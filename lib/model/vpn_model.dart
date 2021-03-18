486e996806c5acddb20ff23b722f3630f96a799d
Ox Cart <ox.to.a.cart@gmail.com>
2021-03-17 20:34:21 -0500

    Factored vpn.dart into smaller functions, removed currently unneeded messaging related stuff from UI

--- lib/model/vpn_model.dart
+++ lib/model/vpn_model.dart
@@ -1,15 +1,28 @@
 import 'package:lantern/model/model.dart';
+import 'package:lantern/model/protos/vpn.pb.dart';
+
+import '../package_store.dart';
 
 class VpnModel extends Model {
   VpnModel() : super("vpn");
-
-  static const PATH_VPN_STATUS = "/vpn_status";
-  static const PATH_SERVER_INFO = "/server_info";
-  static const PATH_BANDWIDTH = "/bandwidth";
 
   Future<void> switchVPN<T>(bool on) async {
     methodChannel.invokeMethod('switchVPN', <String, dynamic>{
       "on": on,
     });
+  }
+
+  ValueListenableBuilder<String> vpnStatus(ValueWidgetBuilder<String> builder) {
+    return subscribedBuilder<String>("/vpn_status", builder: builder);
+  }
+
+  ValueListenableBuilder<ServerInfo> serverInfo(
+      ValueWidgetBuilder<ServerInfo> builder) {
+    return subscribedBuilder<ServerInfo>("/server_info", builder: builder);
+  }
+
+  ValueListenableBuilder<Bandwidth> bandwidth(
+      ValueWidgetBuilder<Bandwidth> builder) {
+    return subscribedBuilder<Bandwidth>("/bandwidth", builder: builder);
   }
 }
