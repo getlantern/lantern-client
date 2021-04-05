d8102b4629c744d0ecb3e0ff353aa9edcc978e48
Markhor Sarwar <markhor@getlantern.org>
2021-04-05 14:24:02 +0700

    Implement native UI for "Language" page

--- android/app/src/main/java/org/getlantern/mobilesdk/activity/LanguageActivity.kt
+++ android/app/src/main/java/org/getlantern/mobilesdk/activity/LanguageActivity.kt
@@ -1,10 +1,15 @@
 package org.getlantern.mobilesdk.activity
 
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.getlantern.lantern.LanternApp
+import org.getlantern.lantern.R
 import org.getlantern.lantern.activity.Launcher
 import org.getlantern.lantern.databinding.LanguagesBinding
 import org.getlantern.mobilesdk.Logger
@@ -15,7 +20,6 @@ import java.util.*
 class LanguageActivity : FragmentActivity() {
     private lateinit var languages: ArrayList<String>
     private lateinit var localeMap: MutableMap<String, Locale>
-    private lateinit var adapter: LangAdapter
 
     private lateinit var binding: LanguagesBinding
 
@@ -32,10 +36,32 @@ class LanguageActivity : FragmentActivity() {
             localeMap[localeInfo.label] = localeInfo.locale
         }
         languages.sort()
-        adapter = LangAdapter(this, languages, localeMap)
-        binding.list.adapter = adapter
-        binding.list.choiceMode = ListView.CHOICE_MODE_SINGLE
-        binding.list.setOnItemClickListener { _, _, _, id  -> setLocale(languages[id.toInt()]) }
+        binding.list.apply {
+            this.adapter = LanguageAdapter().apply {
+                callback = object: LanguageAdapter.Callback {
+                    override fun onClick(view: View, pos: Int, item: LanguageAdapterModel) {
+                        setLocale(item.lang)
+                    }
+                }
+                val current = LanternApp.getSession().language
+                lang = languages.map {
+                    LanguageAdapterModel(it, current == localeMap[it].toString())
+                }
+            }
+            layoutManager = LinearLayoutManager(this@LanguageActivity)
+            setHasFixedSize(true)
+            addItemDecoration(object: RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    super.getItemOffsets(outRect, view, parent, state)
+                    outRect.top = resources.getDimensionPixelSize(R.dimen.space_small)
+                }
+            })
+        }
     }
 
     fun setLocale(lang: String) {
