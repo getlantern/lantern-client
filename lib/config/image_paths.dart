21b0f3a8a0713b3e042f6385401a4d3b7d04cd99
Markhor Sarwar <markhor@getlantern.org>
2021-04-21 16:55:23 +0700

    Replace double quote with single quote for Dart code

--- lib/config/image_paths.dart
+++ lib/config/image_paths.dart
@@ -1,29 +1,29 @@
 class ImagePaths {
-  static const account_icon = "assets/images/account_icon.svg";
-  static const key_icon = "assets/images/key_icon.svg";
-  static const crown_icon = "assets/images/crown_icon.svg";
-  static const info_icon = "assets/images/info_icon.svg";
-  static const keyboard_arrow_right_icon = "assets/images/keyboard_arrow_right_icon.svg";
-  static const location_on_icon = "assets/images/location_on_icon.svg";
-  static const exchange_icon = "assets/images/exchange_icon.svg";
+  static const account_icon = 'assets/images/account_icon.svg';
+  static const key_icon = 'assets/images/key_icon.svg';
+  static const crown_icon = 'assets/images/crown_icon.svg';
+  static const info_icon = 'assets/images/info_icon.svg';
+  static const keyboard_arrow_right_icon = 'assets/images/keyboard_arrow_right_icon.svg';
+  static const location_on_icon = 'assets/images/location_on_icon.svg';
+  static const exchange_icon = 'assets/images/exchange_icon.svg';
 
-  static const devices_icon = "assets/images/devices_icon.svg";
-  static const star_icon = "assets/images/star_icon.svg";
-  static const desktop_icon = "assets/images/desktop_icon.svg";
-  static const yinbi_icon = "assets/images/yinbi_icon.svg";
-  static const settings_icon = "assets/images/settings_icon.svg";
+  static const devices_icon = 'assets/images/devices_icon.svg';
+  static const star_icon = 'assets/images/star_icon.svg';
+  static const desktop_icon = 'assets/images/desktop_icon.svg';
+  static const yinbi_icon = 'assets/images/yinbi_icon.svg';
+  static const settings_icon = 'assets/images/settings_icon.svg';
 
-  static const alert_icon = "assets/images/alert_icon.svg";
-  static const translate_icon = "assets/images/translate_icon.svg";
+  static const alert_icon = 'assets/images/alert_icon.svg';
+  static const translate_icon = 'assets/images/translate_icon.svg';
 
-  static const free_logo = "assets/images/free_logo.svg";
-  static const pro_logo = "assets/images/pro_logo.svg";
+  static const free_logo = 'assets/images/free_logo.svg';
+  static const pro_logo = 'assets/images/pro_logo.svg';
 
-  static const paxful_logo = "assets/images/paxful_icon.svg";
+  static const paxful_logo = 'assets/images/paxful_icon.svg';
 
-  static const bitcoin_icon = "assets/images/bitcoin_icon.svg";
-  static const tether_icon = "assets/images/tether_icon.svg";
-  static const foreign_icon = "assets/images/foreign_icon.svg";
-  static const dropdown_icon = "assets/images/dropdown_icon.svg";
-  static const open_in_new_icon = "assets/images/open_in_new_icon.svg";
+  static const bitcoin_icon = 'assets/images/bitcoin_icon.svg';
+  static const tether_icon = 'assets/images/tether_icon.svg';
+  static const foreign_icon = 'assets/images/foreign_icon.svg';
+  static const dropdown_icon = 'assets/images/dropdown_icon.svg';
+  static const open_in_new_icon = 'assets/images/open_in_new_icon.svg';
 }
