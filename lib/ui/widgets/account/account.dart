02ecfaecd2e4c1b8afcb5a2a670c936a748df487
Ox Cart <ox.to.a.cart@gmail.com>
2021-04-29 09:59:28 -0500

    Fixed hardware back button handling

--- lib/ui/widgets/account/account.dart
+++ lib/ui/widgets/account/account.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:lantern/package_store.dart';
+import 'package:lantern/ui/widgets/back_button_respecting_navigator.dart';
 
 import 'account_menu.dart';
 import 'settings.dart';
@@ -7,7 +8,8 @@ import 'settings.dart';
 class AccountTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Navigator(onGenerateRoute: (RouteSettings settings) {
+    return BackButtonRespectingNavigator(
+        onGenerateRoute: (RouteSettings settings) {
       WidgetBuilder builder;
       switch (settings.name) {
         case '/':
