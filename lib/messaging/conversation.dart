8baca281111f9952ed8f0dc1257d50f4d3d8ea41
Kallirroi <kallirroi.retzepi@gmail.com>
2021-05-05 13:31:30 -0400

    extract conversation.dart widgets into their own methods

--- lib/messaging/conversation.dart
+++ lib/messaging/conversation.dart
@@ -168,6 +168,7 @@ class _ConversationState extends State<Conversation> {
   Widget build(BuildContext context) {
     model = context.watch<MessagingModel>();
     return BaseScreen(
+      // Conversation title (contact name)
       title: widget._contact.displayName.isEmpty
           ? widget._contact.contactId.id
           : widget._contact.displayName,
@@ -188,162 +189,174 @@ class _ConversationState extends State<Conversation> {
         onPanEnd: (details) async {
           await _finishRecording();
         },
+        // Conversation body
         child: Stack(children: [
           Column(children: [
+            // Conversation subtitle
             Padding(
                 padding: const EdgeInsets.only(top: 8, bottom: 8),
-                child: model.singleContact(
-                    context,
-                    widget._contact,
-                    (context, contact, child) => contact
-                                .messagesDisappearAfterSeconds >
-                            0
-                        ? Text(
-                            'New messages disappear after ${contact.messagesDisappearAfterSeconds.humanizeSeconds(longForm: true)}')
-                        : Container())),
+                child: buildMessagesLifeExpectancy()),
+            // Message bubbles
             Expanded(
-              child: model.contactMessages(widget._contact, builder: (context,
-                  Iterable<PathAndValue<StoredMessage>> messageRecords,
-                  Widget? child) {
-                return ListView.builder(
-                  reverse: true,
-                  itemCount: messageRecords.length,
-                  itemBuilder: (context, index) {
-                    return MessageBubble(
-                        messageRecords.elementAt(index),
-                        index >= messageRecords.length - 1
-                            ? null
-                            : messageRecords.elementAt(index + 1).value,
-                        index == 0
-                            ? null
-                            : messageRecords.elementAt(index - 1).value);
-                  },
-                );
-              }),
+              child: buildMessageBubbles(),
             ),
             const Padding(
               padding: EdgeInsets.only(top: 8),
               child: Divider(height: 3),
             ),
+            // Message bar
             Padding(
               padding: const EdgeInsets.all(8),
-              child: Row(children: [
-                // TODO(kallirroi): Refine the UI/UX
-                Expanded(
-                  child: TextFormField(
-                    textInputAction: TextInputAction.send,
-                    onFieldSubmitted: _send,
-                    controller: _newMessage,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          icon: const Icon(Icons.send),
-                          onPressed: () {
-                            var text = _newMessage.value.text;
-                            if (text.isEmpty) {
-                              return;
-                            }
-                            _send(text);
-                          }),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: 'Message'.i18n,
-                    ),
-                  ),
-                ),
-                // TODO(kallirroi): Refine the UI/UX
-                GestureDetector(
-                  onTap: () => _selectFilesToShare(),
-                  child: const Icon(Icons.image),
-                ),
-                // TODO(kallirroi): Refine the UI/UX
-                GestureDetector(
-                  onTapDown: (details) {
-                    _startRecording();
-                  },
-                  onTapUp: (details) async {
-                    await _finishRecording();
-                  },
-                  child: const Icon(Icons.mic),
-                ),
-              ]),
+              child: buildMessageBar(),
             ),
           ]),
-          // TODO(kallirroi): Refine the UI/UX
-          if (_recording)
-            Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-              Flexible(
-                child: ColoredBox(
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 16, bottom: 17),
-                        child: Icon(Icons.circle, color: Colors.red),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16, bottom: 22),
-                        child: StreamBuilder<int>(
-                          stream: _stopWatchTimer.rawTime,
-                          initialData:
-                              _stopWatchTimer.rawTime.valueWrapper?.value,
-                          builder: (context, snap) {
-                            final value = snap.data;
-                            final displayTime = StopWatchTimer.getDisplayTime(
-                                value ?? 0,
-                                minute: true,
-                                second: true,
-                                hours: false,
-                                milliSecond: false);
-                            return Text(displayTime,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold));
-                          },
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 63,
-                          child: Padding(
-                              padding: const EdgeInsets.only(right: 24),
-                              child: Text(
-                                  _willCancelRecording
-                                      ? 'will cancel'.i18n
-                                      : '< ' + 'swipe to cancel'.i18n,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold))),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTapUp: (details) async {
-                          await _finishRecording();
-                        },
-                        child: Transform.scale(
-                          scale: 2,
-                          alignment: Alignment.bottomRight,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(38)),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(
-                                  left: 15, top: 15, right: 4, bottom: 4),
-                              child: Icon(Icons.mic_none),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ]),
+          // Voice recorder
+          if (_recording) buildVoiceRecorder(),
         ]),
       ),
     );
+  }
+
+  Widget buildMessagesLifeExpectancy() {
+    return model.singleContact(
+        context,
+        widget._contact,
+        (context, contact, child) => contact.messagesDisappearAfterSeconds > 0
+            ? Text(
+                'New messages disappear after ${contact.messagesDisappearAfterSeconds.humanizeSeconds(longForm: true)}')
+            : Container());
+  }
+
+  Widget buildMessageBubbles() {
+    return model.contactMessages(widget._contact, builder: (context,
+        Iterable<PathAndValue<StoredMessage>> messageRecords, Widget? child) {
+      return ListView.builder(
+        reverse: true,
+        itemCount: messageRecords.length,
+        itemBuilder: (context, index) {
+          return MessageBubble(
+              messageRecords.elementAt(index),
+              index >= messageRecords.length - 1
+                  ? null
+                  : messageRecords.elementAt(index + 1).value,
+              index == 0 ? null : messageRecords.elementAt(index - 1).value);
+        },
+      );
+    });
+  }
+
+  Widget buildMessageBar() {
+    return Row(children: [
+      Expanded(
+        // Text field
+        child: TextFormField(
+          textInputAction: TextInputAction.send,
+          onFieldSubmitted: _send,
+          controller: _newMessage,
+          decoration: InputDecoration(
+            // Send icon
+            suffixIcon: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () {
+                  var text = _newMessage.value.text;
+                  if (text.isEmpty) {
+                    return;
+                  }
+                  _send(text);
+                }),
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: 'Message'.i18n,
+          ),
+        ),
+      ),
+      // Attachments icon
+      GestureDetector(
+        onTap: () => _selectFilesToShare(),
+        child: const Icon(Icons.image),
+      ),
+      // Recording icon
+      GestureDetector(
+        onTapDown: (details) {
+          _startRecording();
+        },
+        onTapUp: (details) async {
+          await _finishRecording();
+        },
+        child: const Icon(Icons.mic),
+      ),
+    ]);
+  }
+
+  Widget buildVoiceRecorder() {
+    return Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+      Flexible(
+        child: ColoredBox(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 17),
+                child: Icon(Icons.circle, color: Colors.red),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 22),
+                child: StreamBuilder<int>(
+                  stream: _stopWatchTimer.rawTime,
+                  initialData: _stopWatchTimer.rawTime.valueWrapper?.value,
+                  builder: (context, snap) {
+                    final value = snap.data;
+                    final displayTime = StopWatchTimer.getDisplayTime(
+                        value ?? 0,
+                        minute: true,
+                        second: true,
+                        hours: false,
+                        milliSecond: false);
+                    return Text(displayTime,
+                        style: const TextStyle(fontWeight: FontWeight.bold));
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 63,
+                  child: Padding(
+                      padding: const EdgeInsets.only(right: 24),
+                      child: Text(
+                          _willCancelRecording
+                              ? 'will cancel'.i18n
+                              : '< ' + 'swipe to cancel'.i18n,
+                          style: const TextStyle(fontWeight: FontWeight.bold))),
+                ),
+              ),
+              GestureDetector(
+                onTapUp: (details) async {
+                  await _finishRecording();
+                },
+                child: Transform.scale(
+                  scale: 2,
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(38)),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                          left: 15, top: 15, right: 4, bottom: 4),
+                      child: Icon(Icons.mic_none),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ]);
   }
 }
