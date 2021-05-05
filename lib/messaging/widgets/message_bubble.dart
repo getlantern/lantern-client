64b9503669854142c098ba954f4bc045005141c7
Kallirroi <kallirroi.retzepi@gmail.com>
2021-05-05 14:41:04 -0400

    built messag actions menu

--- lib/messaging/widgets/message_bubble.dart
+++ lib/messaging/widgets/message_bubble.dart
@@ -2,6 +2,7 @@ import 'package:lantern/model/model.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pb.dart';
 import 'package:lantern/package_store.dart';
 import 'package:lantern/utils/humanize.dart';
+import 'package:flutter/services.dart';
 
 import '../messaging_model.dart';
 import 'attachment.dart';
@@ -60,6 +61,7 @@ class MessageBubble extends StatelessWidget {
             ]),
           ]);
 
+      // Render attachments
       innerColumn.children.addAll(msg.attachments.values
           .map((attachment) => attachmentWidget(attachment)));
       innerColumn.children.add(statusRow);
@@ -82,97 +84,145 @@ class MessageBubble extends StatelessWidget {
       var endOfBlock = nextMessage == null ||
           nextMessage!.direction != message.value.direction;
       var newestMessage = nextMessage == null;
-      var row = Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment:
-            outbound ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: [
-          Flexible(
-            child: Padding(
-              padding: EdgeInsets.only(
-                  left: outbound ? 20 : 4,
-                  right: outbound ? 4 : 20,
-                  top: 4,
-                  bottom: 4),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: outbound ? Colors.black38 : Colors.black12,
-                  borderRadius: BorderRadius.only(
-                    topLeft: inbound && !startOfBlock
-                        ? Radius.zero
-                        : const Radius.circular(5),
-                    topRight: outbound && !startOfBlock
-                        ? Radius.zero
-                        : const Radius.circular(5),
-                    bottomRight: outbound && (!endOfBlock || newestMessage)
-                        ? Radius.zero
-                        : const Radius.circular(5),
-                    bottomLeft: inbound && (!endOfBlock || newestMessage)
-                        ? Radius.zero
-                        : const Radius.circular(5),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      top: 4, bottom: 4, left: 8, right: 8),
-                  child: innerColumn,
-                ),
-              ),
-            ),
-          ),
-        ],
-      );
-
       return InkWell(
-        onDoubleTap: () {},
         onLongPress: () {
-          showModalBottomSheet(
-              context: context,
-              isDismissible: true,
-              builder: (context) {
-                return Wrap(children: [
-                  if (msg.direction == MessageDirection.IN)
-                    Row(
-                      children: ['👍', '👎', '😄', '❤', '😢']
-                          .map(
-                            (e) => Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: InkWell(
-                                onTap: () {
-                                  model.react(message, e);
-                                  Navigator.pop(context);
-                                },
-                                child: Transform.scale(
-                                    scale: 2,
-                                    child: Text(e,
-                                        style: const TextStyle(fontSize: 16))),
-                              ),
-                            ),
-                          )
-                          .toList(growable: false),
-                    ),
-                  ListTile(
-                    leading: const Icon(Icons.delete),
-                    title: Text('Delete for me'.i18n),
-                    onTap: () {
-                      model.deleteLocally(message);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  if (msg.direction == MessageDirection.OUT)
-                    ListTile(
-                      leading: const Icon(Icons.delete),
-                      title: Text('Delete for everyone'.i18n),
-                      onTap: () {
-                        model.deleteGlobally(message);
-                        Navigator.pop(context);
-                      },
-                    ),
-                ]);
-              });
+          _buildActionsPopup(context, msg, model);
         },
-        child: row,
+        child: _buildRow(outbound, inbound, startOfBlock, endOfBlock,
+            newestMessage, innerColumn),
       );
     });
+  }
+
+  Widget _buildRow(bool outbound, bool inbound, bool startOfBlock,
+      bool endOfBlock, bool newestMessage, Column innerColumn) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment:
+          outbound ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: outbound ? 20 : 4,
+                right: outbound ? 4 : 20,
+                top: 4,
+                bottom: 4),
+            child: _buildBubbleUI(outbound, inbound, startOfBlock, endOfBlock,
+                newestMessage, innerColumn),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future _buildActionsPopup(
+      BuildContext context, StoredMessage msg, MessagingModel model) {
+    return showModalBottomSheet(
+        context: context,
+        isDismissible: true,
+        builder: (context) {
+          return Wrap(children: [
+            const Padding(
+              padding: EdgeInsets.all(8),
+            ),
+            // Other users' messages
+            // if (msg.direction == MessageDirection.IN)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                '👍',
+                '👎',
+                '😄',
+                '❤',
+                '😢',
+                '...'
+              ] // TODO: render dots as icon
+                  .map((e) => Container(
+                        margin: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color:
+                              Colors.grey.shade200, // TODO generalize in theme
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(999)),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: InkWell(
+                            onTap: () {
+                              model.react(message, e);
+                              Navigator.pop(context);
+                            },
+                            child: Transform.scale(
+                                scale: 1.2,
+                                child: Text(e,
+                                    style: const TextStyle(fontSize: 16))),
+                          ),
+                        ),
+                      ))
+                  .toList(growable: false),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Divider(height: 3),
+            ),
+            ListTile(
+              leading: const Icon(Icons.reply),
+              title: Text('Reply'.i18n),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.copy),
+              title: Text('Copy Text'.i18n),
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: message.value.text));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.delete),
+              title: Text('Delete for me'.i18n),
+              onTap: () {
+                model.deleteLocally(message);
+                Navigator.pop(context);
+              },
+            ),
+            // User's own messages
+            if (msg.direction == MessageDirection.OUT)
+              ListTile(
+                leading: const Icon(Icons.delete_forever),
+                title: Text('Delete for everyone'.i18n),
+                onTap: () {
+                  model.deleteGlobally(message);
+                  Navigator.pop(context);
+                },
+              ),
+          ]);
+        });
+  }
+
+  Widget _buildBubbleUI(bool outbound, bool inbound, bool startOfBlock,
+      bool endOfBlock, bool newestMessage, Column innerColumn) {
+    return Container(
+      decoration: BoxDecoration(
+        color: outbound ? Colors.black38 : Colors.black12,
+        borderRadius: BorderRadius.only(
+          topLeft:
+              inbound && !startOfBlock ? Radius.zero : const Radius.circular(5),
+          topRight: outbound && !startOfBlock
+              ? Radius.zero
+              : const Radius.circular(5),
+          bottomRight: outbound && (!endOfBlock || newestMessage)
+              ? Radius.zero
+              : const Radius.circular(5),
+          bottomLeft: inbound && (!endOfBlock || newestMessage)
+              ? Radius.zero
+              : const Radius.circular(5),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 4, left: 8, right: 8),
+        child: innerColumn,
+      ),
+    );
   }
 }
