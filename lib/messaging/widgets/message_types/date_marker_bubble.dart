6e02e2fd905fe29dabfdf8551aa1104e05ddd8c8
Kallirroi <kallirroi.retzepi@gmail.com>
2021-05-12 14:30:02 -0400

    styled DatedMarker

--- lib/messaging/widgets/message_types/date_marker_bubble.dart
+++ lib/messaging/widgets/message_types/date_marker_bubble.dart
@@ -1,11 +1,8 @@
-import 'package:lantern/model/protos_flutteronly/messaging.pbserver.dart';
 import 'package:lantern/package_store.dart';
-import 'package:lantern/utils/humanize.dart';
+import 'package:intl/intl.dart';
 
 class DateMarker extends StatelessWidget {
-  final StoredMessage msg;
-
-  const DateMarker(this.msg) : super();
+  final date = DateFormat.yMMMMd('en_US').format(DateTime.now()).toString();
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +14,8 @@ class DateMarker extends StatelessWidget {
             color: Colors.black,
             borderRadius: BorderRadius.all(Radius.circular(50)),
           ),
-          child: Text(msg.ts.toInt().humanizeDate(),
-              style: const TextStyle(
-                  fontSize: 10,
-                  color: Colors.white)), //TODO: Return [day/date] format
+          child: Text(date, // TODO: Add i18n
+              style: const TextStyle(fontSize: 10, color: Colors.white)),
         ));
   }
 }
