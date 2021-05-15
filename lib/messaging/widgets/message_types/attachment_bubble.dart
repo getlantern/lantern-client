f49838c400951e1e99c2834e8d32768df6a55dc6
Kallirroi <kallirroi.retzepi@gmail.com>
2021-05-15 18:09:34 -0400

    cleaned up attachment_bubble

--- lib/messaging/widgets/message_types/attachment_bubble.dart
+++ lib/messaging/widgets/message_types/attachment_bubble.dart
@@ -1,9 +1,9 @@
+import 'package:lantern/messaging/widgets/attachment.dart';
+import 'package:lantern/messaging/widgets/message_types/content_container.dart';
+import 'package:lantern/messaging/widgets/message_types/status_row.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pbserver.dart';
 import 'package:lantern/package_store.dart';
 import 'package:lantern/model/model.dart';
-import '../attachment.dart';
-import '../message_utils.dart';
-import 'package:lantern/utils/humanize.dart';
 
 class AttachmentBubble extends StatelessWidget {
   final bool outbound;
@@ -28,76 +28,56 @@ class AttachmentBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Row that holds the reactions (if any) and the timestamp
-    var statusRow = Row(mainAxisSize: MainAxisSize.min, children: []);
-    // add reactions to statusRow
-    reactions.forEach((key, value) {
-      // only render this if the list of [reactorIds] is not empty
-      if (value.isNotEmpty) {
-        statusRow.children.add(Container(
-            margin: const EdgeInsets.only(top: 4),
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: GestureDetector(
-                // Tap on emoji to bring modal with breakdown of interactions
-                onTap: () =>
-                    displayEmojiBreakdownPopup(context, msg, reactions),
-                child: displayEmojiCount(reactions, key))));
-      }
-    });
-    // add timestamp to statusRow
-    statusRow.children.add(Opacity(
-      opacity: 0.5,
-      child: Text(
-        message.value.ts.toInt().humanizeDate(),
-        style: TextStyle(
-          color: outbound ? Colors.white : Colors.black,
-          fontSize: 12,
-        ),
-      ),
-    ));
-    // add statusIcon to statusRow
-    final statusIcon = getStatusIcon(inbound, msg);
-    if (statusIcon != null) {
-      statusRow.children
-          .add(Transform.scale(scale: .5, child: Icon(statusIcon)));
-    }
-
-    // contains body of message
-    var contentContainer = Column(
+    final attachments = msg.attachments.values
+        .map((attachment) => attachmentWidget(attachment));
+    return Column(
         crossAxisAlignment:
             outbound ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
-        children: []);
-
-    // add attachments to contentContainer
-    //TODO: This is problematic because it re-renders every time
-    contentContainer.children.addAll(msg.attachments.values
-        .map((attachment) => attachmentWidget(attachment)));
-
-    // add statusRow to contentContainer
-    contentContainer.children.add(statusRow);
-
-    return Container(
-      decoration: BoxDecoration(
-        color: outbound ? Colors.black38 : Colors.black12,
-        borderRadius: BorderRadius.only(
-          topLeft:
-              inbound && !startOfBlock ? Radius.zero : const Radius.circular(5),
-          topRight: outbound && !startOfBlock
-              ? Radius.zero
-              : const Radius.circular(5),
-          bottomRight: outbound && (!endOfBlock || newestMessage)
-              ? Radius.zero
-              : const Radius.circular(5),
-          bottomLeft: inbound && (!endOfBlock || newestMessage)
-              ? Radius.zero
-              : const Radius.circular(5),
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-        child: contentContainer,
-      ),
-    );
+        children: [
+          Container(
+              constraints: const BoxConstraints(
+                minWidth: 90,
+              ),
+              decoration: BoxDecoration(
+                color: outbound ? Colors.black38 : Colors.black12,
+                borderRadius: BorderRadius.only(
+                  topLeft: inbound && !startOfBlock
+                      ? Radius.zero
+                      : const Radius.circular(5),
+                  topRight: outbound && !startOfBlock
+                      ? Radius.zero
+                      : const Radius.circular(5),
+                  bottomRight: outbound && (!endOfBlock || newestMessage)
+                      ? Radius.zero
+                      : const Radius.circular(5),
+                  bottomLeft: inbound && (!endOfBlock || newestMessage)
+                      ? Radius.zero
+                      : const Radius.circular(5),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: outbound
+                    ? CrossAxisAlignment.end
+                    : CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(
+                          top: 4, bottom: 4, left: 8, right: 8),
+                      child: Column(
+                          crossAxisAlignment: outbound
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [...attachments]),
+                          ])),
+                  StatusRow(outbound, inbound, reactions, msg, message)
+                ],
+              )),
+        ]);
   }
 }
