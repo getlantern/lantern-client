5f41e73336a018ddf3d9b9746b5c80a1905cdfb1
atavism <todd@lana.io>
2021-06-05 20:26:34 -0700

    initial commit

--- lib/ui/widgets/auth/validators.dart
+++ lib/ui/widgets/auth/validators.dart
@@ -4,12 +4,12 @@ class Validators {
   static const String _regexUsername = r"^(?=.*[\w])(?=.{1,}[^\d]).{4,28}$";
    static const String _regexPassword = r"^(?=.*\d)(?=.*[\w])(?=.{1,}[^\d]).{6,20}$";
 
-  static bool validateUsername(String value) {
-    return RegExp(_regexUsername).hasMatch(value);
+  static bool validateUsername(String? value) {
+    return value != null && RegExp(_regexUsername).hasMatch(value);
   }
 
   static FormFieldValidator<String> usernameValidator() {
-    return (String value) {
+    return (String? value) {
       if (validateUsername(value)) {
         return null;
       }
@@ -18,8 +18,8 @@ class Validators {
   }
 
   static FormFieldValidator<String> passwordValidator() {
-    return (String value) {
-      if (RegExp(_regexPassword).hasMatch(value)) {
+    return (String? value) {
+      if (value != null && RegExp(_regexPassword).hasMatch(value)) {
         return null;
       }
       return "Password must contain at least 1 letter, 1 number and be between 6 and 20 characters";
@@ -27,7 +27,7 @@ class Validators {
   }
 
   static FormFieldValidator<String> confirmPasswordValidator(TextEditingController passwordController) {
-    return (String value) {
+    return (String? value) {
       if (value != passwordController.text) return "Passwords must match";
       return null;
     };
