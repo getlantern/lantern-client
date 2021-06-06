8bf5e6a8b60736ba21d29af1bb7ad616bd626c57
atavism <todd@lana.io>
2021-06-06 06:21:51 -0700

    Add create account screens

---
+++ lib/ui/widgets/auth/register/step_one.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:lantern/package_store.dart';
+import 'package:lantern/ui/routes.dart';
+import 'package:lantern/ui/widgets/custom_text_field.dart';
+import 'package:lantern/ui/widgets/auth/validators.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+import '../../button.dart';
+
+class StepOne extends StatelessWidget {
+  StepOne({Key? key}) : super(key: key);
+
+  final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var sessionModel = context.watch<SessionModel>();
+
+    return BaseScreen(
+      title: 'Create Account'.i18n,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsetsDirectional.only(top: 32),
+                child: CustomTextField(
+                  controller: usernameController,
+                  label: 'Username'.i18n,
+                  keyboardType: TextInputType.text,
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  validator: Validators.usernameValidator(),
+                ),
+              ),
+              const Spacer(),
+              Container(
+                margin: const EdgeInsetsDirectional.only(bottom: 32),
+                child: Button(
+                  width: 200,
+                  text: 'Continue'.i18n,
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      context.loaderOverlay.show();
+                      sessionModel
+                          .setUsername(usernameController.value.text);
+                      context.loaderOverlay.hide();
+                      Navigator.pushNamed(
+                            context, routeAuthRegisterStepTwo);    
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/auth/register/step_two.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:lantern/package_store.dart';
+import 'package:lantern/ui/routes.dart';
+import 'package:lantern/ui/widgets/custom_text_field.dart';
+import 'package:lantern/ui/widgets/auth/validators.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+import '../../button.dart';
+
+class StepTwo extends StatelessWidget {
+  StepTwo({Key? key}) : super(key: key);
+
+  final passwordController        = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var sessionModel = context.watch<SessionModel>();
+
+    return BaseScreen(
+      title: 'Create Account'.i18n,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsetsDirectional.only(top: 32),
+                child: CustomTextField(
+                  controller: passwordController,
+                  label: 'Password'.i18n,
+                  keyboardType: TextInputType.text,
+                  prefixIcon: const Icon(
+                    Icons.lock_outline,
+                    color: Colors.black,
+                  ),
+                  suffixIcon: const Icon(
+                    Icons.visibility,
+                    color: Colors.black,
+                  ),
+                  validator: Validators.passwordValidator(),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsetsDirectional.only(top: 32),
+                child: CustomTextField(
+                  controller: confirmPasswordController,
+                  label: 'Confirm Password'.i18n,
+                  keyboardType: TextInputType.text,
+                  prefixIcon: const Icon(
+                    Icons.lock_outline,
+                    color: Colors.black,
+                  ),
+                  validator: Validators.confirmPasswordValidator(passwordController),
+                ),
+              ),
+              const Spacer(),
+              Container(
+                margin: const EdgeInsetsDirectional.only(bottom: 32),
+                child: Button(
+                  width: 200,
+                  text: 'Continue'.i18n,
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      context.loaderOverlay.show();
+                      sessionModel
+                          .createAccount(passwordController.value.text)
+                          .then((result) {
+                        context.loaderOverlay.hide();
+                        Navigator.pushNamed(
+                            context, routeAuthRegisterStepTwo);
+                      }).onError((error, stackTrace) {
+                        context.loaderOverlay.hide();
+                      });
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
