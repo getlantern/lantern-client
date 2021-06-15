fc54ea4cb3dfe8850598b08691e5ac678e7ff556
Kallirroi <kallirroi.retzepi@gmail.com>
2021-06-15 11:46:20 -0400

    added sizer plugin, styling QR screen

--- lib/messaging/add_contact_QR.dart
+++ lib/messaging/add_contact_QR.dart
@@ -6,6 +6,8 @@ import 'package:lantern/package_store.dart';
 import 'package:lantern/ui/widgets/button.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:sizer/sizer.dart';
 
 class AddViaQR extends StatefulWidget {
   @override
@@ -14,7 +16,6 @@ class AddViaQR extends StatefulWidget {
 
 class _AddViaQRState extends State<AddViaQR> {
   final _qrKey = GlobalKey(debugLabel: 'QR');
-  final _formKey = GlobalKey<FormState>(debugLabel: 'Contact Form');
 
   QRViewController? qrController;
   TextEditingController contactId = TextEditingController();
@@ -75,59 +76,91 @@ class _AddViaQRState extends State<AddViaQR> {
     var model = context.watch<MessagingModel>();
 
     return BaseScreen(
-      title: 'Add Contact'.i18n,
-      actions: [
-        IconButton(
-            icon: const Icon(Icons.qr_code),
-            tooltip: 'Your Contact Info'.i18n,
-            onPressed: () {
-              Navigator.restorablePushNamed(context, '/your_contact_info');
-            }),
-      ],
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 400,
-              child: QRView(
-                key: _qrKey,
-                onQRViewCreated: _onQRViewCreated,
-              ),
-            ),
-            Column(children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                    'To start a message with your friend, scan each others QR code.  This process will verify the security and end-to-end encryption of your conversation.'
-                        .i18n),
-              ),
-              if (isVerified)
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Button(
-                          text: 'Continue to message'.i18n,
-                          onPressed: () async {
-                            context.loaderOverlay.show();
-                            try {
-                              await model.addOrUpdateDirectContact(
-                                  contactId.value.text, displayName.value.text);
-                              // Navigator.pushNamedAndRemoveUntil(
-                              //     context, 'conversations', (r) => false);
-                              Navigator.pop(context);
-                            } finally {
-                              context.loaderOverlay.hide();
-                            }
-                          },
+        title: 'QR Code'.i18n,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.qr_code),
+              tooltip: 'Your Contact Info'.i18n,
+              onPressed: () {
+                Navigator.restorablePushNamed(context, '/your_contact_info');
+              }),
+        ],
+        body: model.me((BuildContext context, Contact me, Widget? child) {
+          displayName.text = me.displayName;
+
+          return SingleChildScrollView(
+              child: Container(
+            height: 100.h,
+            color: Colors.black,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 50, bottom: 10),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
                         ),
-                      ]),
-                )
-            ]),
-          ],
-        ),
-      ),
-    );
+                        width: 200,
+                        height: 200,
+                        child: QRView(
+                          key: _qrKey,
+                          onQRViewCreated: _onQRViewCreated,
+                        ),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: 200,
+                        height: 200,
+                        child: QrImage(
+                          data: me.writeToJson(),
+                          errorCorrectionLevel: QrErrorCorrectLevel.H,
+                          version: QrVersions.auto,
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                        width: 70.w,
+                        child: Text(
+                          'To start a message with your friend, scan each others QR code.  This process will verify the security and end-to-end encryption of your conversation.'
+                              .i18n,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                  ),
+                  // if (isVerified)
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Button(
+                            text: 'Continue to message'.i18n,
+                            onPressed: () async {
+                              context.loaderOverlay.show();
+                              try {
+                                await model.addOrUpdateDirectContact(
+                                    contactId.value.text,
+                                    displayName.value.text);
+                                // Navigator.pushNamedAndRemoveUntil(
+                                //     context, 'conversations', (r) => false);
+                                Navigator.pop(context);
+                              } finally {
+                                context.loaderOverlay.hide();
+                              }
+                            },
+                          ),
+                        ]),
+                  )
+                ]),
+          ));
+        }));
   }
 }
