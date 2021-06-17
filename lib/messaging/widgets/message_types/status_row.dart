e7a773847fa8486e61cbd4e8cdf689db65c80514
Kallirroi <kallirroi.retzepi@gmail.com>
2021-06-17 12:25:02 -0400

    animation works

--- lib/messaging/widgets/message_types/status_row.dart
+++ lib/messaging/widgets/message_types/status_row.dart
@@ -3,15 +3,14 @@ import 'package:lantern/package_store.dart';
 import 'package:lantern/model/model.dart';
 import 'package:lantern/messaging/widgets/message_utils.dart';
 import 'package:lantern/utils/humanize.dart';
-import 'dart:async';
 
-class StatusRow extends StatefulWidget {
+class StatusRow extends StatelessWidget {
   final bool outbound;
   final bool inbound;
   final StoredMessage msg;
   final PathAndValue<StoredMessage> message;
 
-  StatusRow(
+  const StatusRow(
     this.outbound,
     this.inbound,
     this.msg,
@@ -19,77 +18,53 @@ class StatusRow extends StatefulWidget {
   ) : super();
 
   @override
-  _StatusRowState createState() => _StatusRowState();
-}
-
-class _StatusRowState extends State<StatusRow>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation animation;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-        duration: Duration(milliseconds: 4000), vsync: this);
-
-    animation = Tween(begin: 4.00, end: 12.00).animate(controller);
-
-    controller.forward();
-
-    controller.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final statusIcon = getStatusIcon(widget.inbound, widget.msg);
+    final statusIcon = getStatusIcon(inbound, msg);
 
-    final lifespan =
-        (widget.msg.disappearAt - DateTime.now().millisecondsSinceEpoch);
-    var step = (lifespan.toInt() / 12).round();
-    var remainingTime = lifespan.toInt() - step;
-    var index = (remainingTime / step).floor();
+    // final lifespan =
+    //     (msg.disappearAt - DateTime.now().millisecondsSinceEpoch);
+    // var step = (lifespan.toInt() / 12).round();
+    // var remainingTime = lifespan.toInt() - step;
+    // var index = (remainingTime / step).floor();
 
-    return Container(
-        child: Opacity(
-            opacity: 0.8,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 1),
-                    child: Text(
-                      widget.message.value.ts.toInt().humanizeDate(),
-                      style: tsMessageStatus(widget.outbound),
-                    ),
-                  ),
-                  if (statusIcon != null)
-                    Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 1),
-                        child: Icon(
-                          statusIcon,
-                          size: 12,
-                          color: widget.outbound
-                              ? outboundMsgColor
-                              : inboundMsgColor,
-                        )),
-                  Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 1),
-                      child: CustomAssetImage(
-                          path: ImagePaths.countdownPaths[0],
-                          size: animation.value,
-                          color: widget.outbound
-                              ? outboundMsgColor
-                              : inboundMsgColor)),
-                ])));
+    return TweenAnimationBuilder<int>(
+        tween: IntTween(begin: 0, end: 12),
+        duration: const Duration(seconds: 10),
+        builder: (BuildContext context, int index, Widget? child) {
+          return Container(
+              child: Opacity(
+                  opacity: 0.8,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 1),
+                          child: Text(
+                            message.value.ts.toInt().humanizeDate(),
+                            style: tsMessageStatus(outbound),
+                          ),
+                        ),
+                        if (statusIcon != null)
+                          Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 1),
+                              child: Icon(
+                                statusIcon,
+                                size: 12,
+                                color: outbound
+                                    ? outboundMsgColor
+                                    : inboundMsgColor,
+                              )),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 1),
+                            child: CustomAssetImage(
+                                path: ImagePaths.countdownPaths[index],
+                                size: 12,
+                                color: outbound
+                                    ? outboundMsgColor
+                                    : inboundMsgColor)),
+                      ])));
+        });
   }
 }
