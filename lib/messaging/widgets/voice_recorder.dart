7de6107551f770b2e71a2ce74f82706bc869a086
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-06-21 08:49:51 -0600

    Replaced voice_recorder code
    Moved everything related with voice_recorder from message_bar into voice_recorder.dart

--- lib/messaging/widgets/voice_recorder.dart
+++ lib/messaging/widgets/voice_recorder.dart
@@ -2,74 +2,62 @@ import 'dart:ui';
 import 'package:flutter/widgets.dart';
 import 'package:lantern/package_store.dart';
 import 'package:lantern/utils/custom_pan_gesture_recognizer.dart';
-import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class VoiceRecorder extends StatelessWidget {
   const VoiceRecorder({
     Key? key,
-    required this.stopWatchTimer,
     required this.willCancelRecording,
     required this.onSwipeLeft,
+    required this.isRecording,
+    required this.onRecording,
+    required this.onStopRecording,
     required this.onTapUpListener,
   }) : super(key: key);
 
-  final StopWatchTimer stopWatchTimer;
   final bool willCancelRecording;
+  final bool isRecording;
   final VoidCallback onSwipeLeft;
+  final VoidCallback onRecording;
+  final VoidCallback onStopRecording;
   final Function onTapUpListener;
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-      Flexible(
-        child: ColoredBox(
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Expanded(
+    return Stack(
+      clipBehavior: Clip.none,
+      fit: StackFit.passthrough,
+      children: [
+        isRecording
+            ? Transform.scale(
+                scale: 2,
+                alignment: Alignment.bottomRight,
                 child: Container(
-                  alignment: Alignment.center,
-                  height: 63,
-                  child: Padding(
-                      padding: const EdgeInsets.only(right: 24),
-                      child: Text(
-                          willCancelRecording
-                              ? 'will cancel'.i18n
-                              : '< ' + 'swipe to cancel'.i18n,
-                          style: const TextStyle(fontWeight: FontWeight.bold))),
-                ),
-              ),
-              ForcedPanDetector(
-                onPanDown: _onPanDown,
-                onPanEnd: _onPanEnd,
-                onPanUpdate: _onPanUpdate,
-                onTap: () {},
-                onDoubleTap: () {},
-                // onTapUp: (details) => onTapUpListener(),
-                child: Transform.scale(
-                  scale: 2,
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.red,
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(38)),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.only(
-                          left: 15, top: 15, right: 4, bottom: 4),
-                      child: Icon(Icons.mic_none),
-                    ),
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(38)),
+                  ),
+                  child: const SizedBox(
+                    height: 50,
+                    width: 40,
                   ),
                 ),
-              ),
-            ],
+              )
+            : const SizedBox(),
+        ForcedPanDetector(
+          onPanDown: _onPanDown,
+          onPanUpdate: _onPanUpdate,
+          onPanEnd: _onPanEnd,
+          onDoubleTap: () {},
+          onTap: () {},
+          child: Icon(
+            Icons.mic,
+            size: isRecording ? 30.0 : 25,
+            color: isRecording ? Colors.white : Colors.black,
           ),
         ),
-      ),
-    ]);
+      ],
+    );
   }
 
   void _onPanUpdate(Offset details) => _handlePan(details, false);
@@ -80,9 +68,13 @@ class VoiceRecorder extends StatelessWidget {
     if (isPanEnd && details.dx <= 200.0) {
       onSwipeLeft();
     }
+    if (isPanEnd && details.dx > 200.0) {
+      onStopRecording();
+    }
   }
 
   bool _onPanDown(Offset details) {
+    onRecording();
     _handlePan(details, false);
     return true;
   }
