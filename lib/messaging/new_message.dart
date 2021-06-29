e6bf87c4116d5b855aedb32d51950d3fb1a7485e
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-06-29 16:27:19 -0600

    Working on navigation
    Implemented navigation for qr, username and everything related to messaging

--- lib/messaging/new_message.dart
+++ lib/messaging/new_message.dart
@@ -1,7 +1,10 @@
+import 'package:lantern/messaging/add_contact_username.dart';
 import 'package:lantern/messaging/messaging_model.dart';
 import 'package:lantern/model/model.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pb.dart';
 import 'package:lantern/package_store.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:lantern/core/router/router.gr.dart';
 
 class NewMessage extends StatelessWidget {
   static const NUM_RECENT_CONTACTS = 10;
@@ -25,18 +28,14 @@ class NewMessage extends StatelessWidget {
           leading: const Icon(Icons.person_add),
           title: Text('Add Contact by username'.i18n),
           trailing: const Icon(Icons.keyboard_arrow_right_outlined),
-          onTap: () {
-            Navigator.restorablePushNamed(context, '/add_contact_username');
-          },
+          onTap: () async => await context.pushRoute(const AddUsername()),
         ),
         const Divider(thickness: 1),
         ListTile(
           leading: const Icon(Icons.qr_code),
           title: Text('Scan QR Code'.i18n),
           trailing: const Icon(Icons.keyboard_arrow_right_outlined),
-          onTap: () {
-            Navigator.restorablePushNamed(context, '/add_contact_QR');
-          },
+          onTap: () async => await context.pushRoute(const AddQR()),
         ),
         const Divider(thickness: 1),
         ListTile(
@@ -84,10 +83,9 @@ class NewMessage extends StatelessWidget {
                       title: Text(contact.value.displayName.isEmpty
                           ? 'Unnamed contact'.i18n
                           : contact.value.displayName),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/conversation',
-                            arguments: contact.value);
-                      },
+                      onTap: () async => await context.pushRoute(
+                        Conversation(contact: contact.value),
+                      ),
                     ));
               },
             );
