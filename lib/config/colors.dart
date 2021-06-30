acfd1037775be9c0797a1fc6508aeea9f08c0445
Kallirroi <kallirroi.retzepi@gmail.com>
2021-06-30 14:10:34 -0400

    tiny color fix

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -15,20 +15,21 @@ Color grey2 = HexColor('#F5F5F5');
 Color grey3 = HexColor('#EBEBEB');
 Color grey4 = HexColor('#BFBFBF');
 Color grey5 = HexColor('#707070');
-Color indicatorRed = HexColor('#D5001F');
-Color indicatorGreen = HexColor('#00A83E');
 Color overlayBlack = HexColor('#000000CB');
+Color borderColor = grey3;
 
 Color outboundBgColor = HexColor('#007A7C');
 Color outboundMsgColor = HexColor('#FFFFFF');
 Color inboundMsgColor = HexColor('#000000');
 Color inboundBgColor = HexColor('#EBEBEB');
 
+Color indicatorRed = HexColor('#D5001F');
+Color indicatorGreen = secondaryBlue;
+Color offSwitchColor = grey5;
+Color onSwitchColor = secondaryBlue;
+Color usedDataBarColor = primaryBlue;
+
 Color unselectedTabColor = grey1;
 Color selectedTabColor = white;
 Color unselectedTabLabelColor = grey5;
 Color selectedTabLabelColor = black;
-Color borderColor = grey3;
-Color offSwitchColor = grey5;
-Color onSwitchColor = secondaryBlue;
-Color usedDataBarColor = primaryBlue;
