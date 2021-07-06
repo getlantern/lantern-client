ca8df3e6a5170a21f1c2704f25c62b19fd235e1b
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-06 08:01:33 -0600

    Working on Audio Preview

--- lib/messaging/widgets/message_bar.dart
+++ lib/messaging/widgets/message_bar.dart
@@ -1,12 +1,16 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:lantern/messaging/widgets/countdown_timer.dart';
-import 'package:lantern/messaging/widgets/voice_recorder.dart';
+import 'package:lantern/messaging/widgets/message_bar_preview_recording.dart';
+import 'package:lantern/messaging/widgets/message_bar_recording.dart';
 import 'package:lantern/package_store.dart';
 import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class MessageBar extends StatelessWidget {
   final bool displayEmojis;
   final bool willCancelRecording;
+  final bool finishedRecording;
+  final Uint8List? recording;
   final VoidCallback? onEmojiTap;
   final VoidCallback onTextFieldTap;
   final VoidCallback? onSend;
@@ -23,13 +27,15 @@ class MessageBar extends StatelessWidget {
   final bool isRecording;
   final double width;
   final double height;
-  final VoidCallback onSwipeLeft;
+  final VoidCallback? onCancelRecording;
   final Function onTapUpListener;
 
   MessageBar(
       {this.onEmojiTap,
       this.focusNode,
-      required this.onSwipeLeft,
+      required this.recording,
+      required this.finishedRecording,
+      required this.onCancelRecording,
       required this.onTapUpListener,
       required this.willCancelRecording,
       required this.stopWatchTimer,
@@ -58,82 +64,33 @@ class MessageBar extends StatelessWidget {
       margin: isRecording
           ? const EdgeInsets.only(right: 0, left: 8.0, bottom: 0)
           : const EdgeInsets.only(right: 1.0, left: 1.0, bottom: 6.0),
-      child: ListTile(
-        contentPadding: isRecording
-            ? const EdgeInsets.only(right: 0, left: 8.0, bottom: 0)
-            : const EdgeInsets.only(right: 8.0, left: 8.0, bottom: 0),
-        leading: isRecording
-            ? Flex(
-                direction: Axis.horizontal,
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Flexible(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.red,
-                      radius: 12,
-                    ),
-                  ),
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: CountdownTimer(stopWatchTimer: stopWatchTimer),
-                    ),
-                  ),
-                ],
-              )
-            : IconButton(
-                onPressed: onEmojiTap,
-                icon: Icon(Icons.sentiment_very_satisfied,
-                    color: !displayEmojis
-                        ? Theme.of(context).primaryIconTheme.color
-                        : Theme.of(context).primaryColorDark),
-              ),
-        title: isRecording
-            ? const SizedBox()
-            : TextFormField(
-                autofocus: false,
-                textInputAction: TextInputAction.send,
-                controller: messageController,
-                onTap: onTextFieldTap,
-                onChanged: onTextFieldChanged,
-                focusNode: focusNode,
-                onFieldSubmitted: onFieldSubmitted,
-                decoration: InputDecoration(
-                  // Send icon
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  hintText: 'Message'.i18n,
-                  border: const OutlineInputBorder(),
+      child: IndexedStack(
+        index: finishedRecording ? 1 : 0,
+        children: [
+          MessageBarRecording(
+            finishedRecording: finishedRecording,
+            onTapUpListener: onTapUpListener,
+            stopWatchTimer: stopWatchTimer,
+            isRecording: isRecording,
+            onSend: onSend,
+            hasPermission: hasPermission,
+            onRecording: onRecording,
+            sendIcon: sendIcon,
+            onStopRecording: onStopRecording,
+            onTextFieldChanged: onTextFieldChanged,
+            messageController: messageController,
+            onFieldSubmitted: onFieldSubmitted,
+            onFileSend: onFileSend,
+            displayEmojis: displayEmojis,
+            onTextFieldTap: onTextFieldTap,
+          ),
+          recording == null
+              ? const SizedBox()
+              : MessageBarPreviewRecording(
+                  onCancelRecording: onCancelRecording!,
+                  recording: recording,
                 ),
-              ),
-        trailing: sendIcon && !isRecording
-            ? IconButton(
-                icon: const Icon(Icons.send, color: Colors.black),
-                onPressed: onSend,
-              )
-            : Flex(
-                direction: Axis.horizontal,
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  isRecording
-                      ? const SizedBox()
-                      : IconButton(
-                          onPressed: onFileSend,
-                          icon: const Icon(Icons.add_circle_rounded),
-                        ),
-                  VoiceRecorder(
-                    willCancelRecording: willCancelRecording,
-                    onSwipeLeft: onSwipeLeft,
-                    isRecording: isRecording,
-                    onStopRecording: onStopRecording,
-                    onTapUpListener: onTapUpListener,
-                    onRecording: onRecording,
-                  ),
-                ],
-              ),
+        ],
       ),
     );
   }
---
+++ lib/messaging/widgets/message_bar_recording.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:lantern/messaging/widgets/countdown_timer.dart';
+import 'package:lantern/messaging/widgets/voice_recorder.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:lantern/package_store.dart';
+
+class MessageBarRecording extends StatelessWidget {
+  final bool displayEmojis;
+  final bool finishedRecording;
+  final VoidCallback? onEmojiTap;
+  final VoidCallback onTextFieldTap;
+  final VoidCallback? onSend;
+  final VoidCallback? onFileSend;
+  final VoidCallback onRecording;
+  final VoidCallback onStopRecording;
+  final Function(String)? onTextFieldChanged;
+  final Function(String)? onFieldSubmitted;
+  final TextEditingController messageController;
+  final StopWatchTimer stopWatchTimer;
+  final bool hasPermission;
+  final FocusNode? focusNode;
+  final bool sendIcon;
+  final bool isRecording;
+  final Function onTapUpListener;
+  const MessageBarRecording(
+      {this.onEmojiTap,
+      this.focusNode,
+      required this.finishedRecording,
+      required this.onTapUpListener,
+      required this.stopWatchTimer,
+      required this.isRecording,
+      required this.onSend,
+      required this.hasPermission,
+      required this.onRecording,
+      required this.onStopRecording,
+      required this.onTextFieldChanged,
+      required this.messageController,
+      required this.onFieldSubmitted,
+      required this.onFileSend,
+      required this.displayEmojis,
+      required this.onTextFieldTap,
+      this.sendIcon = false,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: isRecording
+          ? const EdgeInsets.only(right: 0, left: 8.0, bottom: 0)
+          : const EdgeInsets.only(right: 8.0, left: 8.0, bottom: 0),
+      leading: isRecording
+          ? Flex(
+              direction: Axis.horizontal,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Flexible(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.red,
+                    radius: 12,
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: CountdownTimer(stopWatchTimer: stopWatchTimer),
+                  ),
+                ),
+              ],
+            )
+          : IconButton(
+              onPressed: onEmojiTap,
+              icon: Icon(Icons.sentiment_very_satisfied,
+                  color: !displayEmojis
+                      ? Theme.of(context).primaryIconTheme.color
+                      : Theme.of(context).primaryColorDark),
+            ),
+      title: isRecording
+          ? const SizedBox()
+          : TextFormField(
+              autofocus: false,
+              textInputAction: TextInputAction.send,
+              controller: messageController,
+              onTap: onTextFieldTap,
+              onChanged: onTextFieldChanged,
+              focusNode: focusNode,
+              onFieldSubmitted: onFieldSubmitted,
+              decoration: InputDecoration(
+                // Send icon
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                hintText: 'Message'.i18n,
+                border: const OutlineInputBorder(),
+              ),
+            ),
+      trailing: sendIcon && !isRecording
+          ? IconButton(
+              icon: const Icon(Icons.send, color: Colors.black),
+              onPressed: onSend,
+            )
+          : Flex(
+              direction: Axis.horizontal,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                isRecording
+                    ? const SizedBox()
+                    : IconButton(
+                        onPressed: onFileSend,
+                        icon: const Icon(Icons.add_circle_rounded),
+                      ),
+                VoiceRecorder(
+                  isRecording: isRecording,
+                  onStopRecording: onStopRecording,
+                  onTapUpListener: onTapUpListener,
+                  onRecording: onRecording,
+                ),
+              ],
+            ),
+    );
+  }
+}
