9b4cb65024b7006dcf8878b8f132fdfa4b605221
Ox Cart <ox.to.a.cart@gmail.com>
2021-07-13 11:13:07 -0500

    Remove crashlytics

--- android/app/src/main/java/org/getlantern/mobilesdk/ProdLogger.java
+++ android/app/src/main/java/org/getlantern/mobilesdk/ProdLogger.java
@@ -3,11 +3,8 @@ package org.getlantern.mobilesdk;
 import android.content.Context;
 import internalsdk.Internalsdk;
 import android.util.Log;
-import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 public class ProdLogger extends Logger {
-    private FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
-
     protected ProdLogger() {
         super();
     }
@@ -20,20 +17,11 @@ public class ProdLogger extends Logger {
     @Override
     protected void log(int level, String tag, String msg) {
         Internalsdk.debug(tag, msg);
-        logToCrashlytics(level, tag, msg);
     }
 
     @Override
     protected void logError(String tag, String msg, Throwable t) {
         Internalsdk.error(tag, msg);
-        logToCrashlytics(Log.ERROR, tag, msg);
-        if (t != null) {
-            FirebaseCrashlytics.getInstance().recordException(t);
-        }
-    }
-
-    private void logToCrashlytics(int level, String tag, String msg) {
-        crashlytics.log(String.format("%s %s: %s", levelToString(level), tag, msg));
     }
 
     private static String levelToString(int level) {
