c7eda8045958da3790788f4e96472bcf5f8ab526
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-14 06:32:05 -0600

    Working on catcher manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
 
+import 'package:catcher/catcher.dart';
 import 'package:flutter/material.dart';
+import 'package:lantern/config/catcher_setup.dart';
 
 import 'ui/app.dart';
 
 void main() {
-  runApp(LanternApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Catcher(
+      rootWidget: LanternApp(),
+      debugConfig: catcherOptions,
+      releaseConfig: catcherOptions);
+  // runApp(LanternApp());
 }
