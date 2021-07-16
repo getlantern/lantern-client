19af1acfd46960d1b544c3a7776ad4115bae5a3d
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-15 18:03:32 -0600

    Removed showGeneralDialog and replaced with a scaffold

--- lib/ui/widgets/full_screen_dialog.dart
+++ lib/ui/widgets/full_screen_dialog.dart
@@ -1,19 +1,14 @@
-import 'package:lantern/config/transitions.dart';
 import 'package:lantern/package_store.dart';
 
 /// Shows the supplied widget as a full screen dialog
-void showFullScreenDialog(BuildContext context, Widget widget) {
-  showGeneralDialog(
-    context: context,
-    transitionBuilder: defaultTransition,
-    transitionDuration: defaultTransitionDuration,
-    pageBuilder: (BuildContext buildContext, Animation animation,
-        Animation secondaryAnimation) {
-      return SizedBox.expand(
-        child: Material(
-          child: widget,
-        ),
-      );
-    },
-  );
+class FullScreenDialog extends StatelessWidget {
+  final Widget widget;
+  const FullScreenDialog({required this.widget, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget,
+    );
+  }
 }
