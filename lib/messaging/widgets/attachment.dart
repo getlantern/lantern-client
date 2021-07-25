44207b6f6d013fcb4f90f572f0e29f46d186d2ea
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-25 10:14:50 -0600

    Added audio/opus

--- lib/messaging/widgets/attachment.dart
+++ lib/messaging/widgets/attachment.dart
@@ -17,19 +17,12 @@ Widget attachmentWidget(StoredAttachment attachment, bool inbound) {
     case 'audio/mp3':
     case 'audio/m4a':
     case 'audio/flac':
+    case 'audio/opus':
     case 'audio/aac':
-      return Flexible(child: VoiceMemo(attachment, inbound));
-
-    ///(LUIS): Im not quite sure if it's neccesary to sepparate the VoiceMemo from an audio format.
-    /// at the end both are audio files.
     case 'audio/mp4':
     case 'audio/mkv':
     case 'audio/mpeg':
-      return Flexible(
-          child: GenericAttachment(
-              attachmentTitle: attachmentTitle,
-              inbound: inbound,
-              icon: Icons.audiotrack));
+      return Flexible(child: VoiceMemo(attachment, inbound));
     case 'image/jpeg':
     case 'image/png':
     case 'image/bpm':
