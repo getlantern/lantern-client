b3bc468f40c37870b4ec13e3d4156186962c575b
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-25 10:15:45 -0600

    Created wave progress

---
+++ lib/utils/waveform/wave_progress_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:lantern/utils/waveform/background_painter.dart';
+import 'package:lantern/utils/waveform/single_bar_painter.dart';
+import 'dart:math';
+
+class WaveProgressBar extends StatelessWidget {
+  final double progressPercentage;
+  final List<double> listOfHeights;
+  final Alignment alignment;
+  final double width;
+  final Color initalColor;
+  final Color? progressColor;
+  final Color backgroundColor;
+  final double begin;
+
+  WaveProgressBar({
+    required this.listOfHeights,
+    this.initalColor = Colors.grey,
+    this.progressColor = Colors.red,
+    this.backgroundColor = Colors.white,
+    this.alignment = Alignment.center,
+    this.begin = 0,
+    required this.width,
+    required this.progressPercentage,
+  });
+  @override
+  Widget build(BuildContext context) {
+    var arrayOfBars = <Widget>[];
+    arrayOfBars.add(CustomPaint(
+      painter: BackgroundBarPainter(
+        containerWidth: width,
+        containerHeight: listOfHeights.reduce(max),
+        progresPercentage: progressPercentage,
+        initialColor: initalColor,
+        progressColor: progressColor,
+      ),
+    ));
+
+    for (var i = 0; i < listOfHeights.length; i++) {
+      arrayOfBars.add(
+        CustomPaint(
+          painter: SingleBarPainter(
+            startingPosition: i * (width / listOfHeights.length),
+            singleBarWidth: width / listOfHeights.length,
+            maxSeekBarHeight: listOfHeights.reduce(max) + 1,
+            actualSeekBarHeight: listOfHeights[i],
+            heightOfContainer: listOfHeights.reduce(max),
+            backgroundColor: backgroundColor,
+          ),
+        ),
+      );
+    }
+    return Container(
+      height: listOfHeights.reduce(max),
+      width: width,
+      alignment: alignment,
+      child: Row(
+        children: arrayOfBars,
+      ),
+    );
+  }
+}
