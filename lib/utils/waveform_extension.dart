c9d7d2568effe255db9b6bc79d3d73d7ffdec0de
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-25 10:15:22 -0600

    Created function to return a list of factors of commons numbers

---
+++ lib/utils/waveform_extension.dart
@@ -0,0 +1,13 @@
+extension WaveformExtension on List<double> {
+  List<double> reduceList(int factor) {
+    var _reduced = <double>[];
+    var _counter = 0.0;
+    for (var _element in this) {
+      if (_counter % factor == 0) {
+        _reduced.add(_element);
+      }
+      _counter++;
+    }
+    return _reduced;
+  }
+}
