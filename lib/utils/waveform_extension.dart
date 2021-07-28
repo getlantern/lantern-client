f98bfb8ba65199432854e5f76d5db21653a46d06
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-28 15:38:43 -0600

    Comment

--- lib/utils/waveform_extension.dart
+++ lib/utils/waveform_extension.dart
@@ -3,6 +3,7 @@ extension WaveformExtension on List<double> {
   /// ```dart
   /// var list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
   /// list.reduceListWithAverageAndSteps(5, 2) => [1.5, 3.5, 5.5, 7.5, 9.5])
+  /// ```
   List<double> reduceListWithAverageAndSteps(
     int _size,
     int _step,
