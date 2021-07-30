8cc28ad89fb9741372cc03160856c5cb55a94121
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-07-30 09:36:40 -0600

    Working on widget tests

--- test/base_screen_test.dart
+++ test/base_screen_test.dart
@@ -1,31 +1,95 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lantern/core/router/router.gr.dart';
+import 'package:lantern/messaging/messaging_model.dart';
 import 'package:lantern/package_store.dart';
 import 'package:lantern/ui/app.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sizer/sizer.dart';
+
+class MockNavigationObserver extends Mock implements NavigatorObserver {}
 
 void main() {
+  NavigatorObserver mockObserver;
+
   group(
     'Widget startup',
     () {
+      mockObserver = MockNavigationObserver();
+
+      Future<void> _buildHomeScreen(WidgetTester tester) async {
+        await tester.pumpWidget(MultiProvider(
+          providers: [
+            Provider(create: (context) => MessagingModel()),
+            Provider(create: (context) => VpnModel()),
+            Provider(create: (context) => AudioPlayer()),
+            Provider(create: (context) => SessionModel()),
+            Provider(
+                create: (context) => EventManager('lantern_event_channel')),
+            Provider(
+                create: (context) =>
+                    const MethodChannel('lantern_method_channel')),
+          ],
+          child: FutureBuilder(
+              future: Localization.loadTranslations(),
+              builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+                return GlobalLoaderOverlay(
+                  child: I18n(
+                    initialLocale: const Locale('en', 'US'),
+                    child: Sizer(
+                      builder: (context, orientation, deviceType) {
+                        return MaterialApp.router(
+                          debugShowCheckedModeBanner: false,
+                          title: 'Lantern Messenger',
+                          localizationsDelegates: [
+                            GlobalMaterialLocalizations.delegate,
+                            GlobalWidgetsLocalizations.delegate,
+                            GlobalCupertinoLocalizations.delegate,
+                          ],
+                          routeInformationParser:
+                              globalRouter.defaultRouteParser(),
+                          routerDelegate: globalRouter.delegate(
+                            navigatorObservers: () => [
+                              mockObserver,
+                            ],
+                          ),
+                          builder: BotToastInit(),
+                          supportedLocales: [
+                            const Locale('ar', 'EG'),
+                            const Locale('fr', 'FR'),
+                            const Locale('en', 'US'),
+                            const Locale('fa', 'IR'),
+                            const Locale('th', 'TH'),
+                            const Locale('ms', 'MY'),
+                            const Locale('ru', 'RU'),
+                            const Locale('ur', 'IN'),
+                            const Locale('zh', 'CN'),
+                            const Locale('zh', 'HK'),
+                            const Locale('es', 'ES'),
+                            const Locale('tr', 'TR'),
+                            const Locale('vi', 'VN'),
+                            const Locale('my', 'MM'),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                );
+              }),
+        ));
+        // Check that navigator observer was called.
+        expect(mockObserver, MockNavigationObserver);
+      }
+
       testWidgets(
-        'Test coverage for base_screen.dart',
-        (WidgetTester tester) async {
-          print('Load the root widget without catcher');
-          await tester.pumpWidget(LanternApp());
-
-          var baseScreen = find.byType(BaseScreen);
-          print('Check for the BaseScreen to be loaded correctly');
-          expect(baseScreen, findsOneWidget);
-
-          var customAppbar = find.byType(CustomAppBar);
-          print('Check for the CustomAppBar to be loaded correctly');
-          expect(customAppbar, findsOneWidget);
-
-          var homePage = find.byType(HomePage);
-          print(
-              'At this point the widget has already been loaded, however the router still is not configured');
-          expect(homePage, findsNothing);
-        },
-      );
+          'when tapping "country" button, should navigate to details page',
+          (WidgetTester tester) async {
+        await _buildHomeScreen(tester);
+        // Ensure that state has ben changed
+        await tester.pump();
+      });
     },
   );
 }
