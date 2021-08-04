b33e3e422e2d076b6ea9de70ada1cdddf3304266
Ox Cart <ox.to.a.cart@gmail.com>
2021-08-04 12:16:06 -0500

    Factored repetitive logic in reply_mime into _PreviewBuilder

--- lib/messaging/widgets/reply/reply_mime.dart
+++ lib/messaging/widgets/reply/reply_mime.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
-import 'package:lantern/config/colors.dart';
 
 import 'package:flutter/material.dart';
+import 'package:lantern/config/colors.dart';
 import 'package:lantern/enums/enum_extension.dart';
 import 'package:lantern/messaging/messaging_model.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pb.dart';
@@ -20,47 +20,37 @@ class ReplyMime extends StatelessWidget {
 
     switch (_mimeType) {
       case MimeTypes.AUDIO:
-        return FutureBuilder(
-            future: model.decryptAttachment(
-                storedMessage.attachments[0] as StoredAttachment),
-            builder:
-                (BuildContext context, AsyncSnapshot<Uint8List?>? snapshot) =>
-                    snapshot == null || !snapshot.hasData
-                        ? _getIconWrapper(Icons.error_outlined)
-                        : _getIconWrapper(Icons.volume_up));
+        return _PreviewBuilder(
+          future: model.decryptAttachment(
+              storedMessage.attachments[0] as StoredAttachment),
+          builder: (BuildContext context, Uint8List data) =>
+              _getIconWrapper(Icons.volume_up),
+        );
       case MimeTypes.VIDEO:
-        return FutureBuilder(
+        return _PreviewBuilder(
           future:
               model.thumbnail(storedMessage.attachments[0] as StoredAttachment),
-          builder:
-              (BuildContext context, AsyncSnapshot<Uint8List?>? snapshot) =>
-                  snapshot == null || !snapshot.hasData
-                      ? _getIconWrapper(Icons.error_outlined)
-                      : Stack(alignment: Alignment.center, children: [
-                          Image.memory(snapshot.data!,
-                              errorBuilder: (BuildContext context, Object error,
-                                      StackTrace? stackTrace) =>
-                                  _getIconWrapper(Icons.error_outlined),
-                              filterQuality: FilterQuality.high,
-                              height: 56),
-                          const Icon(Icons.play_circle_outline,
-                              color: Colors.white),
-                        ]),
+          builder: (BuildContext context, Uint8List data) =>
+              Stack(alignment: Alignment.center, children: [
+            Image.memory(data,
+                errorBuilder: (BuildContext context, Object error,
+                        StackTrace? stackTrace) =>
+                    _getIconWrapper(Icons.error_outlined),
+                filterQuality: FilterQuality.high,
+                height: 56),
+            const Icon(Icons.play_circle_outline, color: Colors.white),
+          ]),
         );
       case MimeTypes.IMAGE:
-        return FutureBuilder(
+        return _PreviewBuilder(
           future:
               model.thumbnail(storedMessage.attachments[0] as StoredAttachment),
-          builder:
-              (BuildContext context, AsyncSnapshot<Uint8List?>? snapshot) =>
-                  snapshot == null || !snapshot.hasData
-                      ? _getIconWrapper(Icons.error_outlined)
-                      : Image.memory(snapshot.data!,
-                          errorBuilder: (BuildContext context, Object error,
-                                  StackTrace? stackTrace) =>
-                              _getIconWrapper(Icons.error_outlined),
-                          filterQuality: FilterQuality.high,
-                          height: 56),
+          builder: (BuildContext context, Uint8List data) => Image.memory(data,
+              errorBuilder: (BuildContext context, Object error,
+                      StackTrace? stackTrace) =>
+                  _getIconWrapper(Icons.error_outlined),
+              filterQuality: FilterQuality.high,
+              height: 56),
         );
       case MimeTypes.OTHERS:
       case MimeTypes.EMPTY:
@@ -76,16 +66,30 @@ class ReplyMime extends StatelessWidget {
         );
     }
   }
+}
 
-  Widget _getIconWrapper(IconData icon) {
-    return Container(
-      color: snippetBgIconColor,
-      padding: const EdgeInsets.all(8.0),
-      child: Icon(
-        icon,
-        size: 18,
-        color: Colors.white,
-      ),
-    );
-  }
+class _PreviewBuilder<T> extends FutureBuilder<T> {
+  _PreviewBuilder({
+    Key? key,
+    required Future<T> future,
+    required Widget Function(BuildContext context, T value) builder,
+  }) : super(
+            key: key,
+            future: future,
+            builder: (BuildContext context, AsyncSnapshot<T>? snapshot) =>
+                snapshot == null || !snapshot.hasData
+                    ? _getIconWrapper(Icons.error_outlined)
+                    : builder(context, snapshot.data!));
+}
+
+Widget _getIconWrapper(IconData icon) {
+  return Container(
+    color: snippetBgIconColor,
+    padding: const EdgeInsets.all(8.0),
+    child: Icon(
+      icon,
+      size: 18,
+      color: Colors.white,
+    ),
+  );
 }
