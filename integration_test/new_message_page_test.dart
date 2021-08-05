fbed7e81e1abbc4fa4a275f7562e0ab755b2854d
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-05 15:29:32 -0600

    Setup for testing on list of contact recently added.

--- integration_test/new_message_page_test.dart
+++ integration_test/new_message_page_test.dart
@@ -1,15 +1,10 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:lantern/messaging/messages.dart';
-import 'package:lantern/messaging/messaging_model.dart';
 import 'package:lantern/messaging/new_message.dart';
 import 'package:lantern/messaging/widgets/contact_message_preview.dart';
 import 'package:lantern/ui/app.dart';
-import 'package:lantern/ui/home.dart';
-import 'package:lantern/ui/widgets/custom_bottom_bar.dart';
-import 'package:mockito/mockito.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:i18n_extension/i18n_widget.dart';
 export 'package:lantern/i18n/i18n.dart';
@@ -63,11 +58,10 @@ void main() {
       expect(contactList.semanticChildCount, equals(1));
       print('We check if the ListView can be scrollable');
       print('Manually scrolling 200 pixels down');
-      await tester.drag(find.byType(ListView), const Offset(0.0, 200.0));
+      await tester.drag(find.byType(ListView), const Offset(0.0, -300.0));
       print('Flush the widget tree');
       await tester.pumpAndSettle();
-      var contactElement = tester.widget<ContactMessagePreview>(
-          find.byType(ContactMessagePreview, skipOffstage: false));
+      var contactElement = find.widgetWithText(ContactMessagePreview, 'Me');
       expect(contactElement, findsOneWidget);
     });
 
