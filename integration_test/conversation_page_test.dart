e0e98c6bcdf006321894dff3f70bc5f36cfe7336
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-09 13:32:45 -0600

    Working on integration test cases

--- integration_test/conversation_page_test.dart
+++ integration_test/conversation_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:lantern/messaging/conversation.dart';
 import 'package:lantern/messaging/new_message.dart';
 import 'package:lantern/messaging/widgets/contact_message_preview.dart';
 import 'package:lantern/ui/app.dart';
@@ -24,7 +25,41 @@ void main() {
       expect(find.byType(NewMessage), findsOneWidget);
       await tester.tap(find.widgetWithText(ContactMessagePreview, 'me'));
       await tester.pumpAndSettle();
-      await tester.pump(const Duration(seconds: 20));
+      expect(find.byType(Conversation), findsOneWidget);
+    });
+
+    testWidgets('Input message to send', (WidgetTester tester) async {
+      await tester.pumpWidget(LanternApp());
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(NewMessage), findsOneWidget);
+      await tester.tap(find.widgetWithText(ContactMessagePreview, 'me'));
+      await tester.pumpAndSettle();
+      print('Enter `hello this a message send from Flutter Test`');
+      await tester.enterText(find.byType(TextFormField),
+          'hello this a message send from Flutter Test');
+      await tester.pump();
+      print(
+          'await 2 seconds just to visualize the message in our TextFormField');
+      await tester.pump(const Duration(seconds: 2));
+      var textformfield =
+          tester.widget<TextFormField>(find.byType(TextFormField));
+      expect(textformfield.controller!.text,
+          equals('hello this a message send from Flutter Test'));
+    });
+
+    testWidgets('Go back using physical button to New Message Page',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(LanternApp());
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      await tester.pageBack();
+      await tester.pumpAndSettle();
+      expect(find.byType(NewMessage), findsOneWidget);
     });
   });
 }
