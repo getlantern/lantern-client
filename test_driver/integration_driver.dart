bedd21102f38587c5033f07ce006b7db038418a7
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-14 13:32:25 -0600

    Working on integration test for audio

--- test_driver/integration_driver.dart
+++ test_driver/integration_driver.dart
@@ -1,3 +1,29 @@
+import 'dart:io';
+
 import 'package:integration_test/integration_test_driver.dart';
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  /// Looks like flutter has a bug where it doesn't properly handle
+  /// the permissions request.
+  /// https://github.com/flutter/flutter/issues/12561
+  /// we need to execute a custom command with android adb in order to grant
+  /// permissions to the app.
+
+  /// Please change the following line to your own adb path.
+  final adbPath = '/Users/crdzbird/Library/Android/sdk/platform-tools/adb';
+  await Process.run(adbPath, [
+    'shell',
+    'pm',
+    'grant',
+    'org.getlantern.lantern.LanternApp',
+    'android.permission.READ_EXTERNAL_STORAGE'
+  ]);
+  await Process.run(adbPath, [
+    'shell',
+    'pm',
+    'grant',
+    'org.getlantern.lantern.LanternApp',
+    'android.permission.RECORD_AUDIO'
+  ]);
+  await integrationDriver();
+}
