66f30e017137a268daa3d8aecf227c571c4b224b
Kallirroi <kallirroi.retzepi@gmail.com>
2021-08-17 13:55:30 +0300

    better handling of no contacts state

--- lib/messaging/introduce.dart
+++ lib/messaging/introduce.dart
@@ -27,97 +27,114 @@ class _IntroduceState extends State<Introduce> {
   Widget build(BuildContext context) {
     var model = context.watch<MessagingModel>();
     return BaseScreen(
-      title: 'Introduce Contacts (${selectedContactIds.length})'.i18n,
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-          child: Text(
-              'Select two or more contacts to introduce.  They will be sent invitations to start messaging each other. '
-                  .i18n,
-              style: tsBaseScreenBodyText),
-        ),
-        Expanded(
-          child: model.contacts(builder: (context,
-              Iterable<PathAndValue<Contact>> _contacts, Widget? child) {
-            if (_contacts.isEmpty) {
-              return Container(
-                alignment: AlignmentDirectional.center,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 16.0),
-                child: Text(
-                    'No contacts yet. Once you have two or more contacts you will be able to create introductions.'
-                        .i18n,
-                    style: tsBaseScreenBodyText),
-              );
-            }
-            var sortedContacts = _contacts.toList()
-              ..sort((a, b) => sanitizeContactName(a.value.displayName)
-                  .compareTo(sanitizeContactName(b.value.displayName)));
+        title: 'Introduce Contacts (${selectedContactIds.length})'.i18n,
+        body: model.contacts(builder: (context,
+            Iterable<PathAndValue<Contact>> _contacts, Widget? child) {
+          var sortedContacts = _contacts.toList()
+            ..sort((a, b) => sanitizeContactName(a.value.displayName)
+                .compareTo(sanitizeContactName(b.value.displayName)));
 
-            var groupedSortedContacts = sortedContacts
-                .groupBy((el) => sanitizeContactName(el.value.displayName));
+          var groupedSortedContacts = sortedContacts
+              .groupBy((el) => sanitizeContactName(el.value.displayName));
 
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+          return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                if (sortedContacts.isNotEmpty)
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0, vertical: 16.0),
+                    child: Text(
+                        'Select two or more contacts to introduce.  They will be sent invitations to start messaging each other. '
+                            .i18n,
+                        style: tsEmptyContactState),
+                  ),
                 Expanded(
-                  flex: 2,
-                  child: groupedContactListGenerator(
-                      groupedSortedList: groupedSortedContacts,
-                      leadingCallback: (Contact contact) => CircleAvatar(
-                            backgroundColor: avatarBgColors[
-                                generateUniqueColorIndex(contact.contactId.id)],
+                    child: (sortedContacts.isEmpty)
+                        ? Container(
+                            alignment: AlignmentDirectional.center,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 16.0),
                             child: Text(
-                                sanitizeContactName(contact.displayName)
-                                    .substring(0, 2)
-                                    .toUpperCase(),
-                                style: const TextStyle(color: Colors.white)),
-                          ),
-                      trailingCallback: (int index, Contact contact) =>
-                          Checkbox(
-                            checkColor: Colors.white,
-                            fillColor: MaterialStateProperty.resolveWith(
-                                getCheckboxColor),
-                            value: selectedContactIds
-                                .contains(contact.contactId.id),
-                            shape: const CircleBorder(side: BorderSide.none),
-                            onChanged: (bool? value) => setState(() {
-                              value!
-                                  ? selectedContactIds.add(contact.contactId.id)
-                                  : selectedContactIds
-                                      .remove(contact.contactId.id);
-                            }),
-                          )),
-                ),
-                (selectedContactIds.isNotEmpty)
-                    ? Container(
-                        color: grey1,
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                                'Once you have two or more contacts you will be able to create introductions.'
+                                    .i18n,
+                                style: tsBaseScreenBodyText),
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Button(
-                                width: 200,
-                                text: 'Send Invitations'.i18n.toUpperCase(),
-                                onPressed: () async {
-                                  await model.introduce(selectedContactIds);
-                                  showSnackbar(
-                                      context, 'Introductions Sent!'.i18n);
-                                  await Future.delayed(
-                                    const Duration(milliseconds: 1000),
-                                    () async => await context.router.pop(),
-                                  );
-                                },
-                              ),
-                            ]),
-                      )
-                    : Container()
-              ],
-            );
-          }),
-        )
-      ]),
-    );
+                                Expanded(
+                                  flex: 2,
+                                  child: groupedContactListGenerator(
+                                      groupedSortedList: groupedSortedContacts,
+                                      leadingCallback: (Contact contact) =>
+                                          CircleAvatar(
+                                            backgroundColor: avatarBgColors[
+                                                generateUniqueColorIndex(
+                                                    contact.contactId.id)],
+                                            child: Text(
+                                                sanitizeContactName(
+                                                        contact.displayName)
+                                                    .substring(0, 2)
+                                                    .toUpperCase(),
+                                                style: const TextStyle(
+                                                    color: Colors.white)),
+                                          ),
+                                      trailingCallback: (int index,
+                                              Contact contact) =>
+                                          Checkbox(
+                                            checkColor: Colors.white,
+                                            fillColor: MaterialStateProperty
+                                                .resolveWith(getCheckboxColor),
+                                            value: selectedContactIds
+                                                .contains(contact.contactId.id),
+                                            shape: const CircleBorder(
+                                                side: BorderSide.none),
+                                            onChanged: (bool? value) =>
+                                                setState(() {
+                                              value!
+                                                  ? selectedContactIds
+                                                      .add(contact.contactId.id)
+                                                  : selectedContactIds.remove(
+                                                      contact.contactId.id);
+                                            }),
+                                          )),
+                                ),
+                                (selectedContactIds.isNotEmpty)
+                                    ? Container(
+                                        color: grey1,
+                                        padding: const EdgeInsets.all(20.0),
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Button(
+                                                width: 200,
+                                                text: 'Send Invitations'
+                                                    .i18n
+                                                    .toUpperCase(),
+                                                onPressed: () async {
+                                                  await model.introduce(
+                                                      selectedContactIds);
+                                                  showSnackbar(
+                                                      context,
+                                                      'Introductions Sent!'
+                                                          .i18n);
+                                                  await Future.delayed(
+                                                    const Duration(
+                                                        milliseconds: 1000),
+                                                    () async => await context
+                                                        .router
+                                                        .pop(),
+                                                  );
+                                                },
+                                              ),
+                                            ]),
+                                      )
+                                    : Container()
+                              ]))
+              ]);
+        }));
   }
 }
