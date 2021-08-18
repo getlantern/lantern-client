4ff26ae4e87f8592491b1622ffdc33d44a585fe2
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-18 14:09:21 -0600

    finished audio playback functionality on integration test

--- integration_test/conversation_page_audio_test.dart
+++ integration_test/conversation_page_audio_test.dart
@@ -16,6 +16,7 @@ import 'package:lantern/messaging/widgets/voice_recorder/audio_widget.dart';
 import 'package:lantern/ui/app.dart';
 import 'package:lantern/utils/waveform/wave_progress_bar.dart';
 
+import 'helper_test.dart';
 import 'mock_clipboard.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:i18n_extension/i18n_widget.dart';
@@ -33,13 +34,6 @@ void main() {
       SystemChannels.platform
           .setMockMethodCallHandler(mockClipboard?.handleMethodCall);
     });
-
-    /// This function is called during each new message, to ensure that the
-    /// message is displayed correctly, we need to wait for the message to be
-    /// displayed.
-    Future<void> waitUntilSended(WidgetTester tester) async {
-      await tester.pump(const Duration(seconds: 1));
-    }
 
     testWidgets('Check for an audio preview being rendered',
         (WidgetTester tester) async {
@@ -98,10 +92,10 @@ void main() {
       await tester.pumpAndSettle();
       print('Press the play button');
       await tester.tap(find.widgetWithIcon(TextButton, Icons.play_arrow));
-      print('We wait 2 seconds and then press on the pause button');
+      print('We wait 3 seconds and then press on the pause button');
       print(
           'This should be approximately 33% or a little more of the whole audio duration');
-      await tester.pump(const Duration(seconds: 2));
+      await tester.pump(const Duration(seconds: 3));
       await tester.tap(find.widgetWithIcon(TextButton, Icons.pause));
       await tester.pumpAndSettle();
       expect(find.byType(Slider), findsOneWidget);
@@ -147,6 +141,63 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.byType(Slider), findsNothing);
       expect(find.byType(WaveProgressBar), findsNothing);
+    });
+
+    testWidgets('Send an AudioPreview', (WidgetTester tester) async {
+      await tester.pumpWidget(LanternApp());
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(NewMessage), findsOneWidget);
+      await tester
+          .tap(find.widgetWithText(ContactMessagePreview, 'Note to self'));
+      await tester.pumpAndSettle();
+      print('We should hold the recording button for 5 seconds');
+      print(
+          'Since flutter test don\'t have anything that ressemble a hold for X seconds event');
+      print(
+          'We use a timedDrag which in theory does the same, but we need to add +1 seconds due to the movement of the virtual pointer');
+      await tester.timedDrag(
+          find.byKey(
+            const ValueKey('btnRecord'),
+          ),
+          tester.getCenter(find.byKey(const ValueKey('btnRecord'))),
+          const Duration(seconds: 6));
+      await tester.pumpAndSettle();
+      print('Press the send button');
+      await tester.tap(find.widgetWithIcon(GestureDetector, Icons.send));
+      await waitUntilSended(tester);
+      expect(find.byType(MessageBubble), findsOneWidget);
+    });
+
+    testWidgets('Play the audio bubble', (WidgetTester tester) async {
+      await tester.pumpWidget(LanternApp());
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(NewMessage), findsOneWidget);
+      await tester
+          .tap(find.widgetWithText(ContactMessagePreview, 'Note to self'));
+      await waitUntilSended(tester);
+      print('Press the play button');
+      await tester.tap(find.widgetWithIcon(TextButton, Icons.play_arrow));
+      print('We wait 3 seconds and then press on the pause button');
+      print(
+          'This should be approximately 33% or a little more of the whole audio duration');
+      await tester.pump(const Duration(seconds: 3));
+      await tester.tap(find.widgetWithIcon(TextButton, Icons.pause));
+      await waitUntilSended(tester);
+      expect(find.byType(Slider), findsOneWidget);
+      var slider = tester.widget<Slider>(find.byType(Slider));
+      var waveform =
+          tester.widget<WaveProgressBar>(find.byType(WaveProgressBar));
+      print(
+          'Check if the value progress for the SLIDER and WAVEPROGRESSBAR is the same');
+      //check if progressPercentage is in a range.
+      expect((waveform).progressPercentage, greaterThanOrEqualTo(33));
+      expect((slider).value, greaterThanOrEqualTo(33));
     });
   });
 }
