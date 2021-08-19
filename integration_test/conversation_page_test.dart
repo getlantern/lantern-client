792dfef25968d4fc72bad5a53aaf768eb49613ed
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-19 10:11:33 -0600

    Fix of conversation_page_test.dart

--- integration_test/conversation_page_test.dart
+++ integration_test/conversation_page_test.dart
@@ -11,6 +11,7 @@ import 'package:lantern/messaging/widgets/message_types/content_container.dart';
 import 'package:lantern/messaging/widgets/message_types/status_row.dart';
 import 'package:lantern/ui/app.dart';
 
+import 'helper_test.dart';
 import 'mock_clipboard.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:i18n_extension/i18n_widget.dart';
@@ -28,13 +29,6 @@ void main() {
       SystemChannels.platform
           .setMockMethodCallHandler(mockClipboard?.handleMethodCall);
     });
-
-    /// This function is called during each new message, to ensure that the
-    /// message is displayed correctly, we need to wait for the message to be
-    /// displayed.
-    Future<void> waitUntilSended(WidgetTester tester) async {
-      await tester.pump(const Duration(seconds: 1));
-    }
 
     testWidgets(
         'Navigate from Messages Page into New Message Page into Conversation Page',
@@ -105,73 +99,7 @@ void main() {
           'We should see an AlertDialog with a text "This will delete the message for you only. Everyone else will still be able to see it."');
       expect(find.byType(AlertDialog), findsOneWidget);
       await waitUntilSended(tester);
-      await tester.tap(find.text('Delete'));
-      await waitUntilSended(tester);
-      print('The alert dialog should has been dismissed');
-      expect(find.byType(AlertDialog), findsNothing);
-      print('We shouldn\'t have any bubble widget');
-      expect(
-          find.widgetWithText(
-              ContentContainer, 'hello this a message send from Flutter Test'),
-          findsNothing);
-    });
-
-    testWidgets('Input a message to send and evaluate if the text was received',
-        (WidgetTester tester) async {
-      final sendButtonFinder = find.byKey(const ValueKey('send_message'));
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      expect(find.byType(NewMessage), findsOneWidget);
-      await tester.tap(find.widgetWithText(ContactListItem, 'Note to self'));
-      await tester.pumpAndSettle();
-      print('Enter `hello this a message send from Flutter Test`');
-      await tester.enterText(find.byType(TextFormField),
-          'hello this a message send from Flutter Test');
-      await tester.pump();
-      print(
-          'await 2 seconds just to visualize the message in our TextFormField');
-      await tester.pump(const Duration(seconds: 2));
-      var textformfield =
-          tester.widget<TextFormField>(find.byType(TextFormField));
-      expect(textformfield.controller!.text,
-          equals('hello this a message send from Flutter Test'));
-      await tester.tap(sendButtonFinder);
-      print('message has been send');
-      await waitUntilSended(tester);
-      print('check if MessageBubble was rendered');
-      expect(find.byType(MessageBubble), findsOneWidget);
-      print('Conversation should have a new widget with a text message');
-      expect(
-          find.widgetWithText(
-              ContentContainer, 'hello this a message send from Flutter Test'),
-          findsOneWidget);
-    });
-
-    testWidgets('Remove the message sended for everyone',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      expect(find.byType(NewMessage), findsOneWidget);
-      await tester.tap(find.widgetWithText(ContactListItem, 'Note to self'));
-      await waitUntilSended(tester);
-      await tester.longPress(find.widgetWithText(
-          ContentContainer, 'hello this a message send from Flutter Test'));
-      await waitUntilSended(tester);
-      print('Proceed to tap on the option "DELETE FOR EVERYONE"');
-      await tester.tap(find.text('Delete for everyone'));
-      print('Refresh the screen after doing a tap on "DELETE FOR EVERYONE"');
-      await waitUntilSended(tester);
-      print(
-          'We should see an AlertDialog with a text "This will delete the message for everyone."');
-      expect(find.byType(AlertDialog), findsOneWidget);
-      await waitUntilSended(tester);
-      await tester.tap(find.text('Delete'));
+      await tester.tap(find.text('DELETE'));
       await waitUntilSended(tester);
       print('The alert dialog should has been dismissed');
       expect(find.byType(AlertDialog), findsNothing);
@@ -233,8 +161,6 @@ void main() {
       await tester.tap(find.text('Copy Text'));
       print('Refresh the screen after doing a tap on "COPY TEXT"');
       await tester.pump();
-      print('We should see a Snackbar with a text "Copy Text"');
-      expect(find.text('Text copied'), findsOneWidget);
       await waitUntilSended(tester);
       print('The clipboard should have the same content as our MessageBubble');
       expect(
@@ -281,6 +207,25 @@ void main() {
       print('Refresh the screen after doing a tap on "👎"');
       await waitUntilSended(tester);
       expect(find.widgetWithText(StatusRow, '👎'), findsOneWidget);
+    });
+
+    testWidgets('Generate a 😄 reaction', (WidgetTester tester) async {
+      await tester.pumpWidget(LanternApp());
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(NewMessage), findsOneWidget);
+      await tester.tap(find.widgetWithText(ContactListItem, 'Note to self'));
+      await waitUntilSended(tester);
+      await tester.longPress(find.widgetWithText(
+          ContentContainer, 'hello this a message send from Flutter Test'));
+      await waitUntilSended(tester);
+      print('Proceed to tap on the 😄 reaction');
+      await tester.tap(find.byKey(const ValueKey('😄')));
+      print('Refresh the screen after doing a tap on "😄"');
+      await waitUntilSended(tester);
+      expect(find.widgetWithText(StatusRow, '😄'), findsOneWidget);
     });
 
     testWidgets('Generate a ❤ reaction', (WidgetTester tester) async {
@@ -435,25 +380,6 @@ void main() {
       print('Check if the message has been removed');
       expect(find.widgetWithText(ContentContainer, 'This will be shortly gone'),
           findsNothing);
-    });
-
-    testWidgets('Generate a 😄 reaction', (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      expect(find.byType(NewMessage), findsOneWidget);
-      await tester.tap(find.widgetWithText(ContactListItem, 'Note to self'));
-      await waitUntilSended(tester);
-      await tester.longPress(find.widgetWithText(
-          ContentContainer, 'hello this a message send from Flutter Test'));
-      await waitUntilSended(tester);
-      print('Proceed to tap on the 😄 reaction');
-      await tester.tap(find.byKey(const ValueKey('😄')));
-      print('Refresh the screen after doing a tap on "😄"');
-      await waitUntilSended(tester);
-      expect(find.widgetWithText(StatusRow, '😄'), findsOneWidget);
     });
 
     testWidgets('Send a custom emoji', (WidgetTester tester) async {
