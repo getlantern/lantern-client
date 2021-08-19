b12e398e9e3a7e2cf2e84b3afe87e5503818a031
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-19 14:45:55 -0600

    Removed enableFlutterDrive from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
 import 'package:lantern/config/catcher_setup.dart';
 
 import 'ui/app.dart';
 
 Future<void> main() async {
-  enableFlutterDriverExtension();
   WidgetsFlutterBinding.ensureInitialized();
   setupCatcherAndRun(LanternApp());
 }
