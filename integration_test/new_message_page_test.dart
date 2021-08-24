9335ae42da28ec72d560d34ea8fc2e2bdae8d53b
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-24 16:02:38 -0600

    Reduced coded for integration test cases

--- integration_test/new_message_page_test.dart
+++ integration_test/new_message_page_test.dart
@@ -4,7 +4,9 @@ import 'package:integration_test/integration_test.dart';
 import 'package:lantern/messaging/messages.dart';
 import 'package:lantern/messaging/new_message.dart';
 import 'package:lantern/messaging/widgets/contact_list_item.dart';
-import 'package:lantern/ui/app.dart';
+
+import 'action/navigation_action_test.dart';
+import 'enums/screens_test.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:i18n_extension/i18n_widget.dart';
 export 'package:lantern/i18n/i18n.dart';
@@ -16,24 +18,18 @@ void main() {
   group('New Message Test', () {
     testWidgets('Navigate from Messages Page into New Message Page',
         (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
+      await GoTo.navigateTo(tester, to: SCREENS.MESSAGES);
+      await GoTo.navigateTo(tester,
+          from: SCREENS.MESSAGES, to: SCREENS.CONTACTS);
       expect(find.byType(NewMessage), findsOneWidget);
     });
 
     testWidgets(
         'Check for components loaded correctly on New Message Page AppBar',
         (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      print(
-          'We use 2 pump and settle due to the elevated number of process that must been done on Android before starting flutter.');
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
+      await GoTo.navigateTo(tester, to: SCREENS.MESSAGES);
+      await GoTo.navigateTo(tester,
+          from: SCREENS.MESSAGES, to: SCREENS.CONTACTS);
       print('Search for a CustomAppBar');
       expect(find.byType(AppBar), findsOneWidget);
       expect(find.widgetWithText(AppBar, 'New Message'), findsOneWidget);
@@ -44,36 +40,29 @@ void main() {
 
     testWidgets('Check for the body components on New Message Page',
         (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      print(
-          'We use 2 pump and settle due to the elevated number of process that must been done on Android before starting flutter.');
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      print('We have 1 contact registered');
-      expect(find.text('Recent contacts'.toUpperCase()), findsOneWidget);
+      await GoTo.navigateTo(tester, to: SCREENS.MESSAGES);
+      await GoTo.navigateTo(tester,
+          from: SCREENS.MESSAGES, to: SCREENS.CONTACTS);
+      expect(find.text('Recent contacts'.toUpperCase()), findsOneWidget,
+          reason: 'We have 1 contact registered');
       var contactList = tester.widget<ListView>(find.byType(ListView));
-      print('The list should have 1 element');
-      expect(contactList.semanticChildCount, equals(1));
+      expect(contactList.semanticChildCount, equals(1),
+          reason: 'The list should have 1 element');
       print('We check if the ListView can be scrollable');
       print('Manually scrolling 200 pixels down');
       await tester.drag(find.byType(ListView), const Offset(0.0, -300.0));
       print('Flush the widget tree');
       await tester.pumpAndSettle();
-      var contactElement = find.widgetWithText(ContactListItem, 'Me');
+      var contactElement = find.widgetWithText(ContactListItem, 'Note to self');
       expect(contactElement, findsOneWidget);
     });
 
     testWidgets('Go back using physical button to Messages Page',
         (WidgetTester tester) async {
-      await tester.pumpWidget(LanternApp());
-      await tester.pumpAndSettle();
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      await tester.pageBack();
-      await tester.pumpAndSettle();
+      await GoTo.navigateTo(tester, to: SCREENS.MESSAGES);
+      await GoTo.navigateTo(tester,
+          from: SCREENS.MESSAGES, to: SCREENS.CONTACTS);
+      await GoTo.navigateBack(tester);
       expect(find.byType(Messages), findsOneWidget);
     });
   });
