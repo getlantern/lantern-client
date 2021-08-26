c8f96642ecb44661331c81bf825f6d435ccca5b4
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-08-25 20:24:29 -0600

    Added limitation param for waveform

--- lib/utils/waveform_extension.dart
+++ lib/utils/waveform_extension.dart
@@ -5,13 +5,13 @@ extension WaveformExtension on List<int> {
   /// 2. Taking the max bar from each group
   /// 3. Converting the amplitude into a percentage
   /// ```
-  List<double> reducedWaveform() {
+  List<double> reducedWaveform({double limit = 100.0}) {
     var input = this;
     var output = <double>[];
-    var groupSize = (input.length / 100).round();
+    var groupSize = (input.length / limit).round();
     var max = 0.0;
     for (var i = 0; i < input.length; i++) {
-      var value = input[i].toDouble() / 255.0 * 100.0;
+      var value = input[i].toDouble() / 255.0 * limit;
       if (value > max) max = value;
       var next = i + 1;
       if (next % groupSize == 0 || next == input.length) {
