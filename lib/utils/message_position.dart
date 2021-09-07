d83a4eb9085f9be655eacf66240f35964c55364d
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-09-06 18:03:26 -0600

    Fix issue found during PR Review

--- lib/utils/message_position.dart
+++ lib/utils/message_position.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pbserver.dart';
 
 class MessagePosition {
+  //Returns the padding based on multiple conditions such as: Direction of the message, reactions, has previous bubble, has new bubble
   static double topPosition(Widget? widget, StoredMessage? nexMessage,
       StoredMessage? priorMessage, bool inbound, bool outbound) {
     if (priorMessage == null && nexMessage != null) return 15.0;
     return 0.0;
   }
 
+//Returns the padding based on multiple conditions such as: Direction of the message, reactions, has previous bubble, has new bubble
   static double bottomPosition(Widget? widget, StoredMessage? nexMessage,
       StoredMessage? priorMessage, bool inbound, bool outbound) {
     if (nexMessage != null && nexMessage.reactions.isNotEmpty) {
@@ -22,6 +24,6 @@ class MessagePosition {
         nexMessage.direction == MessageDirection.OUT) return 6.0;
     if (inbound && nexMessage != null && priorMessage != null) return 2.0;
     if (nexMessage == null) return 8.0;
-    return 0.0;
+    return 2.0;
   }
 }
