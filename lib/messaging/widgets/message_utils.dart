34a8d872746ff495cb07dfaab77cf432f5a881ac
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-08 17:09:52 -0400

    splitting contactID in tetrads

--- lib/messaging/widgets/message_utils.dart
+++ lib/messaging/widgets/message_utils.dart
@@ -346,3 +346,11 @@ Future<void> displayConversationOptions(
             ],
           ));
 }
+
+String humanizeContactId(String id) {
+  var humanizedId = id.splitMapJoin(RegExp('.{4}'),
+      onMatch: (m) => '${m[0]}', // (or no onMatch at all)
+      onNonMatch: (n) => '-');
+
+  return humanizedId.substring(1, humanizedId.length - 1);
+}
