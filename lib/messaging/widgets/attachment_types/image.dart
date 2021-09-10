803e3cbdfecae93c2bbfa3621582477f7ea25345
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2021-09-10 07:31:48 -0600

    Router to video/image page

--- lib/messaging/widgets/attachment_types/image.dart
+++ lib/messaging/widgets/attachment_types/image.dart
@@ -4,6 +4,8 @@ import 'package:lantern/messaging/widgets/attachment.dart';
 import 'package:lantern/model/protos_flutteronly/messaging.pb.dart';
 import 'package:lantern/package_store.dart';
 import 'package:lantern/ui/widgets/basic_memory_image.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:lantern/core/router/router.gr.dart';
 
 class ImageAttachment extends StatelessWidget {
   final StoredAttachment attachment;
@@ -20,28 +22,31 @@ class ImageAttachment extends StatelessWidget {
           inbound: inbound,
           defaultIcon: Icons.image,
           builder: (BuildContext context, Uint8List thumbnail) {
-            return ConstrainedBox(
-              // this box keeps the video from being too tall
-              constraints: BoxConstraints(maxHeight: constraints.maxWidth),
-              child: ShaderMask(
-                shaderCallback: (rect) {
-                  return LinearGradient(
-                    begin: Alignment.center,
-                    end: Alignment.bottomCenter,
-                    colors: [black, transparent],
-                  ).createShader(
-                      Rect.fromLTRB(0, 0, rect.width, rect.height * 1.5));
-                },
-                blendMode: BlendMode.dstIn,
-                child: FittedBox(
-                  child: BasicMemoryImage(
-                    thumbnail,
-                    filterQuality: FilterQuality.high,
-                    errorBuilder: (BuildContext context, Object error,
-                            StackTrace? stackTrace) =>
-                        Icon(Icons.error_outlined,
-                            color:
-                                inbound ? inboundMsgColor : outboundMsgColor),
+            return GestureDetector(
+              onTap: () => context.router.push(const ImageVideoDetailPage()),
+              child: ConstrainedBox(
+                // this box keeps the video from being too tall
+                constraints: BoxConstraints(maxHeight: constraints.maxWidth),
+                child: ShaderMask(
+                  shaderCallback: (rect) {
+                    return LinearGradient(
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter,
+                      colors: [black, transparent],
+                    ).createShader(
+                        Rect.fromLTRB(0, 0, rect.width, rect.height * 1.5));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: FittedBox(
+                    child: BasicMemoryImage(
+                      thumbnail,
+                      filterQuality: FilterQuality.high,
+                      errorBuilder: (BuildContext context, Object error,
+                              StackTrace? stackTrace) =>
+                          Icon(Icons.error_outlined,
+                              color:
+                                  inbound ? inboundMsgColor : outboundMsgColor),
+                    ),
                   ),
                 ),
               ),
