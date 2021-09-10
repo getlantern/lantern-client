09ee2280b24e22e9db05953d35851fff3336e897
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-10 15:20:53 -0400

    improved button UX

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -18,49 +18,55 @@ class Button extends StatelessWidget {
       this.disabled = false});
 
   void _handleOnPress() {
-    if (!disabled) onPressed!();
+    if (disabled) return null;
+    return onPressed!();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 56,
-      width: width,
-      child: OutlinedButton(
-        onPressed: _handleOnPress,
-        style: OutlinedButton.styleFrom(
-          backgroundColor: disabled
-              ? grey3
-              : secondary
-                  ? white
-                  : primaryPink,
-          padding: const EdgeInsets.symmetric(vertical: 15),
-          side: BorderSide(width: 2, color: disabled ? grey5 : primaryPink),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 23),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                text.toUpperCase(),
-                style: TextStyle(
-                    color: disabled
-                        ? grey5
-                        : secondary
-                            ? primaryPink
-                            : white,
-                    fontSize: 14),
-                textAlign: TextAlign.center,
-              ),
-              if (iconPath != null) const SizedBox(width: 5),
-              if (iconPath != null)
-                CustomAssetImage(
-                  path: iconPath!,
-                  color: Colors.white,
-                )
-            ],
+    return Opacity(
+      opacity: disabled ? 0.7 : 1,
+      child: SizedBox(
+        height: 56,
+        width: width,
+        child: OutlinedButton(
+          onPressed: _handleOnPress,
+          style: OutlinedButton.styleFrom(
+            splashFactory:
+                disabled ? NoSplash.splashFactory : InkSplash.splashFactory,
+            backgroundColor: disabled
+                ? grey3
+                : secondary
+                    ? white
+                    : primaryPink,
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            side: BorderSide(width: 2, color: disabled ? grey5 : primaryPink),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 23),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  text.toUpperCase(),
+                  style: TextStyle(
+                      color: disabled
+                          ? grey5
+                          : secondary
+                              ? primaryPink
+                              : white,
+                      fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                if (iconPath != null) const SizedBox(width: 5),
+                if (iconPath != null)
+                  CustomAssetImage(
+                    path: iconPath!,
+                    color: Colors.white,
+                  )
+              ],
+            ),
           ),
         ),
       ),
