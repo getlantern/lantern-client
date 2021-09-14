cacb82fbd063e0324b047597ee916d0bac5e5f03
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-14 13:22:49 -0400

    better initials logic

--- lib/messaging/custom_avatar.dart
+++ lib/messaging/custom_avatar.dart
@@ -14,8 +14,21 @@ class CustomAvatar extends StatelessWidget {
       backgroundColor:
           customColor ?? avatarBgColors[id!.hashCode % avatarBgColors.length],
       child: Text(
-          sanitizeContactName(displayName ?? '').substring(0, 2).toUpperCase(),
+          sanitizeContactName(displayName ?? '').getInitials().toUpperCase(),
           style: tsCircleAvatarLetter),
     );
+  }
+}
+
+extension StringExtensions on String {
+  String getInitials() {
+    // example
+    // 'kalli retzepi' => ['kalli', 'retzepi'] => we pick K and R (first characters of first and last substring)
+    // 'mies van der rohe' => ['mies', 'van', 'der', 'rohe'] => we pick M and R
+    // 'kalliretzepi' => ['k', 'a', 'l', ....] => we pick K and A (first and second)
+    var parts = this.split(' ');
+    return parts.isNotEmpty
+        ? '${parts.first.characters.first.toString()}${parts.last.characters.first.toString()}'
+        : parts.first.substring(0, 2);
   }
 }
