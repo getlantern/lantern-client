c0e1b5a9b8efcc985f111628e8a98929af70f831
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-14 11:06:35 -0400

    created sorting extension

--- lib/messaging/contacts/grouped_contact_list.dart
+++ lib/messaging/contacts/grouped_contact_list.dart
@@ -20,7 +20,6 @@ ScrollablePositionedList groupedContactListGenerator({
     itemBuilder: (context, index) {
       var key = groupedSortedList.keys.elementAt(index);
       var itemsPerKey = groupedSortedList.values.elementAt(index);
-      // print('\x1B[33m$key\x1B[0m');
       return ListBody(
         children: [
           Row(
