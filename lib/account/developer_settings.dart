a6c683e3db60e155a6076d4102ed012c48cf940d
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-21 14:49:47 -0400

    fix Developer screen

--- lib/account/developer_settings.dart
+++ lib/account/developer_settings.dart
@@ -12,17 +12,16 @@ class DeveloperSettingsTab extends StatelessWidget {
     return BaseScreen(
       title: 'Developer Settings'.i18n,
       body: ListView(
-        padding: const EdgeInsetsDirectional.all(8),
         children: [
           Container(
-            margin: const EdgeInsets.all(8),
+            margin: const EdgeInsetsDirectional.all(16.0),
             child: CTextWrap(
               'These settings are for development use only. Changing any setting will automatically stop and restart the application',
               style: tsBody3,
             ),
           ),
           Container(
-            margin: const EdgeInsets.all(8),
+            margin: const EdgeInsetsDirectional.all(16.0),
             child: CTextWrap(
                 'When Payment Test Mode is enabled, the app uses a single hardcoded user. You can make Stripe purchases using the number 4242 4242 4242 4242, an expiration date in the future, and any CVV.',
                 style: tsBody3),
@@ -31,16 +30,19 @@ class DeveloperSettingsTab extends StatelessWidget {
             title: 'Payment Test Mode'.i18n,
             child: sessionModel.paymentTestMode(
                 (BuildContext context, bool value, Widget? child) {
-              return FlutterSwitch(
-                width: 44.0,
-                height: 24.0,
-                valueFontSize: 12.0,
-                padding: 2,
-                toggleSize: 18.0,
-                value: value,
-                onToggle: (bool newValue) {
-                  sessionModel.setPaymentTestMode(newValue);
-                },
+              return Padding(
+                padding: const EdgeInsetsDirectional.only(end: 16.0),
+                child: FlutterSwitch(
+                  width: 44.0,
+                  height: 24.0,
+                  valueFontSize: 12.0,
+                  padding: 2,
+                  toggleSize: 18.0,
+                  value: value,
+                  onToggle: (bool newValue) {
+                    sessionModel.setPaymentTestMode(newValue);
+                  },
+                ),
               );
             }),
           ),
@@ -48,16 +50,19 @@ class DeveloperSettingsTab extends StatelessWidget {
             title: 'Play Version'.i18n,
             child: sessionModel
                 .playVersion((BuildContext context, bool value, Widget? child) {
-              return FlutterSwitch(
-                width: 44.0,
-                height: 24.0,
-                valueFontSize: 12.0,
-                padding: 2,
-                toggleSize: 18.0,
-                value: value,
-                onToggle: (bool newValue) {
-                  sessionModel.setPlayVersion(newValue);
-                },
+              return Padding(
+                padding: const EdgeInsetsDirectional.only(end: 16.0),
+                child: FlutterSwitch(
+                  width: 44.0,
+                  height: 24.0,
+                  valueFontSize: 12.0,
+                  padding: 2,
+                  toggleSize: 18.0,
+                  value: value,
+                  onToggle: (bool newValue) {
+                    sessionModel.setPlayVersion(newValue);
+                  },
+                ),
               );
             }),
           ),
@@ -65,26 +70,29 @@ class DeveloperSettingsTab extends StatelessWidget {
             title: 'Force Country'.i18n,
             child: sessionModel.forceCountry(
                 (BuildContext context, String value, Widget? child) {
-              return DropdownButton<String>(
-                value: value,
-                icon: const Icon(Icons.arrow_downward),
-                iconSize: iconSize,
-                elevation: 16,
-                underline: Container(
-                  height: 2,
-                  color: Colors.deepPurpleAccent,
+              return Padding(
+                padding: const EdgeInsetsDirectional.only(end: 16.0),
+                child: DropdownButton<String>(
+                  value: value,
+                  icon: const Icon(Icons.arrow_downward),
+                  iconSize: iconSize,
+                  elevation: 16,
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String? newValue) {
+                    sessionModel
+                        .setForceCountry(newValue == '' ? null : newValue);
+                  },
+                  items: <String>['', 'CN', 'IR', 'US']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: CText(value, style: tsBody1),
+                    );
+                  }).toList(),
                 ),
-                onChanged: (String? newValue) {
-                  sessionModel
-                      .setForceCountry(newValue == '' ? null : newValue);
-                },
-                items: <String>['', 'CN', 'IR', 'US']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: CText(value, style: tsBody1),
-                  );
-                }).toList(),
               );
             }),
           ),
