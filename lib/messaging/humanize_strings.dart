0583a925786a3a7d10caf54c601c38ce64acf42f
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-22 13:48:31 -0400

    comments

--- lib/messaging/humanize_strings.dart
+++ lib/messaging/humanize_strings.dart
@@ -1,9 +1,11 @@
 import 'messaging.dart';
 
+// TODO: do we need this anymore?
 String sanitizeContactName(String displayName) {
   return displayName.isEmpty ? 'unnamed_contact'.i18n : displayName.toString();
 }
 
+// splits the contactId into more human readable groups of 4 chars
 String humanizeContactId(String id) {
   var humanizedId = id.splitMapJoin(RegExp('.{4}'),
       onMatch: (m) => '${m[0]}', // (or no onMatch at all)
