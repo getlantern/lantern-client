73a728d7d79adb5ae806225ae38fccb736706e40
Ox Cart <ox.to.a.cart@gmail.com>
2021-09-22 11:16:22 -0500

    FocusedMenu comments

--- lib/common/ui/focused_menu.dart
+++ lib/common/ui/focused_menu.dart
@@ -20,7 +20,7 @@ class FocusedMenuItem {
 class FocusedMenuHolder extends StatefulWidget {
   final Widget child;
   final double menuItemExtent;
-  final double? menuWidth;
+  final double menuWidth;
   final List<FocusedMenuItem> menuItems;
   final bool? animateMenuItems;
   final BoxDecoration? menuBoxDecoration;
@@ -46,7 +46,7 @@ class FocusedMenuHolder extends StatefulWidget {
       this.animateMenuItems,
       this.blurSize,
       this.blurBackgroundColor,
-      this.menuWidth,
+      required this.menuWidth,
       this.menuOffset = 0,
       this.paddingTop = 0,
       this.paddingBottom = 0,
@@ -132,7 +132,7 @@ class FocusedMenuDetails extends StatelessWidget {
   final Widget child;
   final bool animateMenu;
   final double? blurSize;
-  final double? menuWidth;
+  final double menuWidth;
   final Color? blurBackgroundColor;
   final double menuOffset;
   final double paddingTop;
@@ -160,19 +160,26 @@ class FocusedMenuDetails extends StatelessWidget {
     var mediaQuery = MediaQuery.of(context);
     var size = mediaQuery.size;
 
-    final maxMenuWidth = menuWidth ?? (size.width * 0.80);
-    final menuX = (childOffset.dx + maxMenuWidth) < size.width
+    final menuX = (childOffset.dx + menuWidth) < size.width
         ? childOffset.dx
-        : (childOffset.dx - maxMenuWidth + childSize!.width);
+        : (childOffset.dx - menuWidth + childSize!.width);
 
-    final menuHeight = menuItems.length * itemExtent;
+    // constrain height of menu to fit within bounds, relying on scrolling list
+    // to handle overflow
+    final maxMenuHeight =
+        size.height - paddingTop - paddingBottom - mediaQuery.viewPadding.top;
+    final menuHeight = min(maxMenuHeight, menuItems.length * itemExtent);
+
+    // always position menu so that we can fit all of it on the screen
     var menuY = max(paddingTop + mediaQuery.viewPadding.top,
         size.height - paddingBottom - menuHeight);
+    // position child above menu, allowing it to overflow off top of screen if
+    // necessary
     var childY = menuY - childSize!.height - menuOffset;
     if (childY > childOffset.dy) {
       // our calculated child position is actually lower than the current position
-      // of the child, adjust it and the menu upwards to keep child in same
-      // position.
+      // of the child, adjust it and the menu upwards to keep child in original
+      // position
       final yAdjustment = childY - childOffset.dy;
       childY = childOffset.dy;
       menuY -= yAdjustment;
@@ -210,7 +217,7 @@ class FocusedMenuDetails extends StatelessWidget {
                 },
                 tween: Tween(begin: 0.0, end: 1.0),
                 child: Container(
-                  width: maxMenuWidth,
+                  width: menuWidth,
                   height: menuHeight,
                   decoration: menuBoxDecoration ??
                       BoxDecoration(
