c37ab28a4af6d0c75d7b057f2bea8724f44f80d8
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-22 22:08:44 -0400

    basic layout, contact + messages results display

--- lib/messaging/custom_search_delegate.dart
+++ lib/messaging/custom_search_delegate.dart
@@ -45,7 +45,8 @@ class CustomSearchDelegate extends SearchDelegate {
         : query.length < 3
             ? Center(child: Text('Please enter at least 3 letters'.i18n))
             : FutureBuilder(
-                future: model.searchContacts(query),
+                future: Future.wait(
+                    [model.searchContacts(query), model.searchMessages(query)]),
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
@@ -55,9 +56,49 @@ class CustomSearchDelegate extends SearchDelegate {
                         // TODO: handle on UI
                         return Center(child: Text('Error: ${snapshot.error}'));
                       } else {
-                        return SuggestedContactList(
-                          suggestedContacts: snapshot.data as List<Contact>,
-                        );
+                        final results = snapshot.data as List;
+
+                        return LayoutBuilder(builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          // scale height to keep line height the same even though font size changed
+                          return Container(
+                            width: constraints.maxWidth,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.only(
+                                      start: 20, top: 20),
+                                  child: Text(
+                                    'Contacts (${results[0].length} results)'
+                                        .i18n
+                                        .toUpperCase(),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: SuggestedContacts(
+                                    contacts: results[0] as List<Contact>,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.only(
+                                      start: 20, top: 20),
+                                  child: Text(
+                                      'Messages (${results[1].length} results)'
+                                          .i18n
+                                          .toUpperCase()),
+                                ),
+                                Flexible(
+                                  child: SuggestedMessages(
+                                    model: model,
+                                    messages: results[1] as List<StoredMessage>,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        });
                       }
                   }
                 });
@@ -65,27 +106,28 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container();
+    return const Center(child: Text('build results'));
   }
 }
 
-class SuggestedContactList extends StatelessWidget {
-  final List<Contact>? suggestedContacts;
+class SuggestedContacts extends StatelessWidget {
+  final List<Contact> contacts;
 
-  const SuggestedContactList({this.suggestedContacts});
+  const SuggestedContacts({required this.contacts});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: suggestedContacts!.length,
+        itemCount: contacts.length,
         physics: defaultScrollPhysics,
         itemBuilder: (context, index) {
-          var contact = suggestedContacts![index];
+          var contact = contacts[index];
           return Column(
             children: [
               ContactListItem(
                 contact: contact,
                 index: index,
+                // TODO: remove * for the avatar
                 leading: CustomAvatar(
                     id: contact.contactId.id, displayName: contact.displayName),
                 title: sanitizeContactName(contact.displayName),
@@ -93,9 +135,52 @@ class SuggestedContactList extends StatelessWidget {
                     .pushRoute(Conversation(contactId: contact.contactId)),
                 trailing: null,
                 disableBorders: true,
+                enableRichText: false,
               ),
             ],
           );
+        });
+    ;
+  }
+}
+
+class SuggestedMessages extends StatelessWidget {
+  final MessagingModel model;
+  final List<StoredMessage> messages;
+
+  const SuggestedMessages({required this.model, required this.messages});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: messages.length,
+        physics: defaultScrollPhysics,
+        itemBuilder: (context, index) {
+          var message = messages[index];
+          return model.singleContactById(context, message.contactId,
+              (context, contact, child) {
+            return Column(
+              children: [
+                ContactListItem(
+                  contact: contact,
+                  index: index,
+                  // TODO: remove * for the avatar
+                  leading: CustomAvatar(
+                      id: message.contactId.id,
+                      displayName: contact.displayName),
+                  title: sanitizeContactName(contact.displayName),
+                  subtitle: CText(
+                      "${contact.mostRecentMessageText.isNotEmpty ? contact.mostRecentMessageText : 'attachment'.i18n}",
+                      style: tsBody2.copiedWith(color: grey5)),
+                  onTap: () async => await context
+                      .pushRoute(Conversation(contactId: message.contactId)),
+                  trailing: null,
+                  disableBorders: true,
+                  enableRichText: false,
+                ),
+              ],
+            );
+          });
         });
     ;
   }
