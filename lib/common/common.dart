212d63212f518dce0f78eaa908945d990350bd84
Kallirroi <kallirroi.retzepi@gmail.com>
2021-09-28 15:29:31 -0400

    custom markdown body

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -86,3 +86,4 @@ export 'ui/custom/text.dart';
 export 'ui/custom/text_field.dart';
 export 'ui/custom/list_tile.dart';
 export 'ui/custom/rounded_rectangle_border.dart';
+export 'ui/custom/markdown_body.dart';
