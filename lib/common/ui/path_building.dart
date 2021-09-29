c3e50c009a3b3391fd2e4a6e60b1a6b017faa834
Ox Cart <ox.to.a.cart@gmail.com>
2021-09-29 05:55:03 -0500

    Correct borders around deleted messages

---
+++ lib/common/ui/path_building.dart
@@ -0,0 +1,20 @@
+import 'package:lantern/common/common.dart';
+
+extension PathBuilding on BorderRadius {
+  /// Creates a path that fills the given size and rounds corners to match this
+  /// BorderRadius.
+  Path toPath(Size size) {
+    return Path()
+      ..moveTo(topLeft.x / 2, 0)
+      ..lineTo(size.width - topRight.x / 2, 0)
+      ..arcToPoint(Offset(size.width, topRight.y / 2), radius: topRight)
+      ..lineTo(size.width, size.height - bottomRight.y / 2)
+      ..arcToPoint(Offset(size.width - bottomRight.x / 2, size.height),
+          radius: bottomRight)
+      ..lineTo(bottomLeft.x / 2, size.height)
+      ..arcToPoint(Offset(0, size.height - bottomLeft.y / 2),
+          radius: bottomLeft)
+      ..lineTo(0, topLeft.y / 2)
+      ..arcToPoint(Offset(topLeft.x / 2, 0), radius: topLeft);
+  }
+}
