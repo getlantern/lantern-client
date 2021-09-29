d068fbb7d320981bf597040fff5dd6d5de0a1d12
Ox Cart <ox.to.a.cart@gmail.com>
2021-09-28 20:05:11 -0500

    Support multiple highlighted fragments in TextHighlighter

--- lib/common/ui/text_highlighter.dart
+++ lib/common/ui/text_highlighter.dart
@@ -13,18 +13,20 @@ class TextHighlighter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final fragments = text.split('**');
+    final children = <TextSpan>[];
+    for (var i = 1; i < fragments.length; i++) {
+      children.add(TextSpan(
+          text: fragments[i],
+          style: i % 2 == 0
+              ? style
+              : style.copiedWith(color: pink4, fontWeight: FontWeight.w500)));
+    }
     return RichText(
         overflow: TextOverflow.ellipsis,
         text: TextSpan(
           text: fragments[0],
           style: style,
-          children: <TextSpan>[
-            TextSpan(
-                text: fragments[1],
-                style: style.copiedWith(
-                    color: pink4, fontWeight: FontWeight.w500)),
-            TextSpan(text: fragments[2], style: style),
-          ],
+          children: children,
         ));
   }
 }
