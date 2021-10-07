fe1e61676294a0eea1b1e5c863c70df02b408eca
Kallirroi <kallirroi.retzepi@gmail.com>
2021-10-07 11:42:09 -0400

    reverted back to native notifs, using custom XML and RemoteViews

--- android/app/src/main/java/org/getlantern/lantern/model/MessagingHolder.kt
+++ android/app/src/main/java/org/getlantern/lantern/model/MessagingHolder.kt
@@ -1,12 +1,16 @@
 package org.getlantern.lantern.model
 
 import android.app.Application
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Build
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import io.lantern.android.model.BaseModel
 import io.lantern.android.model.MessagingModel
@@ -15,11 +19,13 @@ import io.lantern.db.Subscriber
 import io.lantern.messaging.Messaging
 import io.lantern.messaging.Model
 import io.lantern.messaging.Schema
+import io.lantern.messaging.WebRTCSignal
 import io.lantern.messaging.directContactPath
 import io.lantern.messaging.path
 import io.lantern.messaging.tassis.websocket.WebSocketTransportFactory
 import org.getlantern.lantern.MainActivity
 import org.getlantern.lantern.R
+import org.getlantern.lantern.util.Json
 import java.io.File
 import java.util.HashMap
 
@@ -30,6 +36,7 @@ internal const val defaultNotificationChannelId = "default"
 class MessagingHolder {
     lateinit var messaging: Messaging
     private val contactNotificationIds = HashMap<Model.ContactId, Int>()
+    private val callNotificationIds = HashMap<String, Int>()
     private var nextNotificationId = 5000
 
     fun init(application: Application) {
@@ -60,6 +67,19 @@ class MessagingHolder {
                 },
                 receiveInitial = false
             )
+
+            // show notifications for incoming calls
+            messaging.subscribeToWebRTCSignals("systemNotifications") { signal ->
+                val msg = Json.gson.fromJson(signal.content.toString(Charsets.UTF_8), SignalingMessage::class.java)
+                when (msg.type) {
+                    "offer" -> notifyCall(application, notificationManager, signal)
+                    "bye" -> {
+                        callNotificationIds.remove(signal.senderId)?.let { notificationId ->
+                            notificationManager.cancel(notificationId)
+                        }
+                    }
+                }
+            }
         } catch (t: Throwable) {
             throw RuntimeException(t)
         }
@@ -123,4 +143,87 @@ class MessagingHolder {
             }
         }
     }
+
+    private fun notifyCall(
+        application: Application,
+        notificationManager: NotificationManager,
+        signal: WebRTCSignal
+    ) {
+//        if (MainActivity.visible) {
+//            // don't bother notifying if the MainActivity is currently visible, since it has its
+//            // own incoming call notification
+//            return
+//        }
+        val contact =
+            messaging.db.get<Model.Contact>(signal.senderId.directContactPath)
+        contact?.let {
+            var notificationId = callNotificationIds[signal.senderId]
+            if (notificationId == null) {
+                notificationId = nextNotificationId++
+                callNotificationIds[signal.senderId] = notificationId!!
+            }
+            val mainActivityIntent =
+                Intent(application, MainActivity::class.java)
+            mainActivityIntent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+            mainActivityIntent.putExtra(
+                "signal",
+                Json.gson.toJson(signal)
+            )
+            val openMainActivity = PendingIntent.getActivity(
+                application, notificationId!!, mainActivityIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val builder = NotificationCompat.Builder(
+                application,
+                defaultNotificationChannelId
+            )
+
+            val notificationLayout = RemoteViews(application.packageName, R.layout.notification_custom)
+            notificationLayout.setOnClickPendingIntent(R.id.btnAccept, null) // TODO: define acceptIntent
+            notificationLayout.setOnClickPendingIntent(R.id.btnDecline, null) // TODO: define declineIntent
+
+            // set strings in custom notification
+            notificationLayout.setTextViewText(R.id.name, contact.displayName)
+            notificationLayout.setTextViewText(R.id.incomingCall, application.getString(R.string.incoming_call))
+            notificationLayout.setTextViewText(R.id.btnAccept, application.getString(R.string.accept))
+            notificationLayout.setTextViewText(R.id.btnDecline, application.getString(R.string.decline))
+
+            builder.setContentTitle(application.getString(R.string.incoming_call))
+            builder.setContentText(contact.displayName)
+            builder.setSmallIcon(R.drawable.status_on)
+            builder.setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            builder.setCustomContentView(notificationLayout)
+            builder.setOngoing(true)
+            builder.setCategory(NotificationCompat.CATEGORY_CALL)
+            builder.setContentIntent(openMainActivity) // TODO: this needs to change
+
+            val ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                val notificationChannel = NotificationChannel(
+                    callNotificationChannelId,
+                    "CallNotificationChannel",
+                    importance
+                )
+                notificationChannel.enableVibration(true)
+                notificationChannel.enableLights(true)
+                val ringtoneAttrs = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+                notificationChannel.setSound(ringtone, ringtoneAttrs)
+                builder.setChannelId(callNotificationChannelId)
+                notificationManager.createNotificationChannel(
+                    notificationChannel
+                )
+            }
+            builder.setDefaults(Notification.DEFAULT_ALL)
+            builder.setTimeoutAfter(30000)
+            notificationManager.notify(notificationId!!, builder.build())
+        }
+    }
+}
+
+class SignalingMessage {
+    var type: String? = null
 }
