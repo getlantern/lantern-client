2b9d9a12ad72eaa4c05b4db1edc868fe5172728d
Ox Cart <ox.to.a.cart@gmail.com>
2021-10-14 14:58:03 -0500

    Updated to latest messaging-android

--- lib/core/router/tabs/message_tab_router.dart
+++ lib/core/router/tabs/message_tab_router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:lantern/account/messaging/display_name.dart';
 import 'package:lantern/common/ui/transitions.dart';
 import 'package:lantern/messaging/messages.dart';
 
@@ -11,13 +10,6 @@ const message_tab_router = CustomRoute<void>(
     CustomRoute<void>(
         page: Messages,
         path: '',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: DisplayName,
-        name: 'DisplayName',
-        path: 'displayName',
         transitionsBuilder: defaultTransition,
         durationInMilliseconds: defaultTransitionMillis,
         reverseDurationInMilliseconds: defaultTransitionMillis),
