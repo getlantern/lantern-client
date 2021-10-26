eaba343c26ceac1257f2131770dd3b2cdfd7164c
Kallirroi <kallirroi.retzepi@gmail.com>
2021-10-26 16:24:50 -0400

    TextFormField updates

--- lib/messaging/contacts/add_contact_identifier.dart
+++ lib/messaging/contacts/add_contact_identifier.dart
@@ -8,20 +8,13 @@ class AddViaIdentifier extends StatefulWidget {
 class _AddViaIdentifierState extends State<AddViaIdentifier> {
   final _formKey = GlobalKey<FormState>(debugLabel: 'contactIdInput');
   late final contactIdController = CustomTextEditingController(
-      formKey: _formKey,
-      validator: (value) => value == null ||
-              value.isEmpty ||
-              value ==
-                  'Kalli' // TODO: Button should only become active after our minumum username length of 2 character
-          ? 'contact_id_error_description'
-              .i18n // TODO: we should differentiate between messenger ID and username
-          : null);
-
-  var hasError = false;
+      formKey: _formKey, validator: (value) => validateInput(value));
+  var shouldSubmit = false;
 
   @override
   void initState() {
     super.initState();
+    contactIdController.focusNode.requestFocus();
   }
 
   @override
@@ -31,70 +24,87 @@ class _AddViaIdentifierState extends State<AddViaIdentifier> {
   }
 
   void handleButtonPress() async {
-    // checking if the input field is not empty
-    if (_formKey.currentState!.validate()) {
+    contactIdController.focusNode.unfocus();
+    if (_formKey.currentState?.validate() == true) {
       // TODO: talk to model and add contact
-
       // TODO: direct to Conversation view
     }
+  }
+
+  String? validateInput(String? value) {
+    // input is invalid
+    if (value == null ||
+        value.isEmpty ||
+        value.length < 2 ||
+        isValidUsername(value) ||
+        isValidMessengerID(value)) {
+      return 'contact_id_validation_general'.i18n;
+    }
+    // input is valid
+    return null;
+  }
+
+  bool isValidUsername(String value) {
+    var pattern = r'(^[a-z][a-z0-9\-]{2,28}[a-z0-9]$)';
+    var regExp = RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  bool isValidMessengerID(String value) {
+    // TODO: pull isSanitizedContactId from messaging-android
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-        title: 'add_contact'.i18n,
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: Container(
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsetsDirectional.only(
-                                start: 24.0, top: 60, end: 24.0),
-                            child: Wrap(
-                              children: [
-                                CTextField(
-                                  controller: contactIdController,
-                                  autovalidateMode: AutovalidateMode.disabled,
-                                  label: 'contact_id_messenger_id'.i18n,
-                                  prefixIcon: const CAssetImage(
-                                      path: ImagePaths.people),
-                                  hintText: 'contact_id_type'.i18n,
-                                  keyboardType: TextInputType.text,
-                                  maxLines: null,
-                                ),
-                              ],
+      title: 'add_contact'.i18n,
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Form(
+                onChanged: () => setState(
+                    () => shouldSubmit = _formKey.currentState!.validate()),
+                key: _formKey,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.only(
+                            start: 24.0, top: 60, end: 24.0),
+                        child: Wrap(
+                          children: [
+                            CTextField(
+                              controller: contactIdController,
+                              autovalidateMode: AutovalidateMode.always,
+                              label: 'contact_id_messenger_id'.i18n,
+                              prefixIcon:
+                                  const CAssetImage(path: ImagePaths.people),
+                              hintText: 'contact_id_type'.i18n,
+                              keyboardType: TextInputType.text,
+                              maxLines: null,
                             ),
-                          )
-                        ]),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsetsDirectional.only(bottom: 32),
-                  child: Button(
-                    width: 200,
-                    text: 'start_chat'.i18n,
-                    onPressed: () {
-                      handleButtonPress();
-                      FocusScope.of(context).unfocus();
-                    },
-                    // disabled: _formKey.currentState!.validate(),
-                    disabled: hasError || _formKey.currentState!.validate(),
-                  ),
-                ),
-              ],
+                          ],
+                        ),
+                      )
+                    ]),
+              ),
             ),
-          ),
-        ));
+            Container(
+              margin: const EdgeInsetsDirectional.only(bottom: 32),
+              child: Button(
+                width: 200,
+                text: 'start_chat'.i18n,
+                onPressed: () => handleButtonPress(),
+                disabled: !shouldSubmit,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
