b5f326122be610d810c3571cc572f97c2a5d25e8
Kallirroi <kallirroi.retzepi@gmail.com>
2021-11-09 18:32:47 -0500

    actually commit onboarding_router

--- lib/core/router/router.gr.dart
+++ lib/core/router/router.gr.dart
@@ -24,6 +24,7 @@ import 'package:lantern/account/language.dart' as _i18;
 import 'package:lantern/account/recovery_key.dart' as _i23;
 import 'package:lantern/account/secure_chat_number_account.dart' as _i24;
 import 'package:lantern/account/settings.dart' as _i17;
+import 'package:lantern/common/common.dart' as _i27;
 import 'package:lantern/common/ui/full_screen_dialog.dart' as _i2;
 import 'package:lantern/home.dart' as _i1;
 import 'package:lantern/messaging/chats.dart' as _i7;
@@ -33,13 +34,13 @@ import 'package:lantern/messaging/contacts/new_chat.dart' as _i10;
 import 'package:lantern/messaging/conversation/conversation.dart' as _i8;
 import 'package:lantern/messaging/introductions/introduce.dart' as _i12;
 import 'package:lantern/messaging/introductions/introductions.dart' as _i13;
-import 'package:lantern/messaging/messaging_model.dart' as _i28;
+import 'package:lantern/messaging/messaging_model.dart' as _i29;
 import 'package:lantern/messaging/onboarding/secure_chat_number_messaging.dart'
-    as _i5;
-import 'package:lantern/messaging/onboarding/secure_chat_number_recovery.dart'
     as _i6;
+import 'package:lantern/messaging/onboarding/secure_chat_number_recovery.dart'
+    as _i5;
 import 'package:lantern/messaging/onboarding/welcome.dart' as _i4;
-import 'package:lantern/messaging/protos_flutteronly/messaging.pb.dart' as _i27;
+import 'package:lantern/messaging/protos_flutteronly/messaging.pb.dart' as _i28;
 import 'package:lantern/vpn/vpn_tab.dart' as _i14;
 
 class AppRouter extends _i3.RootStackRouter {
@@ -109,20 +110,20 @@ class AppRouter extends _i3.RootStackRouter {
           opaque: true,
           barrierDismissible: false);
     },
-    SecureChatNumberMessaging.name: (routeData) {
+    SecureNumberRecovery.name: (routeData) {
       return _i3.CustomPage<void>(
           routeData: routeData,
-          child: _i5.SecureChatNumberMessaging(),
+          child: _i5.SecureNumberRecovery(),
           transitionsBuilder: _i3.TransitionsBuilders.fadeIn,
           durationInMilliseconds: 200,
           reverseDurationInMilliseconds: 200,
           opaque: true,
           barrierDismissible: false);
     },
-    SecureNumberRecovery.name: (routeData) {
+    SecureChatNumberMessaging.name: (routeData) {
       return _i3.CustomPage<void>(
           routeData: routeData,
-          child: _i6.SecureNumberRecovery(),
+          child: _i6.SecureChatNumberMessaging(),
           transitionsBuilder: _i3.TransitionsBuilders.fadeIn,
           durationInMilliseconds: 200,
           reverseDurationInMilliseconds: 200,
@@ -357,11 +358,12 @@ class AppRouter extends _i3.RootStackRouter {
                     fullMatch: true),
                 _i3.RouteConfig(Welcome.name,
                     path: 'welcome', parent: OnboardingRouter.name),
+                _i3.RouteConfig(SecureNumberRecovery.name,
+                    path: 'secureNumberRecovery',
+                    parent: OnboardingRouter.name),
                 _i3.RouteConfig(SecureChatNumberMessaging.name,
                     path: 'secureChatNumberMessaging',
-                    parent: OnboardingRouter.name),
-                _i3.RouteConfig(SecureNumberRecovery.name,
-                    path: 'secureNumberRecovery', parent: OnboardingRouter.name)
+                    parent: OnboardingRouter.name)
               ]),
           _i3.RouteConfig(MessagesRouter.name,
               path: 'messages',
@@ -433,7 +435,7 @@ class AppRouter extends _i3.RootStackRouter {
 
 /// generated route for [_i1.HomePage]
 class Home extends _i3.PageRouteInfo<HomeArgs> {
-  Home({_i26.Key? key, List<_i3.PageRouteInfo>? children})
+  Home({_i27.Key? key, List<_i3.PageRouteInfo>? children})
       : super(name,
             path: '/', args: HomeArgs(key: key), initialChildren: children);
 
@@ -443,12 +445,12 @@ class Home extends _i3.PageRouteInfo<HomeArgs> {
 class HomeArgs {
   const HomeArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i2.FullScreenDialog]
 class FullScreenDialogPage extends _i3.PageRouteInfo<FullScreenDialogPageArgs> {
-  FullScreenDialogPage({required _i26.Widget widget, _i26.Key? key})
+  FullScreenDialogPage({required _i27.Widget widget, _i27.Key? key})
       : super(name,
             path: 'fullScreenDialogPage',
             args: FullScreenDialogPageArgs(widget: widget, key: key));
@@ -459,9 +461,9 @@ class FullScreenDialogPage extends _i3.PageRouteInfo<FullScreenDialogPageArgs> {
 class FullScreenDialogPageArgs {
   const FullScreenDialogPageArgs({required this.widget, this.key});
 
-  final _i26.Widget widget;
+  final _i27.Widget widget;
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i3.EmptyRouterPage]
@@ -511,19 +513,19 @@ class Welcome extends _i3.PageRouteInfo<void> {
   static const String name = 'Welcome';
 }
 
-/// generated route for [_i5.SecureChatNumberMessaging]
+/// generated route for [_i5.SecureNumberRecovery]
+class SecureNumberRecovery extends _i3.PageRouteInfo<void> {
+  const SecureNumberRecovery() : super(name, path: 'secureNumberRecovery');
+
+  static const String name = 'SecureNumberRecovery';
+}
+
+/// generated route for [_i6.SecureChatNumberMessaging]
 class SecureChatNumberMessaging extends _i3.PageRouteInfo<void> {
   const SecureChatNumberMessaging()
       : super(name, path: 'secureChatNumberMessaging');
 
   static const String name = 'SecureChatNumberMessaging';
-}
-
-/// generated route for [_i6.SecureNumberRecovery]
-class SecureNumberRecovery extends _i3.PageRouteInfo<void> {
-  const SecureNumberRecovery() : super(name, path: 'secureNumberRecovery');
-
-  static const String name = 'SecureNumberRecovery';
 }
 
 /// generated route for [_i7.Chats]
@@ -535,7 +537,7 @@ class Chats extends _i3.PageRouteInfo<void> {
 
 /// generated route for [_i8.Conversation]
 class Conversation extends _i3.PageRouteInfo<ConversationArgs> {
-  Conversation({required _i27.ContactId contactId, int? initialScrollIndex})
+  Conversation({required _i28.ContactId contactId, int? initialScrollIndex})
       : super(name,
             path: 'conversation',
             args: ConversationArgs(
@@ -547,7 +549,7 @@ class Conversation extends _i3.PageRouteInfo<ConversationArgs> {
 class ConversationArgs {
   const ConversationArgs({required this.contactId, this.initialScrollIndex});
 
-  final _i27.ContactId contactId;
+  final _i28.ContactId contactId;
 
   final int? initialScrollIndex;
 }
@@ -555,7 +557,7 @@ class ConversationArgs {
 /// generated route for [_i9.ContactInfo]
 class ContactInfo extends _i3.PageRouteInfo<ContactInfoArgs> {
   ContactInfo(
-      {required _i28.MessagingModel model, required _i27.Contact contact})
+      {required _i29.MessagingModel model, required _i28.Contact contact})
       : super(name,
             path: 'contactInfo',
             args: ContactInfoArgs(model: model, contact: contact));
@@ -566,9 +568,9 @@ class ContactInfo extends _i3.PageRouteInfo<ContactInfoArgs> {
 class ContactInfoArgs {
   const ContactInfoArgs({required this.model, required this.contact});
 
-  final _i28.MessagingModel model;
+  final _i29.MessagingModel model;
 
-  final _i27.Contact contact;
+  final _i28.Contact contact;
 }
 
 /// generated route for [_i10.NewChat]
@@ -601,7 +603,7 @@ class Introductions extends _i3.PageRouteInfo<void> {
 
 /// generated route for [_i14.VPNTab]
 class Vpn extends _i3.PageRouteInfo<VpnArgs> {
-  Vpn({_i26.Key? key}) : super(name, path: '', args: VpnArgs(key: key));
+  Vpn({_i27.Key? key}) : super(name, path: '', args: VpnArgs(key: key));
 
   static const String name = 'Vpn';
 }
@@ -609,7 +611,7 @@ class Vpn extends _i3.PageRouteInfo<VpnArgs> {
 class VpnArgs {
   const VpnArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i15.AccountTab]
@@ -621,7 +623,7 @@ class Account extends _i3.PageRouteInfo<void> {
 
 /// generated route for [_i16.AccountManagement]
 class AccountManagement extends _i3.PageRouteInfo<AccountManagementArgs> {
-  AccountManagement({_i26.Key? key, required bool isPro})
+  AccountManagement({_i27.Key? key, required bool isPro})
       : super(name,
             path: 'accountManagement',
             args: AccountManagementArgs(key: key, isPro: isPro));
@@ -632,14 +634,14 @@ class AccountManagement extends _i3.PageRouteInfo<AccountManagementArgs> {
 class AccountManagementArgs {
   const AccountManagementArgs({this.key, required this.isPro});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 
   final bool isPro;
 }
 
 /// generated route for [_i17.Settings]
 class Settings extends _i3.PageRouteInfo<SettingsArgs> {
-  Settings({_i26.Key? key})
+  Settings({_i27.Key? key})
       : super(name, path: 'settings', args: SettingsArgs(key: key));
 
   static const String name = 'Settings';
@@ -648,12 +650,12 @@ class Settings extends _i3.PageRouteInfo<SettingsArgs> {
 class SettingsArgs {
   const SettingsArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i18.Language]
 class Language extends _i3.PageRouteInfo<LanguageArgs> {
-  Language({_i26.Key? key})
+  Language({_i27.Key? key})
       : super(name, path: 'language', args: LanguageArgs(key: key));
 
   static const String name = 'Language';
@@ -662,12 +664,12 @@ class Language extends _i3.PageRouteInfo<LanguageArgs> {
 class LanguageArgs {
   const LanguageArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i19.AuthorizeDeviceForPro]
 class AuthorizePro extends _i3.PageRouteInfo<AuthorizeProArgs> {
-  AuthorizePro({_i26.Key? key})
+  AuthorizePro({_i27.Key? key})
       : super(name, path: 'authorizePro', args: AuthorizeProArgs(key: key));
 
   static const String name = 'AuthorizePro';
@@ -676,12 +678,12 @@ class AuthorizePro extends _i3.PageRouteInfo<AuthorizeProArgs> {
 class AuthorizeProArgs {
   const AuthorizeProArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i20.AuthorizeDeviceViaEmail]
 class AuthorizeDeviceEmail extends _i3.PageRouteInfo<AuthorizeDeviceEmailArgs> {
-  AuthorizeDeviceEmail({_i26.Key? key})
+  AuthorizeDeviceEmail({_i27.Key? key})
       : super(name,
             path: 'authorizeDeviceEmail',
             args: AuthorizeDeviceEmailArgs(key: key));
@@ -692,13 +694,13 @@ class AuthorizeDeviceEmail extends _i3.PageRouteInfo<AuthorizeDeviceEmailArgs> {
 class AuthorizeDeviceEmailArgs {
   const AuthorizeDeviceEmailArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i21.AuthorizeDeviceViaEmailPin]
 class AuthorizeDeviceEmailPin
     extends _i3.PageRouteInfo<AuthorizeDeviceEmailPinArgs> {
-  AuthorizeDeviceEmailPin({_i26.Key? key})
+  AuthorizeDeviceEmailPin({_i27.Key? key})
       : super(name,
             path: 'authorizeDeviceEmailPin',
             args: AuthorizeDeviceEmailPinArgs(key: key));
@@ -709,12 +711,12 @@ class AuthorizeDeviceEmailPin
 class AuthorizeDeviceEmailPinArgs {
   const AuthorizeDeviceEmailPinArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i22.ApproveDevice]
 class ApproveDevice extends _i3.PageRouteInfo<ApproveDeviceArgs> {
-  ApproveDevice({_i26.Key? key})
+  ApproveDevice({_i27.Key? key})
       : super(name, path: 'approveDevice', args: ApproveDeviceArgs(key: key));
 
   static const String name = 'ApproveDevice';
@@ -723,12 +725,12 @@ class ApproveDevice extends _i3.PageRouteInfo<ApproveDeviceArgs> {
 class ApproveDeviceArgs {
   const ApproveDeviceArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i23.RecoveryKey]
 class RecoveryKey extends _i3.PageRouteInfo<RecoveryKeyArgs> {
-  RecoveryKey({_i26.Key? key})
+  RecoveryKey({_i27.Key? key})
       : super(name, path: 'recoveryKey', args: RecoveryKeyArgs(key: key));
 
   static const String name = 'RecoveryKey';
@@ -737,7 +739,7 @@ class RecoveryKey extends _i3.PageRouteInfo<RecoveryKeyArgs> {
 class RecoveryKeyArgs {
   const RecoveryKeyArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
 
 /// generated route for [_i24.SecureChatNumberAccount]
@@ -750,7 +752,7 @@ class SecureChatNumberAccount extends _i3.PageRouteInfo<void> {
 
 /// generated route for [_i25.DeveloperSettingsTab]
 class DeveloperSettings extends _i3.PageRouteInfo<DeveloperSettingsArgs> {
-  DeveloperSettings({_i26.Key? key})
+  DeveloperSettings({_i27.Key? key})
       : super(name, path: '', args: DeveloperSettingsArgs(key: key));
 
   static const String name = 'DeveloperSettings';
@@ -759,5 +761,5 @@ class DeveloperSettings extends _i3.PageRouteInfo<DeveloperSettingsArgs> {
 class DeveloperSettingsArgs {
   const DeveloperSettingsArgs({this.key});
 
-  final _i26.Key? key;
+  final _i27.Key? key;
 }
