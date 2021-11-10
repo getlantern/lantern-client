98db32618e67b8cd5e0c1ecc04a9e7e01eca6e69
Kallirroi <kallirroi.retzepi@gmail.com>
2021-11-10 15:02:38 -0500

    fix for Conversation keyboard height

--- lib/core/router/tabs/message_tab_router.dart
+++ lib/core/router/tabs/message_tab_router.dart
@@ -1,12 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:lantern/common/ui/transitions.dart';
 import 'package:lantern/messaging/chats.dart';
-import 'package:lantern/messaging/contacts/contact_info.dart';
-import 'package:lantern/messaging/contacts/new_chat.dart';
-import 'package:lantern/messaging/conversation/conversation.dart';
-import 'package:lantern/messaging/introductions/introduce.dart';
-import 'package:lantern/messaging/introductions/introductions.dart';
-import 'package:lantern/messaging/contacts/add_contact_number.dart';
 
 const message_tab_router = CustomRoute<void>(
   page: EmptyRouterPage,
@@ -16,50 +10,7 @@ const message_tab_router = CustomRoute<void>(
     CustomRoute<void>(
         initial: true,
         page: Chats,
-        name: 'Chats',
-        path: 'chats',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: Conversation,
-        name: 'Conversation',
-        path: 'conversation',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: ContactInfo,
-        name: 'ContactInfo',
-        path: 'contactInfo',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: NewChat,
-        name: 'NewChat',
-        path: 'newChat',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: AddViaChatNumber,
-        name: 'AddViaChatNumber',
-        path: 'addViaChatNumber',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: Introduce,
-        name: 'Introduce',
-        path: 'introduce',
-        transitionsBuilder: defaultTransition,
-        durationInMilliseconds: defaultTransitionMillis,
-        reverseDurationInMilliseconds: defaultTransitionMillis),
-    CustomRoute<void>(
-        page: Introductions,
-        name: 'Introductions',
-        path: 'introductions',
+        path: '',
         transitionsBuilder: defaultTransition,
         durationInMilliseconds: defaultTransitionMillis,
         reverseDurationInMilliseconds: defaultTransitionMillis),
