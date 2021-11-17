a034b1d37e629e33724a2c3ce0ff05884a6c4fad
Kallirroi <kallirroi.retzepi@gmail.com>
2021-11-17 17:21:09 -0500

    filter out intros from unaccepted contacts when showing pending introductions

--- lib/messaging/introductions/introduction_extension.dart
+++ lib/messaging/introductions/introduction_extension.dart
@@ -4,7 +4,18 @@ import 'package:lantern/messaging/messaging.dart';
 extension IntroductionExtension on Iterable<PathAndValue<StoredMessage>> {
   Iterable<PathAndValue<StoredMessage>> getPending() {
     return [...this].toList().where((intro) =>
-        intro.value.introduction.status ==
-        IntroductionDetails_IntroductionStatus.PENDING);
+        intro.value.introduction.isPending() &&
+        intro.value.introduction.constrainedVerificationLevel !=
+            VerificationLevel.UNACCEPTED); // get Pending from accepted contacts
+  }
+}
+
+extension IntroductionStatus on IntroductionDetails {
+  bool isPending() {
+    return status == IntroductionDetails_IntroductionStatus.PENDING;
+  }
+
+  bool isAccepted() {
+    return status == IntroductionDetails_IntroductionStatus.ACCEPTED;
   }
 }
