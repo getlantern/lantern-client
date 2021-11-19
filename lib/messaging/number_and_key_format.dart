89181a41f35fd4c8543a3f3298c2a7aa577fce63
Ox Cart <ox.to.a.cart@gmail.com>
2021-11-18 20:20:59 -0600

    Improved formatting of recovery key entry

--- lib/messaging/number_and_key_format.dart
+++ lib/messaging/number_and_key_format.dart
@@ -1,10 +1,12 @@
 final shortChatNumberRegexOutside = RegExp(r'^(5*)([0-9]*)(5*)$');
 final shortChatNumberRegexInside = RegExp(r'([0-9]{3})([0-9]{1,2}$)?');
+final fullRecoveryKeyRegex = RegExp(r'^([a-zA-Z0-9]{4})*([a-zA-Z0-9]{1,3}$)?$');
+final recoveryKeyRegex = RegExp(r'([a-zA-Z0-9]{4})([a-zA-Z0-9]{1,3}$)?');
 final whitespace = RegExp(r'\s');
 final nonNumeric = RegExp(r'[^0-9]');
 final sep = ' ';
 
-extension ChatNumberFormat on String {
+extension NumberAndKeyFormat on String {
   /// Returns a readable formatted version of this chat number, for example:
   ///   123467890123     -> 123 468 890 123
   ///   5512346789012355 -> 55 123 468 890 123 55
@@ -37,6 +39,31 @@ extension ChatNumberFormat on String {
       result.write(sep);
       result.write(outsideMatch.group(3));
     }
+    return result.toString();
+  }
+
+  /// Returns a readable formatted version of this recovery key, for example:
+  ///   abcd1234efgh7890ij -> abcd 1234 efgh 7890 ij
+  String get formattedRecoveryKey {
+    final unformatted = withoutWhitespace;
+    if (fullRecoveryKeyRegex.stringMatch(unformatted) == null) {
+      return unformatted;
+    }
+
+    final matches = recoveryKeyRegex.allMatches(unformatted);
+    if (matches.isEmpty) {
+      return unformatted;
+    }
+
+    final result = StringBuffer();
+    matches.forEach((insideMatch) {
+      if (result.isNotEmpty) result.write(sep);
+      result.write(insideMatch.group(1));
+      if (insideMatch.group(2) != null) {
+        result.write(sep);
+        result.write(insideMatch.group(2));
+      }
+    });
     return result.toString();
   }
 
