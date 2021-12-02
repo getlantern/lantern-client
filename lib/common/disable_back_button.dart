04b5b23fe595fc818f224be0074743dec543f1e1
Ox Cart <ox.to.a.cart@gmail.com>
2021-12-02 06:29:36 -0600

    Comment fix

--- lib/common/disable_back_button.dart
+++ lib/common/disable_back_button.dart
@@ -1,7 +1,7 @@
 import 'common.dart';
 
 /// Disables the system back button. This is often called inside of
-/// [State.initState].Make sure to call [enableBackButton] when you want to
+/// [State.initState]. Make sure to call [enableBackButton] when you want to
 /// allow the back button to work again, for example in [State.dispose].
 void disableBackButton() {
   BackButtonInterceptor.add(_doNotGoBack);
