9242816ade3d690d4af8ea45951ef23a9619845e
Kallirroi <kallirroi.retzepi@gmail.com>
2021-12-08 13:48:29 -0500

    moved reset functionality to DEV tab

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -39,7 +39,6 @@ export 'once.dart';
 export 'session_model.dart';
 export 'single_value_subscriber.dart';
 export 'disable_back_button.dart';
-export 'dev_reset_all_flags.dart';
 
 // UI elements
 export 'ui/audio.dart';
