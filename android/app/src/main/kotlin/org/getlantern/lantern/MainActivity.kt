d5632cb726e5eb88ff84133bfcebb64575a170b1
Ox Cart <ox.to.a.cart@gmail.com>
2021-12-09 10:21:15 -0600

    Switched to Android DownloadManager for tracking download progress

--- android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
@@ -69,7 +69,7 @@ class MainActivity : FlutterActivity(), MethodChannel.MethodCallHandler {
         messagingModel = MessagingModel(this, flutterEngine, LanternApp.messaging.messaging)
         vpnModel = VpnModel(flutterEngine, ::switchLantern)
         sessionModel = SessionModel(this, flutterEngine)
-        replicaModel = ReplicaModel(flutterEngine)
+        replicaModel = ReplicaModel(this, flutterEngine)
         navigator = Navigator(this, flutterEngine)
         eventManager = object : EventManager("lantern_event_channel", flutterEngine) {
             override fun onListen(event: Event) {
