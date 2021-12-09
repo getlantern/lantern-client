044e2805d380a774761f04643fc625c600bdd3f2
Ox Cart <ox.to.a.cart@gmail.com>
2021-12-08 22:09:14 -0600

    Updated layout logic for introducing images

--- lib/common/ui/custom/asset_image.dart
+++ lib/common/ui/custom/asset_image.dart
@@ -3,18 +3,25 @@ import 'package:lantern/common/common.dart';
 class CAssetImage extends StatelessWidget {
   final String path;
   final double size;
+  final double? width;
+  final double? height;
   final Color? color;
 
   const CAssetImage(
-      {required this.path, this.size = iconSize, this.color, Key? key})
+      {required this.path,
+      this.size = iconSize,
+      this.width,
+      this.height,
+      this.color,
+      Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
       path,
-      height: size,
-      width: size,
+      height: height ?? size,
+      width: width ?? size,
       color: color,
     );
   }
