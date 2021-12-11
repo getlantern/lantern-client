e63a28fdd6bc949efb884027f8f1958149b80c94
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2021-12-11 17:14:48 +0100

    [assets] Commit Replica assets

--- lib/common/ui/image_paths.dart
+++ lib/common/ui/image_paths.dart
@@ -57,6 +57,19 @@ class ImagePaths {
       'assets/images/sticker_figure_foreground.svg';
   static const timer = 'assets/images/timer.svg';
 
+  // Replica
+  static const lantern_logo = 'assets/images/lantern_logo.svg';
+  static const discover = 'assets/images/discover.svg';
+  static const audio = 'assets/images/audio_black.svg';
+  static const doc = 'assets/images/doc_black.svg';
+  static const spreadsheet = 'assets/images/spreadsheet_black.svg';
+  static const share = 'assets/images/share.svg';
+  static const file_download = 'assets/images/file_download.svg';
+  static const pause = 'assets/images/pause.svg';
+  static const play = 'assets/images/play_arrow.svg';
+  static const fast_forward = 'assets/images/fast_forward.svg';
+  static const fast_rewind = 'assets/images/fast_rewind.svg';
+
   // Commonly used
   static const content_copy = 'assets/images/content_copy.svg';
   static const check_green = 'assets/images/check_green.svg';
