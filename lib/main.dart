de7e4cc0e50282c14b4edb21f868fe7870f0b84e
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2021-12-11 17:25:59 +0100

    [replica:api] Use DownloadManager for downloading
    Also, use Dio for all HTTP work.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:lantern/catcher_setup.dart';
 import 'package:lantern/replica/logic/common.dart';
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(debug: true);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await ReplicaCommon.init();
   setupCatcherAndRun(LanternApp());
--- lib/replica/logic/api.dart
+++ lib/replica/logic/api.dart
@@ -1,13 +1,16 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:android_path_provider/android_path_provider.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:lantern/replica/logic/replica_link.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:lantern/messaging/notifications.dart';
+import 'package:lantern/replica/models/replica_link.dart';
 import 'package:lantern/vpn/vpn.dart';
 import 'dart:typed_data';
-import 'package:http/http.dart' as http;
 import 'package:open_file/open_file.dart';
-import 'package:path/path.dart' as path;
 import 'package:lantern/replica/models/search_item.dart';
-import 'package:lantern/replica/ui/searchcategory.dart';
+import 'package:lantern/replica/models/searchcategory.dart';
 import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -16,8 +19,8 @@ var logger = Logger(
   printer: PrettyPrinter(),
 );
 
-// Metadata of a video: it should accept null values since sometimes a
-// thumbnail request succeeds, but not a duration request, and vice versa
+// Metadata of a video: it should accept null values since sometimes a thumbnail
+// request succeeds, but not a duration request, and vice versa
 class Metadata {
   Metadata(this.duration, this.thumbnail);
   double? duration;
@@ -25,7 +28,17 @@ class Metadata {
 }
 
 class ReplicaApi {
-  ReplicaApi(this.replicaHostAddr);
+  ReplicaApi(this.replicaHostAddr) {
+    dio = Dio(BaseOptions(
+      baseUrl: 'http://$replicaHostAddr/replica/',
+      connectTimeout: 10000, // 10s
+    ));
+  }
+  late Dio dio;
+  final _downloaderCallbackPort = ReceivePort();
+  static final _downloadeManagerIsolateName = 'downloader_send_port';
+  // This **must** be initialized from bindDownloaderBackgroundIsolate()
+  late Function() _onDownloadError;
   final String replicaHostAddr;
   final _defaultTimeoutDuration = const Duration(seconds: 7);
   final notifications = Notifications((payloadString) {
@@ -38,64 +51,58 @@ class ReplicaApi {
     }
     OpenFile.open(payload.data);
   });
-  final Dio dio = Dio();
 
   Future<List<ReplicaSearchItem>> search(
       String query, SearchCategory category, int page, String lang) async {
     logger.v('XXX ReplicaApi.search()');
     var s = '';
     switch (category) {
-      case SearchCategory.Web:
-        s = 'http://$replicaHostAddr/replica/search/serp_web?s=$query&offset=$page&lang=$lang';
-        break;
       case SearchCategory.Video:
       case SearchCategory.Audio:
       case SearchCategory.Image:
       case SearchCategory.Document:
       case SearchCategory.App:
-        s = 'http://$replicaHostAddr/replica/search?s=$query&offset=$page&orderBy=relevance&lang=$lang&type=${category.mimeTypes()}';
+        s = 'search?s=$query&offset=$page&orderBy=relevance&lang=$lang&type=${category.mimeTypes()}';
         break;
       case SearchCategory.Unknown:
         throw Exception('Unknown category. Should never be triggered');
     }
     logger.v('XXX _search(): uri: ${Uri.parse(s)}');
-    final resp = await http.get(Uri.parse(s));
+
+    final resp = await dio.get(s);
     if (resp.statusCode == 200) {
       logger.v('XXX Statuscode: ${resp.statusCode}');
-      // logger.v('body: ${resp.body}');
-      return ReplicaSearchItem.fromJson(category, resp.body);
+      logger.v('XXX body: ${resp.data.toString()}');
+      return ReplicaSearchItem.fromJson(category, resp.data);
     } else {
       throw Exception(
-          'Failed to fetch search query: ${resp.statusCode} -> ${resp.body}');
+          'Failed to fetch search query: ${resp.statusCode} -> ${resp.data.toString()}');
     }
   }
 
-  String getThumbnailAddr(ReplicaLink replicaLink, {String? overrideHostAddr}) {
-    var hostAddr = overrideHostAddr ?? replicaHostAddr;
-    return 'http://$hostAddr/replica/thumbnail?replicaLink=${replicaLink.toMagnetLink()}';
+  String getThumbnailAddr(ReplicaLink replicaLink) {
+    return 'http://$replicaHostAddr/replica/thumbnail?replicaLink=${replicaLink.toMagnetLink()}';
   }
 
-  String getViewAddr(ReplicaLink replicaLink, {String? overrideHostAddr}) {
-    var hostAddr = overrideHostAddr ?? replicaHostAddr;
-    return 'http://$hostAddr/replica/view?link=${replicaLink.toMagnetLink()}';
+  String getDownloadAddr(ReplicaLink replicaLink) {
+    return 'http://$replicaHostAddr/replica/download?link=${replicaLink.toMagnetLink()}';
   }
 
-  String getDownloadAddr(ReplicaLink replicaLink, {String? overrideHostAddr}) {
-    var hostAddr = overrideHostAddr ?? replicaHostAddr;
-    return 'http://$hostAddr/replica/download?link=${replicaLink.toMagnetLink()}';
+  String getViewAddr(ReplicaLink replicaLink) {
+    return 'http://$replicaHostAddr/replica/view?link=${replicaLink.toMagnetLink()}';
   }
 
   Future<SearchCategory> fetchCategoryFromReplicaLink(
       ReplicaLink replicaLink) async {
-    var u = Uri.parse(getDownloadAddr(replicaLink));
+    var u = 'download?link=${replicaLink.toMagnetLink()}';
     logger.v('XXX fetchCategoryFromReplicaLink: $u');
 
     try {
-      var resp = await http.head(u).timeout(_defaultTimeoutDuration);
+      var resp = await dio.head(u).timeout(_defaultTimeoutDuration);
       if (resp.statusCode != 200) {
         throw Exception('fetching category from $u');
       }
-      return SearchCategoryFromContentType(resp.headers['content-type']);
+      return SearchCategoryFromContentType(resp.headers.value('content-type'));
     } on TimeoutException catch (_) {
       // On a timeout, just return an unknown category
       return SearchCategory.Unknown;
@@ -104,117 +111,111 @@ class ReplicaApi {
 
   // overrideHostAddr is used mainly for testing and overriding a specific
   // usage of host addr
-  Future<Uint8List> fetchThumbnail(ReplicaLink replicaLink,
-      {String? overrideHostAddr}) async {
-    var hostAddr = overrideHostAddr ?? replicaHostAddr;
-    var uri = Uri.parse(
-        'http://$hostAddr/replica/thumbnail?replicaLink=${replicaLink.toMagnetLink()}');
-    logger.v('XXX Thumbnail request uri: $uri');
-    final thumbnailResp = await http.get(uri);
-    if (thumbnailResp.statusCode != 200) {
-      throw Exception(
-          'fetch search query: ${thumbnailResp.statusCode} -> ${thumbnailResp.body}');
-    }
-    final thumbnail = thumbnailResp.bodyBytes;
-    logger.v('XXX Thumbnail request success: ${thumbnail.length}');
-    return thumbnail;
-  }
-
-  // overrideHostAddr is used mainly for testing and overriding a specific
-  // usage of host addr
-  Future<double> fetchDuration(ReplicaLink replicaLink,
-      {String? overrideHostAddr}) async {
-    var hostAddr = overrideHostAddr ?? replicaHostAddr;
-    var uri = Uri.parse(
-        'http://$hostAddr/replica/duration?replicaLink=${replicaLink.toMagnetLink()}');
-    logger.v('XXX Duration request uri: $uri');
-    final durationResp = await http.get(uri);
+  Future<double> fetchDuration(ReplicaLink replicaLink) async {
+    var s = 'duration?replicaLink=${replicaLink.toMagnetLink()}';
+    logger.v('XXX Duration request uri: $s');
+    final durationResp = await dio.get(s);
     if (durationResp.statusCode != 200) {
       throw Exception(
-          'fetch duration: ${durationResp.statusCode} -> ${durationResp.body}');
+          'fetch duration: ${durationResp.statusCode} -> ${durationResp.data.toString()}');
     }
     var duration = 0.0;
     try {
-      duration = double.parse(durationResp.body);
+      duration = double.parse(durationResp.data.toString());
     } catch (err) {
       throw Exception(
-          'parse duration: ${durationResp.statusCode} -> ${durationResp.body}');
+          'parse duration: ${durationResp.statusCode} -> ${durationResp.data.toString()}');
     }
     logger.v('XXX Duration request success: $duration');
     return duration;
   }
 
-  Future<void> download(ReplicaLink link, String displayName) async {
-    logger.v('Download()');
+  Future<String?> _prepAndFetchDownloadsDir() async {
     var hasPermission = await Permission.storage.request().isGranted;
     if (!hasPermission) {
-      throw Exception('Permission');
+      logger.w('Failed to get read/write storage permission');
+      return null;
     }
-    logger.v('Permission granted');
-    final dir = await getExternalStorageDirectory();
-    if (dir == null) {
-      throw Exception('downloads directory');
+
+    try {
+      return await AndroidPathProvider.downloadsPath;
+    } catch (e) {
+      final d = await getExternalStorageDirectory();
+      return d?.path;
     }
-    final savePath = path.join(dir.path, displayName);
-    logger.v('savePath: $savePath');
-    final u = getDownloadAddr(link);
-    logger.v('XXX downloadAddr: $u');
-    final resp = await dio
-        .download(u, savePath, options: Options(sendTimeout: 10000),
-            onReceiveProgress: (received, total) async {
-      logger.v('received: $received | total: $total');
-      if (total == -1) {
-        return;
-      }
-      await notifications.flutterLocalNotificationsPlugin.show(
-        0,
-        'Lantern',
-        'Downloading "$displayName" ...',
-        NotificationDetails(
-            android: AndroidNotificationDetails(
-                'progress channel', 'progress channel',
-                channelDescription: 'progress channel description',
-                channelShowBadge: false,
-                importance: Importance.max,
-                priority: Priority.high,
-                onlyAlertOnce: true,
-                showProgress: true,
-                maxProgress: 100,
-                progress: (received / total * 100).toInt())),
-      );
-    });
-    logger.v('resp done: ${resp.statusCode}');
-    if (resp.statusCode != 200) {
-      await notifications.flutterLocalNotificationsPlugin
-          .show(0, 'Lantern', 'FAILED', notifications.downloadChannel);
-      throw Exception('download failed');
-    }
-    await notifications.flutterLocalNotificationsPlugin.show(0, 'Lantern',
-        'Done downloading "$displayName"', notifications.downloadChannel,
-        payload: Payload(type: PayloadType.download, data: savePath).toJson());
-    logger.v('displayed 2nd notification');
   }
 
-  // Future<void> download(ReplicaLink replicaLink, String displayName) async {
-  //   logger.v('XXX downloading ${replicaLink.infohash}');
-  //   var hasPermission = await Permission.storage.request().isGranted;
-  //   if (!hasPermission) {
-  //     throw Exception('Permission');
-  //   }
-  //   logger.v('Permission granted');
-  //   final dir = await getExternalStorageDirectory();
-  //   if (dir == null) {
-  //     throw Exception('downloads directory');
-  //   }
-  //   logger.v('dir.path: ${dir.path}');
+  Future<void> download(ReplicaLink link) async {
+    final downloadsDir = await _prepAndFetchDownloadsDir();
+    if (downloadsDir == null) {
+      logger.e('Failed to fetch downloads dir');
+      _onDownloadError();
+      return;
+    }
+    logger.v('downloadsDir: $downloadsDir');
 
-  //   await FlutterDownloader.enqueue(
-  //     url: getDownloadAddr(replicaLink),
-  //     savedDir: dir.path,
-  //     fileName: displayName,
-  //     showNotification: true,
-  //     openFileFromNotification: true,
-  //     saveInPublicStorage: true,
-  //   );
-  // }
+    final u = getDownloadAddr(link);
+    logger.v('XXX downloadAddr: $u');
+    var displayName = link.displayName ?? link.infohash;
+    var taskId = await FlutterDownloader.enqueue(
+      url: u,
+      // headers: {"auth": "test_for_sql_encoding"},
+      savedDir: downloadsDir,
+      showNotification: true,
+      fileName: displayName,
+      openFileFromNotification: true,
+      saveInPublicStorage: true,
+    );
+    logger.v('XXX taskId: $taskId');
+  }
+
+  void bindDownloaderBackgroundIsolate(
+      Function() onDownloadError, Function()? onDownloadStarted) {
+    _onDownloadError = onDownloadError;
+    var isSuccess = IsolateNameServer.registerPortWithName(
+        _downloaderCallbackPort.sendPort, _downloadeManagerIsolateName);
+    if (!isSuccess) {
+      // XXX <10-12-2021> soltzen: This is a rare case but best we handle it
+      logger.w(
+          'Failed to register a callback. Register an empty callback for FlutterDownloader so that downloads do not fail');
+      FlutterDownloader.registerCallback(_emptyDownloaderCallback);
+      return;
+    }
+
+    _downloaderCallbackPort.listen((dynamic data) {
+      logger.v('XXX UI Isolate Callback: $data');
+      String? id = data[0];
+      DownloadTaskStatus? status = data[1];
+      int? progress = data[2];
+
+      if (status == null) {
+        return;
+      }
+
+      if (status == DownloadTaskStatus.failed) {
+        onDownloadError();
+      } else if (status == DownloadTaskStatus.enqueued &&
+          onDownloadStarted != null) {
+        onDownloadStarted();
+      }
+    });
+
+    FlutterDownloader.registerCallback(_defaultDownloaderCallback);
+  }
+
+  void unbindDownloadManagerIsolate() {
+    IsolateNameServer.removePortNameMapping(_downloadeManagerIsolateName);
+  }
+}
+
+void _emptyDownloaderCallback(
+    String id, DownloadTaskStatus status, int progress) {}
+
+void _defaultDownloaderCallback(
+    String id, DownloadTaskStatus status, int progress) {
+  logger.v(
+      'Background Isolate Callback: task ($id) is in status ($status) and process ($progress)');
+  final send = IsolateNameServer.lookupPortByName(
+      ReplicaApi._downloadeManagerIsolateName)!;
+  send.send([id, status, progress]);
 }
