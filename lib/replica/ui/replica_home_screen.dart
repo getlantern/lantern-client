5e22ec75616b6326b331b8b035c0cf8df7ae5e5b
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2021-12-16 11:36:37 +0100

    [ui:replica] General cleanup

--- lib/replica/ui/replica_home_screen.dart
+++ lib/replica/ui/replica_home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lantern/common/common.dart';
 import 'package:auto_route/src/router/auto_router_x.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
 
 /// ReplicaHomeScreen is the entrypoint for the user to search through Replica.
 /// See docs/replica_home.png for a preview
@@ -20,7 +25,7 @@ class _ReplicaHomeScreenState extends State<ReplicaHomeScreen> {
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () {
-          // Remove focus
+          // Dismiss keyboard when clicking anywhere
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: BaseScreen(
@@ -29,58 +34,55 @@ class _ReplicaHomeScreenState extends State<ReplicaHomeScreen> {
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // const SizedBox(height: 50),
-                // const Center(
-                //     child:
-                //         CAssetImage(size: 70, path: ImagePaths.lantern_logo)),
-                // const SizedBox(height: 40),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: TextFormField(
-                    controller: _textEditingController,
-                    textInputAction: TextInputAction.search,
-                    onFieldSubmitted: (query) async {
-                      await _navigateToSearchScreen(query);
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Search',
-                      suffixIcon: Material(
-                        color: blue4,
-                        child: IconButton(
-                            onPressed: () async {
-                              await _navigateToSearchScreen(
-                                  _textEditingController.text);
-                            },
-                            icon: const Icon(Icons.search),
-                            color: white),
-                      ),
-                      contentPadding:
-                          const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: grey3,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: blue4,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 22.0, vertical: 10.0),
-                  child: CText(
-                      'Search user-uploaded content on the Lantern Network, or upload your own for others to discover.'
-                          .i18n,
-                      style: tsBody1),
-                )
-              ],
+              children: [renderSearchBar(), renderDescription()],
             )));
+  }
+
+  Widget renderDescription() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 10.0),
+      child: CText(
+          'Search user-uploaded content on the Lantern Network, or upload your own for others to discover.'
+              .i18n,
+          style: tsBody1),
+    );
+  }
+
+  Widget renderSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: TextFormField(
+        controller: _textEditingController,
+        textInputAction: TextInputAction.search,
+        onFieldSubmitted: (query) async {
+          await _navigateToSearchScreen(query);
+        },
+        decoration: InputDecoration(
+          labelText: 'Search',
+          suffixIcon: Material(
+            color: blue4,
+            child: IconButton(
+                onPressed: () async {
+                  await _navigateToSearchScreen(_textEditingController.text);
+                },
+                icon: const Icon(Icons.search),
+                color: white),
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: grey3,
+              width: 2.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: blue4,
+              width: 2.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
