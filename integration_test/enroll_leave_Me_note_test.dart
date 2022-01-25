0eefb11b3b45b620593a6a3bd3e17823e66a5e6f
Kallirroi <kallirroi.retzepi@gmail.com>
2022-01-25 16:30:25 +0100

    some restructuring, setting things up for more tests + screenshots

--- integration_test/enroll_leave_Me_note_test.dart
+++ integration_test/enroll_leave_Me_note_test.dart
@@ -2,27 +2,32 @@ import 'integration_test_common.dart';
 
 Future<void> main() async {
   late FlutterDriver driver;
+  final testName = 'enroll_leave_Me_note_test';
 
   setUpAll(() async {
     // Connect to a running Flutter application instance.
     driver = await FlutterDriver.connect(timeout: const Duration(seconds: 15));
-    await driver.initScreenshotsDirectory();
+    await driver.initScreenshotsDirectory(testName);
   });
 
   tearDownAll(() async {
     await driver.close();
   });
 
-  group('smoke_test_chat', () {
+  group(testName, () {
     test(
       'Enroll and send message to myself',
       () async {
         await driver.tapText(
           'Developer',
           waitText: 'Developer Settings',
+          skipScreenshot: true,
         );
         await driver.scrollTextUntilVisible('RESET FLAGS');
-        await driver.tapText('RESET FLAGS');
+        await driver.tapText(
+          'RESET FLAGS',
+          skipScreenshot: true,
+        );
         await driver.tapText(
           'Chats',
           waitText: 'Welcome to Lantern Chat!',
@@ -44,7 +49,10 @@ Future<void> main() async {
               'Your chats and voice calls with Me are end-to-end encrypted',
         );
         await driver.tapType('TextFormField');
-        await driver.enterText('Hello me', timeout: const Duration(seconds: 5));
+        await driver.enterText(
+          'I want to eat some pizza!',
+          timeout: const Duration(seconds: 5),
+        );
         await driver.tapKey('send_message');
       },
       timeout: const Timeout(Duration(minutes: 5)),
