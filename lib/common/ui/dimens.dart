e5ee35c4356a21b68e622df20d42fee2c9cdce05
Ox Cart <ox.to.a.cart@gmail.com>
2022-01-26 13:31:05 -0600

    Removed whitespace at bottom of message bar

--- lib/common/ui/dimens.dart
+++ lib/common/ui/dimens.dart
@@ -4,7 +4,7 @@ const double borderRadius = 8;
 const double activeIconSize = 8;
 const double iconSize = 24;
 const double badgeSize = 36;
-const double messageBarHeight = 57;
+const double messageBarHeight = 59;
 const double scrollBarRadius = 50;
 
 bool isLTR(BuildContext context) =>
