a5bcc0cded5d40fbc324b57d1296cb8e498a7564
Kallirroi <kallirroi.retzepi@gmail.com>
2022-02-15 15:45:12 +0100

    fix voice_memo_test for cases where _Me_ is encoded by single character

--- integration_test/5_voice_memo_test.dart
+++ integration_test/5_voice_memo_test.dart
@@ -22,9 +22,19 @@ Future<void> main() async {
 
         await driver.tapFAB();
 
-        await driver.tapText(
-          await driver.requestData('me'),
-        );
+        // Looking for "Me" works for most languages.
+        // However in some cases (Chinese), "Me" is a single character, and so is the alphabetic marker we use to separate contacts, which confuses the test driver.
+        // In that case, we can just find the first element of the 'grouped_contact_list' ListBody.
+        try {
+          await driver.tapText(
+            await driver.requestData('me'),
+          );
+        } catch (_) {
+          print(
+            'there was an issue tapping on Me conversation, will find another contact',
+          );
+          await driver.tapFirstItemInList('grouped_contact_list');
+        }
 
         final recorderButtonFinder = find.byValueKey('recorder_button');
 
