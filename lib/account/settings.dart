c8b0c6957979976c1606fdfd5e555e9b411a11fd
Kallirroi <kallirroi.retzepi@gmail.com>
2022-02-17 10:57:20 +0100

    fixes in how we create the screenshots/ directory

--- lib/account/settings.dart
+++ lib/account/settings.dart
@@ -28,6 +28,7 @@ class Settings extends StatelessWidget {
     return BaseScreen(
       title: 'settings'.i18n,
       body: Column(
+        key: const ValueKey('settings_list'),
         children: [
           //* Language
           ListItemFactory.settingsItem(
