f037f8d291a7aa06f4bc4f39e8ad52b9efa885db
Ox Cart <ox.to.a.cart@gmail.com>
2022-02-23 10:34:42 -0600

    Code review updates

--- internalsdk/tcp.go
+++ internalsdk/tcp.go
@@ -1,101 +1,47 @@
 package internalsdk
 
 import (
-	"io"
 	"net"
 	"sync"
 
 	"golang.org/x/net/proxy"
 
 	"github.com/eycorsican/go-tun2socks/core"
+	"github.com/getlantern/errors"
+	"github.com/getlantern/netx"
 )
 
-// Copied and lightly modified from https://github.com/eycorsican/go-tun2socks/blob/master/proxy/socks/udp.go
+// Loosely based on https://github.com/eycorsican/go-tun2socks/blob/master/proxy/socks/udp.go
 type socksTCPHandler struct {
 	sync.Mutex
 
 	proxyAddr string
+	mtu       int
 }
 
-func newSOCKSTCPHandler(proxyAddr string) core.TCPConnHandler {
+func newSOCKSTCPHandler(proxyAddr string, mtu int) core.TCPConnHandler {
 	log.Debugf("Creating new Socks TCP Handler using proxy at %v", proxyAddr)
 	return &socksTCPHandler{
 		proxyAddr: proxyAddr,
+		mtu:       mtu,
 	}
 }
 
-type direction byte
-
-const (
-	dirUplink direction = iota
-	dirDownlink
-)
-
-type duplexConn interface {
-	net.Conn
-	CloseRead() error
-	CloseWrite() error
-}
-
-func (h *socksTCPHandler) relay(lhs, rhs net.Conn) {
-	upCh := make(chan struct{})
-
-	cls := func(dir direction, interrupt bool) {
-		lhsDConn, lhsOk := lhs.(duplexConn)
-		rhsDConn, rhsOk := rhs.(duplexConn)
-		if !interrupt && lhsOk && rhsOk {
-			switch dir {
-			case dirUplink:
-				lhsDConn.CloseRead()
-				rhsDConn.CloseWrite()
-			case dirDownlink:
-				lhsDConn.CloseWrite()
-				rhsDConn.CloseRead()
-			default:
-				panic("unexpected direction")
-			}
-		} else {
-			lhs.Close()
-			rhs.Close()
-		}
-	}
-
-	// Uplink
-	go func() {
-		var err error
-		_, err = io.Copy(rhs, lhs)
-		if err != nil {
-			cls(dirUplink, true) // interrupt the conn if the error is not nil (not EOF)
-		} else {
-			cls(dirUplink, false) // half close uplink direction of the TCP conn if possible
-		}
-		upCh <- struct{}{}
-	}()
-
-	// Downlink
-	var err error
-	_, err = io.Copy(lhs, rhs)
-	if err != nil {
-		cls(dirDownlink, true)
-	} else {
-		cls(dirDownlink, false)
-	}
-
-	<-upCh // Wait for uplink done.
-}
-
-func (h *socksTCPHandler) Handle(conn net.Conn, target *net.TCPAddr) error {
+func (h *socksTCPHandler) Handle(downstream net.Conn, target *net.TCPAddr) error {
 	dialer, err := proxy.SOCKS5("tcp", h.proxyAddr, nil, nil)
 	if err != nil {
-		return err
+		return errors.New("unable to connect to SOCKS proxy at %v: %v", h.proxyAddr, err)
 	}
 
-	c, err := dialer.Dial(target.Network(), target.String())
+	upstream, err := dialer.Dial(target.Network(), target.String())
 	if err != nil {
-		return err
+		return errors.New("unable to dial upstream %v via SOCKS proxy at %v: %v", target, h.proxyAddr, err)
 	}
 
-	go h.relay(conn, c)
+	bufOut := make([]byte, h.mtu)
+	bufIn := make([]byte, h.mtu)
+
+	go netx.BidiCopy(upstream, downstream, bufOut, bufIn)
 
 	return nil
 }
