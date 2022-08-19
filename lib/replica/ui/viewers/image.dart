055de07364dbb52169a335022f6cd19bb7127eb8
Kallirroi <kallirroi.retzepi@gmail.com>
2022-08-19 14:10:04 +0200

    better embedded -> fullscreen Replica video viewer UX

--- lib/replica/ui/viewers/image.dart
+++ lib/replica/ui/viewers/image.dart
@@ -18,11 +18,14 @@ class ReplicaImageViewer extends ReplicaViewerLayout {
 class _ReplicaImageViewerState extends ReplicaViewerLayoutState {
   var thumbnailURL = '';
   var imageURL = '';
+  late Future<Uint8List> loadImageFile;
   @override
   void initState() {
     super.initState();
     thumbnailURL = widget.replicaApi.getThumbnailAddr(widget.item.replicaLink);
     imageURL = widget.replicaApi.getDownloadAddr(widget.item.replicaLink);
+    // while loading, fetch the future we will pass to FullScreenImageViewer so that its ready when we tap the image
+    loadImageFile = widget.replicaApi.getImageBytesFromURL(imageURL);
   }
 
   @override
@@ -46,9 +49,7 @@ class _ReplicaImageViewerState extends ReplicaViewerLayoutState {
     return await context.router.push(
       FullScreenDialogPage(
         widget: FullScreenImageViewer(
-          loadImageFile: widget.replicaApi.getImageBytesFromURL(
-            imageURL,
-          ),
+          loadImageFile: loadImageFile,
           title: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
