c75c095840306f128a544352425ee0f8866ac3de
Kallirroi <kallirroi.retzepi@gmail.com>
2022-08-25 13:32:03 +0200

    added READ DETAILS icon to PDF thumbnail

--- lib/common/ui/info_text_box.dart
+++ lib/common/ui/info_text_box.dart
@@ -2,8 +2,10 @@ import 'package:lantern/common/common.dart';
 
 class InfoTextBox extends StatelessWidget {
   final String text;
+  final bool? invertColors;
   const InfoTextBox({
     required this.text,
+    this.invertColors = false,
     Key? key,
   }) : super(key: key);
 
@@ -17,10 +19,10 @@ class InfoTextBox extends StatelessWidget {
           end: 8,
           bottom: 4,
         ),
-        decoration: BoxDecoration(color: black),
+        decoration: BoxDecoration(color: invertColors! ? white : black),
         child: CText(
           text,
-          style: tsOverline.copiedWith(color: white),
+          style: tsOverline.copiedWith(color: invertColors! ? black : white),
         ),
       ),
     );
