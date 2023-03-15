14efe095f575cffaa7c660989b279b72649473c1
atavism <todd@lana.io>
2023-03-15 09:53:27 -0700

    Add DeviceInfo and InstallFinished call

--- internalsdk/android.go
+++ internalsdk/android.go
@@ -671,13 +671,25 @@ func afterStart(session panickingSession) {
 	}()
 }
 
-// CheckForUpdates checks to see if a new version of Lantern is available
-func CheckForUpdates() (string, error) {
-	return checkForUpdates(buildUpdateCfg())
+// DeviceInfo provides information about a device for sending with ops when
+// downloading and installing auto-updates
+type DeviceInfo interface {
+	DeviceID() string
+	Model() string
+	Hardware() string
+	SdkVersion() int
+	UserID() string
 }
 
-func checkForUpdates(cfg *autoupdate.Config) (string, error) {
+// CheckForUpdates checks to see if a new version of Lantern is available
+func CheckForUpdates(deviceInfo DeviceInfo) (string, error) {
+	return checkForUpdates(buildUpdateCfg(), deviceInfo)
+}
+
+func checkForUpdates(cfg *autoupdate.Config, deviceInfo DeviceInfo) (string, error) {
 	op := ops.Begin("check_update").
+		Set("user_id", deviceInfo.UserID()).
+		Set("device_id", deviceInfo.DeviceID()).
 		Set("current_version", cfg.CurrentVersion)
 	defer op.End()
 	updateURL, err := autoupdate.CheckMobileUpdate(cfg)
@@ -687,18 +699,32 @@ func checkForUpdates(cfg *autoupdate.Config) (string, error) {
 	return updateURL, nil
 }
 
+func deviceOps(name string, deviceInfo DeviceInfo) *ops.Op {
+	return ops.Begin(name).
+		Set("user_id", deviceInfo.UserID()).
+		Set("device_id", deviceInfo.DeviceID()).
+		Set("model", deviceInfo.Model()).
+		Set("hardware", deviceInfo.Hardware()).
+		Set("sdk_version", deviceInfo.SdkVersion())
+}
+
 // DownloadUpdate downloads the latest APK from the given url to the apkPath
 // file destination.
-func DownloadUpdate(model, hardware string, sdkVersion int, url, apkPath string, updater Updater) {
-	op := ops.Begin("autoupdate").
-		Set("model", model).
-		Set("hardware", hardware).
-		Set("sdk_version", sdkVersion)
+func DownloadUpdate(deviceInfo DeviceInfo, url, apkPath string, updater Updater) {
+	op := deviceOps("autoupdate_download", deviceInfo)
 	defer op.End()
 	err := autoupdate.UpdateMobile(url, apkPath, updater, updateClient)
 	if err != nil {
 		op.FailIf(log.Errorf("Error downloading update: %v", err))
 	}
+}
+
+// InstallFinished is called after an update successfully installs or fails to
+// and records ops related to it
+func InstallFinished(deviceInfo DeviceInfo, success bool) {
+	op := deviceOps("autoupdate_install", deviceInfo).
+		Set("success", success)
+	op.End()
 }
 
 func buildUpdateCfg() *autoupdate.Config {
