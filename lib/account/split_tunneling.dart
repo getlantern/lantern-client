3edabe182d15b55d5fb0c4be03ec4716391ea0f6
atavism <todd@lana.io>
2023-04-05 10:19:24 -0700

    UI fixes, update split tunneling icon

--- lib/account/split_tunneling.dart
+++ lib/account/split_tunneling.dart
@@ -27,11 +27,11 @@ class _SplitTunnelingState extends State<SplitTunneling> {
 
   initSplitTunneling() async {
     AppsData _appsData = await sessionModel.appsData();
-    for (var packageName in _appsData.excludedApps.excludedApps.keys) {
-      excludedApps[packageName] = true;
-    }
     setState(() {
       appsData = _appsData;
+      for (var packageName in _appsData.excludedApps.excludedApps.keys) {
+        excludedApps[packageName] = true;
+      }
     });
   }
 
@@ -39,33 +39,38 @@ class _SplitTunnelingState extends State<SplitTunneling> {
   Widget build(BuildContext context) {
     return BaseScreen(
         title: 'split_tunneling'.i18n,
-        body: sessionModel.splitTunneling((BuildContext context, bool value,
-                Widget? child) =>
-            Column(children: <Widget>[
-              ListItemFactory.settingsItem(
-                icon: ImagePaths.split_tunneling,
-                content: 'split_tunneling'.i18n,
-                trailingArray: [
-                  FlutterSwitch(
-                    width: 44.0,
-                    height: 24.0,
-                    valueFontSize: 12.0,
-                    activeColor: Colors.green,
-                    padding: 2,
-                    toggleSize: 18.0,
-                    value: value,
-                    onToggle: (bool newValue) {
-                      sessionModel.setSplitTunneling(newValue);
-                    },
-                  )
-                ],
-              ),
-              CText(value ? 'apps_selected'.i18n : 'split_tunneling_info'.i18n,
-                  style: tsBody3),
-              // if split tunneling is enabled, include the installed apps
-              // in the column
-              if (value) ...buildAppsLists(),
-            ])));
+        body: sessionModel.splitTunneling(
+            (BuildContext context, bool value, Widget? child) =>
+                Column(children: <Widget>[
+                  ListItemFactory.settingsItem(
+                    icon: ImagePaths.split_tunneling,
+                    content: 'split_tunneling'.i18n,
+                    trailingArray: [
+                      FlutterSwitch(
+                        width: 44.0,
+                        height: 24.0,
+                        valueFontSize: 12.0,
+                        activeColor: Colors.green,
+                        padding: 2,
+                        toggleSize: 18.0,
+                        value: value,
+                        onToggle: (bool newValue) {
+                          sessionModel.setSplitTunneling(newValue);
+                        },
+                      )
+                    ],
+                  ),
+                  Padding(
+                      padding: const EdgeInsetsDirectional.only(top: 16),
+                      child: CText(
+                          value
+                              ? 'apps_selected'.i18n
+                              : 'split_tunneling_info'.i18n,
+                          style: tsBody3)),
+                  // if split tunneling is enabled, include the installed apps
+                  // in the column
+                  if (value) ...buildAppsLists(),
+                ])));
   }
 
   // buildAppsLists builds lists for excluded and allowed installed apps and
@@ -95,7 +100,7 @@ class _SplitTunnelingState extends State<SplitTunneling> {
     }
 
     return Expanded(
-        child: ListView.separated(
+        child: ListView.builder(
             itemCount: apps.length,
             itemBuilder: (BuildContext context, int index) {
               var appData = apps[index];
@@ -103,9 +108,6 @@ class _SplitTunnelingState extends State<SplitTunneling> {
               bool isExcluded = isAppExcluded(appData.packageName);
               Widget appItem = buildAppItem(appData, isExcluded);
               return appItem;
-            },
-            separatorBuilder: (context, index) {
-              return Divider(height: 2.0);
             }));
   }
 
@@ -113,52 +115,43 @@ class _SplitTunnelingState extends State<SplitTunneling> {
     Uint8List iconBytes = base64.decode(appData.icon);
     return Container(
         height: 72,
-        child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              ListTile(
-                key: Key(appData.packageName),
-                leading: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ConstrainedBox(
-                        constraints: BoxConstraints(
-                          minWidth: 24,
-                          minHeight: 24,
-                          maxWidth: 24,
-                          maxHeight: 24,
-                        ),
-                        child: new Image.memory(iconBytes, fit: BoxFit.cover),
-                      )
-                    ]),
-                trailing: Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 24),
-                  child: Checkbox(
-                    checkColor: Colors.white,
-                    shape: CircleBorder(),
-                    activeColor: Colors.black,
-                    side: BorderSide(color: Colors.black),
-                    value: isAppExcluded,
-                    onChanged: (bool? value) {
-                      // This is called when the user toggles the switch.
-                      setState(() {
-                        if (value!) {
-                          excludedApps[appData.packageName] = true;
-                          sessionModel.addExcludedApp(appData.packageName);
-                        } else {
-                          excludedApps.remove(appData.packageName);
-                          sessionModel.removeExcludedApp(appData.packageName);
-                        }
-                      });
-                    },
-                  ),
-                ),
-                title: CText(
-                  toBeginningOfSentenceCase(appData.name)!,
-                  softWrap: false,
-                  style: tsSubtitle1.short,
-                ),
-              )
-            ]));
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Color(0xFFEBEBEB), width: 1.0)),
+        ),
+        child: ListTile(
+          key: Key(appData.packageName),
+          leading: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 24,
+              minHeight: 24,
+              maxWidth: 24,
+              maxHeight: 24,
+            ),
+            child: new Image.memory(iconBytes, fit: BoxFit.cover),
+          ),
+          trailing: Checkbox(
+            checkColor: Colors.white,
+            shape: CircleBorder(),
+            activeColor: Colors.black,
+            side: BorderSide(color: Colors.black),
+            value: isAppExcluded,
+            onChanged: (bool? value) {
+              setState(() {
+                if (value != null && value!) {
+                  excludedApps[appData.packageName] = true;
+                  sessionModel.addExcludedApp(appData.packageName);
+                } else {
+                  excludedApps.remove(appData.packageName);
+                  sessionModel.removeExcludedApp(appData.packageName);
+                }
+              });
+            },
+          ),
+          title: CText(
+            toBeginningOfSentenceCase(appData.name)!,
+            softWrap: false,
+            style: tsSubtitle1.short,
+          ),
+        ));
   }
 }
