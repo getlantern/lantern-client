bd34dfea07b11c6bf36d95f59693d2f84a646863
atavism <todd@lana.io>
2023-04-12 05:58:03 -0700

    remaining fixes

--- android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/MainActivity.kt
@@ -460,7 +460,6 @@ class MainActivity : FlutterActivity(), MethodChannel.MethodCallHandler, Corouti
         // disable the on/off switch while the VpnService
         // is updating the connection
         if (on) {
-            internalsdk.Geolookup()
             // Make sure we have the necessary permissions
             val neededPermissions: Array<String> = missingPermissions()
             if (neededPermissions.isNotEmpty()) {
