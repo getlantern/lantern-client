f8fc90480acec45fdf58ab4a0d4bc115b4ff3ded
atavism <todd@lana.io>
2023-04-15 18:19:56 -0700

    run ktlint

--- android/app/src/main/kotlin/org/getlantern/lantern/notification/NotificationHelper.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/notification/NotificationHelper.kt
@@ -1,25 +1,19 @@
 package org.getlantern.lantern.notification
 
+import android.annotation.TargetApi
 import android.app.Activity
-import android.app.PendingIntent
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.graphics.Color
+import android.app.PendingIntent
 import android.content.ContextWrapper
-import android.annotation.TargetApi
-import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
-import android.os.Bundle
-import androidx.core.app.RemoteInput
-import android.widget.RemoteViews
-import io.lantern.model.VpnModel
-import org.getlantern.lantern.activity.afterNotification
-import org.getlantern.lantern.MainActivity
 import org.getlantern.lantern.R
+import org.getlantern.lantern.activity.afterNotification
 
-class NotificationHelper(private val activity:Activity, private val receiver: NotificationReceiver?) : ContextWrapper(activity) {
+class NotificationHelper(private val activity: Activity, private val receiver: NotificationReceiver?) : ContextWrapper(activity) {
 
     // Used to notify a user of events that happen in the background
     private val manager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -30,33 +24,35 @@ class NotificationHelper(private val activity:Activity, private val receiver: No
     @TargetApi(Build.VERSION_CODES.O)
     private fun initChannels() {
         dataUsageNotificationChannel = NotificationChannel(
-            CHANNEL_DATA_USAGE, DATA_USAGE_DESC,
-            NotificationManager.IMPORTANCE_HIGH
+            CHANNEL_DATA_USAGE,
+            DATA_USAGE_DESC,
+            NotificationManager.IMPORTANCE_HIGH,
         )
         vpnNotificationChannel = NotificationChannel(
-            CHANNEL_VPN, VPN_DESC,
-            NotificationManager.IMPORTANCE_HIGH
+            CHANNEL_VPN,
+            VPN_DESC,
+            NotificationManager.IMPORTANCE_HIGH,
         )
-    	val channels:Array<NotificationChannel> = arrayOf(
+        val channels: Array<NotificationChannel> = arrayOf(
             dataUsageNotificationChannel,
             vpnNotificationChannel,
         )
         channels.forEach { setChannelOptions(it) }
     }
 
-    private fun setChannelOptions(notificationChannel:NotificationChannel) {
-    	notificationChannel.enableLights(true)
+    private fun setChannelOptions(notificationChannel: NotificationChannel) {
+        notificationChannel.enableLights(true)
         notificationChannel.lightColor = Color.GREEN
         notificationChannel.enableVibration(false)
         manager.createNotificationChannel(notificationChannel)
     }
 
-    private fun createPendingIntent():PendingIntent {
-    	 val intent = Intent(activity, afterNotification::class.java)
-    	 return PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    private fun createPendingIntent(): PendingIntent {
+        val intent = Intent(activity, afterNotification::class.java)
+        return PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
-    private fun disconnectBroadcast():PendingIntent {
+    private fun disconnectBroadcast(): PendingIntent {
         val intent = Intent(activity, NotificationReceiver::class.java)
         val packageName = activity.packageName
         intent.setAction("$packageName.intent.VPN_DISCONNECTED")
@@ -68,40 +64,40 @@ class NotificationHelper(private val activity:Activity, private val receiver: No
     }*/
 
     public fun vpnConnectedNotification() {
-    	builder.setChannelId(CHANNEL_VPN)
-    	manager.notify(1234, builder.build())
+        builder.setChannelId(CHANNEL_VPN)
+        manager.notify(1234, builder.build())
     }
 
     public fun dataUsageNotification() {
-    	builder.setChannelId(CHANNEL_DATA_USAGE)
-    	manager.notify(1234, builder.build())
+        builder.setChannelId(CHANNEL_DATA_USAGE)
+        manager.notify(1234, builder.build())
     }
 
     fun clearNotification() {
         manager.cancelAll()
     }
 
-	companion object {
-		private val TAG = NotificationHelper::class.java.simpleName
-		private const val LANTERN_NOTIFICATION = "lantern.notification"
-		private const val DATA_USAGE = 36
+    companion object {
+        private val TAG = NotificationHelper::class.java.simpleName
+        private const val LANTERN_NOTIFICATION = "lantern.notification"
+        private const val DATA_USAGE = 36
         private const val VPN_CONNECTED = 37
         private const val VPN_DISCONNECTED = 38
         private const val CHANNEL_VPN = "vpn"
         private const val CHANNEL_DATA_USAGE = "data_usage"
         private const val VPN_DESC = "VPN"
         private const val DATA_USAGE_DESC = "Data Usage"
-	}
+    }
 
     init {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             initChannels()
         }
         builder = Notification.Builder(this)
-        	//.setContent(createContentView())
+            // .setContent(createContentView())
             .setSmallIcon(R.drawable.lantern_notification_icon)
             .setVisibility(Notification.VISIBILITY_PUBLIC)
             .setContentIntent(createPendingIntent())
             .addAction(R.drawable.notification_icon, "Disconnect", disconnectBroadcast())
-    } 
+    }
 }
