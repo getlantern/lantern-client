16414f81effb851f4acdd43b76dffb781eb990ea
atavism <todd@lana.io>
2023-04-25 06:19:44 -0700

    add logos

--- lib/plans/stripe_checkout.dart
+++ lib/plans/stripe_checkout.dart
@@ -267,10 +267,10 @@ class _StripeCheckoutState extends State<StripeCheckout> {
         cvcFieldController.value.text.isEmpty;
 
     // returns true if there is at least one invalid field
-    final anyFieldsInvalid = !emailFieldKey.currentState?.validate() ||
-        !creditCardFieldKey.currentState?.validate() ||
-        !expDateFieldKey.currentState?.validate() ||
-        !cvcFieldKey.currentState?.validate();
+    final anyFieldsInvalid = emailFieldKey.currentState?.validate() == false ||
+        creditCardFieldKey.currentState?.validate() == false ||
+        expDateFieldKey.currentState?.validate() == false ||
+        cvcFieldKey.currentState?.validate()  == false;
 
     return (!anyFieldsEmpty && !anyFieldsInvalid);
   }
