c420949885a1532db486b9ba87c3b619df5597fa
atavism <todd@lana.io>
2023-05-03 16:49:17 -0700

    migrate vpn classes to Kotlin

---
+++ android/app/src/main/kotlin/org/getlantern/lantern/vpn/GoTun2SocksProvider.kt
@@ -0,0 +1,96 @@
+package org.getlantern.lantern.vpn
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.VpnService
+import android.os.ParcelFileDescriptor
+
+import internalsdk.Internalsdk
+
+import org.getlantern.lantern.MainActivity
+import org.getlantern.mobilesdk.Logger
+import org.getlantern.mobilesdk.model.SessionManager
+
+import java.util.Locale
+
+class GoTun2SocksProvider : Provider {
+  private var mInterface: ParcelFileDescriptor? = null
+
+  @Synchronized private fun createBuilder(vpnService:VpnService, builder: VpnService.Builder):ParcelFileDescriptor? {
+    // Set the locale to English
+    // since the VpnBuilder encounters
+    // issues with non-English numerals
+    // See https://code.google.com/p/android/issues/detail?id=61096
+    Locale.setDefault(Locale("en"))
+
+    // Configure a builder while parsing the parameters.
+    builder.setMtu(VPN_MTU)
+
+    builder.addAddress(privateAddress, 24)
+    // route IPv4 through VPN
+    builder.addRoute("0.0.0.0", 0)
+    // Don't capture traffic originating from Lantern itself in the VPN
+    val ourPackageName = vpnService.getPackageName()
+    try {
+      builder.addDisallowedApplication(ourPackageName)
+    } catch (e: PackageManager.NameNotFoundException) {
+      throw RuntimeException("Unable to exclude Lantern from routes", e)
+    }
+    // don't currently route IPv6 through VPN because our proxies don't currently support IPv6
+    // see https://github.com/getlantern/lantern-internal/issues/4961
+    // Note - if someone performs a DNS lookup for an IPv6 only host like ipv6.google.com, dnsgrab
+    // will return an IPv4 address for that site, causing the traffic to get routed through the VPN.
+    // builder.addRoute("0:0:0:0:0:0:0:0", 0)
+
+    // this is a fake DNS server. The precise IP doesn't matter because Lantern will intercept and
+    // route all DNS traffic to dnsgrab internally anyway.
+    builder.addDnsServer(SessionManager.fakeDnsIP)
+
+    val intent = Intent(vpnService, MainActivity::class.java)
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(vpnService, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    builder.setConfigureIntent(pendingIntent)
+
+    builder.setSession(sessionName)
+
+    // Create a new mInterface using the builder and save the parameters.
+    mInterface = builder.establish()
+    Logger.d(TAG, "New mInterface: " + mInterface)
+    return mInterface
+  }
+
+  override fun run(vpnService: VpnService, builder: VpnService.Builder, socksAddr: String, dnsGrabAddr: String) {
+    Logger.d(TAG, "run")
+
+    val defaultLocale = Locale.getDefault()
+    try {
+      Logger.debug(TAG, "Creating VpnBuilder before starting tun2socks")
+      val intf: ParcelFileDescriptor? = createBuilder(vpnService, builder)
+      Logger.debug(TAG, "Running tun2socks")
+      if (intf != null) Internalsdk.tun2Socks(intf.getFd().toLong(), socksAddr, dnsGrabAddr, VPN_MTU.toLong())
+    } catch (t:Throwable) {
+      Logger.e(TAG, "Exception while handling TUN device", t)
+    } finally {
+      Locale.setDefault(defaultLocale)
+    }
+  }
+
+  @Synchronized
+  @Throws(Exception::class)
+  override fun stop() {
+    Logger.d(TAG, "stop")
+    Internalsdk.stopTun2Socks()
+    mInterface?.let {
+      Logger.d(TAG, "closing interface")
+      mInterface!!.close()
+      mInterface = null      
+    }
+  }
+
+  companion object {
+    private val TAG = GoTun2SocksProvider::class.java.simpleName
+    private const val sessionName = "LanternVpn"
+    private const val privateAddress = "10.0.0.2"
+    private const val VPN_MTU = 1500
+  }
+}
