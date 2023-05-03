449928cbfb89aed61fff5b96b4d5dde58dbedb1b
atavism <todd@lana.io>
2023-05-03 05:13:19 -0700

    clean-up

--- android/app/src/main/kotlin/org/getlantern/lantern/LanternApp.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/LanternApp.kt
@@ -9,7 +9,6 @@ import androidx.multidex.MultiDex
 import org.getlantern.lantern.model.InAppBilling
 import org.getlantern.lantern.model.LanternHttpClient
 import org.getlantern.lantern.model.LanternSessionManager
-// import org.getlantern.lantern.model.MessagingHolder;
 import org.getlantern.lantern.util.debugOnly
 import org.getlantern.lantern.util.LanternProxySelector
 import org.getlantern.lantern.util.SentryUtil
