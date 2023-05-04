da1e8f4ca0cc9da8788758899b766cc7ab58fde9
atavism <todd@lana.io>
2023-05-04 07:34:30 -0700

    run ktlint

--- android/app/src/main/kotlin/org/getlantern/lantern/vpn/Provider.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/vpn/Provider.kt
@@ -4,6 +4,9 @@ import android.net.VpnService
 
 // A provider provides the implementation of VPN internals.
 interface Provider {
-  @Throws(Exception::class) fun run(vpnService:VpnService, builder:VpnService.Builder, socksAddr:String, dnsGrabAddr:String)
-  @Throws(Exception::class) fun stop()
+    @Throws(Exception::class)
+    fun run(vpnService: VpnService, builder: VpnService.Builder, socksAddr: String, dnsGrabAddr: String)
+
+    @Throws(Exception::class)
+    fun stop()
 }
