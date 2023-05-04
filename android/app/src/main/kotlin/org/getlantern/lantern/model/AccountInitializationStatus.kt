b3c22f6b60d7edc0a8fc97e157c686d1e9aa11d0
atavism <todd@lana.io>
2023-05-04 07:32:36 -0700

    apply formatting

--- android/app/src/main/kotlin/org/getlantern/lantern/model/AccountInitializationStatus.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/model/AccountInitializationStatus.kt
@@ -1,20 +1,20 @@
 package org.getlantern.lantern.model
 
-data class AccountInitializationStatus(val status:AccountInitializationStatus.Status) {
+data class AccountInitializationStatus(val status: AccountInitializationStatus.Status) {
 
     enum class Status {
         PROCESSING, SUCCESS, FAILURE
     }
 
-    fun isProcessing():Boolean {
+    fun isProcessing(): Boolean {
         return status == Status.PROCESSING
     }
 
-    fun isSuccess():Boolean {
+    fun isSuccess(): Boolean {
         return status == Status.SUCCESS
     }
 
-    fun isFailure():Boolean {
+    fun isFailure(): Boolean {
         return status == Status.FAILURE
     }
 }
