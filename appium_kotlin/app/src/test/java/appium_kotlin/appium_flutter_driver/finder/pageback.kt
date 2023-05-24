b3d0546935e2481ca375b6874538391a9b6d5744
Jigar-f <jigar@getlantern.org>
2023-05-24 12:08:24 +0530

    Migrate Live and Local Test to share same code.

---
+++ appium_kotlin/app/src/test/java/appium_kotlin/appium_flutter_driver/finder/pageback.kt
@@ -0,0 +1,7 @@
+@file:JvmName("_FinderRawMethods")
+@file:JvmMultifileClass
+package appium_flutter_driver.finder
+
+fun pageBack(): FlutterElement {
+  return FlutterElement(mapOf("finderType" to "PageBack"))
+}
