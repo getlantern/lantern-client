bd127dd8a1a78686b122c8d7986f0ccaef8e981f
Jigar-f <jigar@getlantern.org>
2023-05-24 18:44:08 +0530

    Change LOGS_DIALED_MESSAGE message.

--- appium_kotlin/app/src/test/java/appium_kotlin/Const.kt
+++ appium_kotlin/app/src/test/java/appium_kotlin/Const.kt
@@ -8,11 +8,9 @@ enum class ContextType {
     WEBVIEW_CHROME
 }
 
-const val LANTERN_APK_PATH =
-    "/Users/jigarfumakiya/Documents/getlantern/mobile_app/android-lantern/build/app/outputs/flutter-apk/app-prod-debug.apk"
 const val LANTERN_PACKAGE_ID = "org.getlantern.lantern"
 const val CHROME_PACKAGE_ID = "com.android.chrome"
 const val CHROME_PACKAGE_ACTIVITY = "org.chromium.chrome.browser.ChromeTabbedActivity"
 const val IP_REQUEST_URL = "https://api64.ipify.org"
-const val LOGS_DIALED_MESSAGE = "DEBUG balancer: balancer.go:420 Successfully dialed via"
+const val LOGS_DIALED_MESSAGE = "Successfully dialed via"
 
