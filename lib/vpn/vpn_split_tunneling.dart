bc11bf1e0bc5b14b368e0965991034c3acf18b63
Ox Cart <ox.to.a.cart@gmail.com>
2023-05-24 22:17:16 -0500

    Fixed speed and sorting of apps list in Split Tunneling

--- lib/vpn/vpn_split_tunneling.dart
+++ lib/vpn/vpn_split_tunneling.dart
@@ -1,11 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:lantern/common/common.dart';
-import 'package:lantern/i18n/localization_constants.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:lantern/vpn/vpn.dart';
-import 'dart:developer' as dev;
 
 class SplitTunneling extends StatefulWidget {
   SplitTunneling({Key? key});
@@ -26,7 +22,7 @@ class _SplitTunnelingState extends State<SplitTunneling> {
     });
   }
 
-  init() async {
+  void init() async {
     var _vpnConnected = await vpnModel.isVpnConnected();
     setState(() {
       vpnConnected = _vpnConnected;
@@ -36,76 +32,85 @@ class _SplitTunnelingState extends State<SplitTunneling> {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-        title: 'split_tunneling'.i18n,
-        body: vpnModel.splitTunneling(
-            (BuildContext context, bool splitTunnelingEnabled, Widget? child) {
-          return vpnModel.appsData(builder: (
+      title: 'split_tunneling'.i18n,
+      body: vpnModel.splitTunneling(
+          (BuildContext context, bool splitTunnelingEnabled, Widget? child) {
+        return vpnModel.appsData(
+          builder: (
             context,
             Iterable<PathAndValue<AppData>> _appsData,
             Widget? child,
           ) {
-            var appsData = _appsData.toList();
-            appsData.sort((a, b) => a.value.name!.compareTo(b.value.name!));
-            return ListView(
-              shrinkWrap: true,
-              children: [Column(children: <Widget>[
-              ListItemFactory.settingsItem(
-                icon: ImagePaths.split_tunneling,
-                content: 'split_tunneling'.i18n,
-                trailingArray: [
-                  SizedBox(
+            return Column(
+              children: <Widget>[
+                ListItemFactory.settingsItem(
+                  icon: ImagePaths.split_tunneling,
+                  content: 'split_tunneling'.i18n,
+                  trailingArray: [
+                    SizedBox(
                       width: 44.0,
                       height: 24.0,
                       child: CupertinoSwitch(
                         value: splitTunnelingEnabled,
                         activeColor: CupertinoColors.activeGreen,
                         onChanged: (bool? value) {
-                          bool newValue = value ?? false;
+                          var newValue = value ?? false;
                           setState(() {
                             vpnModel.setSplitTunneling(newValue);
                           });
                         },
-                      )),
-                ],
-              ),
-              Padding(
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
                   padding: const EdgeInsetsDirectional.only(top: 16),
                   child: CText(
-                      splitTunnelingEnabled
-                          ? 'apps_selected'.i18n
-                          : 'split_tunneling_info'.i18n,
-                      style: tsBody3)),
-              // if split tunneling is enabled, include the installed apps
-              // in the column
-              if (splitTunnelingEnabled) ...buildAppsLists(appsData),
-            ])]);
-          });
-        }));
+                    splitTunnelingEnabled
+                        ? 'apps_selected'.i18n
+                        : 'split_tunneling_info'.i18n,
+                    style: tsBody3,
+                  ),
+                ),
+                // if split tunneling is enabled, include the installed apps
+                // in the column
+                if (splitTunnelingEnabled) Expanded(child: buildAppsList(_appsData)),
+              ],
+            );
+          },
+        );
+      }),
+    );
   }
 
   // buildAppsLists builds lists for excluded and allowed installed apps and
   // returns both along with their associated headers
-  List<Widget> buildAppsLists(List<PathAndValue<AppData>> appsData) {
-    if (appsData.length == 0) return [];
-    return [
-      ListSectionHeader('excluded_apps'.i18n.toUpperCase()),
-      ...buildAppList(appsData.where((app) => app.value.isExcluded).toList()),
-      ListSectionHeader('allowed_apps'.i18n.toUpperCase()),
-      ...buildAppList(appsData.where((app) => !app.value.isExcluded).toList()),
-    ];
-  }
+  ListView buildAppsList(Iterable<PathAndValue<AppData>> appsData) {
+    var excludedApps = appsData.where((app) => app.value.isExcluded).toList();
+    var allowedApps = appsData.where((app) => !app.value.isExcluded).toList();
 
-  List<Widget> buildAppList(List<PathAndValue<AppData>> apps) {
-    if (apps.length == 0) {
-      return [SizedBox.shrink()];
-    }
+    excludedApps.sort((a, b) => a.value.name.compareTo(b.value.name));
+    allowedApps.sort((a, b) => a.value.name.compareTo(b.value.name));
 
-    List<Widget> widgets = [];
-    for (var appData in apps) {
-      widgets.add(buildAppItem(appData.value));
-    }
-
-    return widgets;
+    return ListView.builder(
+      itemCount: appsData.isEmpty ? 0 : appsData.length + 2,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == 0) {
+          return ListSectionHeader('excluded_apps'.i18n.toUpperCase());
+        }
+        if (index == excludedApps.length+1) {
+          return ListSectionHeader('allowed_apps'.i18n.toUpperCase());
+        }
+        late PathAndValue<AppData> appData;
+        if (index <= excludedApps.length) {
+          appData = excludedApps[index - 1];
+        } else {
+          appData = allowedApps[index - excludedApps.length - 2];
+        }
+        var appItem = buildAppItem(appData.value);
+        return appItem;
+      },
+    );
   }
 
   // showSnackBar shows a snackbar with a message indicating that settings will be applied
@@ -115,67 +120,71 @@ class _SplitTunnelingState extends State<SplitTunneling> {
       return;
     }
     final snackBar = SnackBar(
-        behavior: SnackBarBehavior.floating,
-        duration: const Duration(seconds: 7),
-        backgroundColor: Colors.black,
-        content: CText(
-          'applied_next_time'.i18n,
-          style: tsSubtitle3.copiedWith(color: Colors.white),
-        ));
+      behavior: SnackBarBehavior.floating,
+      duration: const Duration(seconds: 7),
+      backgroundColor: Colors.black,
+      content: CText(
+        'applied_next_time'.i18n,
+        style: tsSubtitle3.copiedWith(color: Colors.white),
+      ),
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
     snackbarShown = true;
   }
 
   Widget buildAppItem(AppData appData) {
-    Uint8List iconBytes = base64.decode(appData.icon!);
-    var packageName = appData.packageName!;
+    var iconBytes = base64.decode(appData.icon);
+    var packageName = appData.packageName;
     return Container(
-        height: 72,
-        padding: EdgeInsets.zero,
-        decoration: BoxDecoration(
-          border:
-              Border(bottom: BorderSide(color: Color(0xFFEBEBEB), width: 1.0)),
+      height: 72,
+      padding: EdgeInsets.zero,
+      decoration: const BoxDecoration(
+        border:
+            Border(bottom: BorderSide(color: Color(0xFFEBEBEB), width: 1.0)),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: ListTile(
+          key: Key(appData.packageName),
+          minLeadingWidth: 20,
+          leading: ConstrainedBox(
+            constraints: const BoxConstraints(
+              minWidth: 24,
+              minHeight: 24,
+              maxWidth: 24,
+              maxHeight: 24,
+            ),
+            child: Image.memory(iconBytes, fit: BoxFit.cover),
+          ),
+          trailing: SizedBox(
+            height: 24.0,
+            width: 24.0,
+            child: Checkbox(
+              checkColor: Colors.white,
+              shape: const CircleBorder(),
+              activeColor: Colors.black,
+              side: const BorderSide(color: Colors.black),
+              value: appData.isExcluded,
+              onChanged: (bool? value) async {
+                setState(() {
+                  if (value != null && value) {
+                    vpnModel.addExcludedApp(packageName);
+                  } else {
+                    vpnModel.removeExcludedApp(packageName);
+                  }
+                  showSnackBar(context);
+                });
+              },
+            ),
+          ),
+          title: CText(
+            toBeginningOfSentenceCase(appData.name)!,
+            softWrap: false,
+            style: tsSubtitle3.short,
+          ),
         ),
-        child: Align(
-            alignment: Alignment.center,
-            child: ListTile(
-              key: Key(appData.packageName!),
-              minLeadingWidth: 20,
-              leading: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minWidth: 24,
-                  minHeight: 24,
-                  maxWidth: 24,
-                  maxHeight: 24,
-                ),
-                child: Image.memory(iconBytes, fit: BoxFit.cover),
-              ),
-              trailing: SizedBox(
-                  height: 24.0,
-                  width: 24.0,
-                  child: Checkbox(
-                    checkColor: Colors.white,
-                    shape: CircleBorder(),
-                    activeColor: Colors.black,
-                    side: BorderSide(color: Colors.black),
-                    value: appData.isExcluded,
-                    onChanged: (bool? value) async {
-                      setState(() {
-                        if (value != null && value!) {
-                          vpnModel.addExcludedApp(packageName);
-                        } else {
-                          vpnModel.removeExcludedApp(packageName);
-                        }
-                        showSnackBar(context);
-                      });
-                    },
-                  )),
-              title: CText(
-                toBeginningOfSentenceCase(appData.name)!,
-                softWrap: false,
-                style: tsSubtitle3.short,
-              ),
-            )));
+      ),
+    );
   }
 }
 
@@ -184,30 +193,36 @@ class SplitTunnelingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return vpnModel.splitTunneling(
-        (BuildContext context, bool value, Widget? child) => InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SplitTunneling(),
-                  ));
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CText(
-                  'split_tunneling'.i18n,
-                  style: tsSubtitle3.copiedWith(
-                    color: unselectedTabIconColor,
-                  ),
-                ),
-                new Spacer(),
-                Padding(
-                    padding: const EdgeInsetsDirectional.only(end: 8),
-                    child: CText(value ? 'on'.i18n : 'off'.i18n,
-                        style: tsSubtitle4)),
-                mirrorLTR(context: context, child: const ContinueArrow())
-              ],
-            )));
+      (BuildContext context, bool value, Widget? child) => InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SplitTunneling(),
+            ),
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CText(
+              'split_tunneling'.i18n,
+              style: tsSubtitle3.copiedWith(
+                color: unselectedTabIconColor,
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(end: 8),
+              child: CText(
+                value ? 'on'.i18n : 'off'.i18n,
+                style: tsSubtitle4,
+              ),
+            ),
+            mirrorLTR(context: context, child: const ContinueArrow())
+          ],
+        ),
+      ),
+    );
   }
 }
