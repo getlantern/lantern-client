2388542f4d8eba92d174eeb6421da87d3a056b5f
atavism <todd@lana.io>
2023-06-06 21:53:14 -0700

    Add InAppBilling test

---
+++ android/app/src/testPlay/kotlin/org/getlantern/lantern/model/InAppBillingTest.kt
@@ -0,0 +1,29 @@
+package org.getlantern.lantern.model
+
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.ConsumeParams
+import com.android.billingclient.api.ConsumeResponseListener
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchaseHistoryRecord
+import com.android.billingclient.api.PurchaseHistoryResponseListener
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.SkuDetails
+import com.android.billingclient.api.SkuDetailsParams
+import com.android.billingclient.api.SkuDetailsResponseListener
+
+class InAppBillingTest {
+
+	private var mockClient: BillingClient = mockk()
+
+
+	@Before
+    fun setUp() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    }
+}
