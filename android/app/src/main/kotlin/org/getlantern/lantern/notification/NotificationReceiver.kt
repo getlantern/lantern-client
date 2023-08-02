8b188183b51ec22ad808359681e98454300c6969
atavism <todd@lana.io>
2023-08-02 04:09:54 -0700

    Additional updates to VpnServiceManager, add service connection class

---
+++ android/app/src/main/kotlin/org/getlantern/lantern/notification/NotificationReceiver.kt
@@ -0,0 +1,29 @@
+package org.getlantern.lantern.notification
+
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.app.NotificationManager
+import io.lantern.model.VpnModel
+import org.getlantern.lantern.LanternApp
+import org.getlantern.lantern.model.Utils
+import org.getlantern.lantern.model.VpnState
+import org.getlantern.lantern.vpn.LanternVpnService
+import org.getlantern.lantern.vpn.VpnServiceManager
+import org.getlantern.mobilesdk.Logger
+import org.greenrobot.eventbus.EventBus
+
+
+class NotificationReceiver(private val serviceManager: VpnServiceManager) : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Logger.debug(TAG, "Received disconnect broadcast")
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(NotificationHelper.VPN_CONNECTED)
+        serviceManager.disconnect()
+    }
+
+    companion object {
+        private val TAG = NotificationReceiver::class.java.simpleName
+    }
+}
