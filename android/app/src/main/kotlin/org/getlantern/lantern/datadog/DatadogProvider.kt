50beccd73769add3a5e17b18b066e1e0f7d70a5f
atavism <todd@lana.io>
2023-08-18 03:16:53 -0700

    enable view tracking

--- android/app/src/main/kotlin/org/getlantern/lantern/datadog/DatadogProvider.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/datadog/DatadogProvider.kt
@@ -15,69 +15,81 @@ import org.getlantern.lantern.BuildConfig
 import org.getlantern.lantern.LanternApp
 import java.net.InetSocketAddress
 import java.net.Proxy
+import java.util.concurrent.atomic.AtomicBoolean
 
-class DatadogProvider {
-    companion object {
-        private val tracedHosts = listOf(
-            "datadoghq.com",
-            "127.0.0.1",
+object DatadogProvider {
+    private val tracedHosts = listOf(
+        "datadoghq.com",
+        "127.0.0.1",
+    )
+    private val initialized = AtomicBoolean()
+    private lateinit var datadogConfig: Configuration
+
+    fun initialize() {
+    	if (initialized.get()) return
+
+        Datadog.setVerbosity(Log.VERBOSE)
+        datadogConfig = createDatadogConfiguration()
+
+        val datadogCredentials = Credentials(
+            clientToken = BuildConfig.DD_CLIENT_TOKEN,
+            envName = "prod",
+            rumApplicationId = BuildConfig.DD_APPLICATION_ID,
+            variant = "release",
+            serviceName = "lantern-android",
         )
 
-        fun initialize() {
-            val session = LanternApp.getSession()
-            Datadog.setVerbosity(Log.VERBOSE)
-            val datadogConfig = Configuration.Builder(
-                logsEnabled = true,
-                tracesEnabled = true,
-                crashReportsEnabled = true,
-                rumEnabled = true,
-            )
-                .setBatchSize(BatchSize.SMALL)
-                .setProxy(
-                    Proxy(
-                        Proxy.Type.HTTP,
-                        InetSocketAddress(
-                            session.settings.httpProxyHost,
-                            session.settings.httpProxyPort.toInt(),
-                        ),
+        Datadog.initialize(
+            LanternApp.getAppContext(),
+            credentials = datadogCredentials,
+            configuration = datadogConfig,
+            TrackingConsent.GRANTED,
+        )
+
+        Datadog.setUserInfo(
+            id = LanternApp.getSession().userId().toString(),
+        )
+
+        val monitor = RumMonitor.Builder().build()
+        GlobalRum.registerIfAbsent(monitor)
+        initialized.set(true)
+    }
+
+    private fun createDatadogConfiguration(): Configuration {
+        val session = LanternApp.getSession()
+        return Configuration.Builder(
+            logsEnabled = true,
+            tracesEnabled = true,
+            crashReportsEnabled = true,
+            rumEnabled = true,
+        )
+            .setBatchSize(BatchSize.SMALL)
+            .setProxy(
+                Proxy(
+                    Proxy.Type.HTTP,
+                    InetSocketAddress(
+                        session.settings.httpProxyHost,
+                        session.settings.httpProxyPort.toInt(),
                     ),
-                    null,
-                )
-                .sampleRumSessions(100f)
-                .setUploadFrequency(UploadFrequency.FREQUENT)
-                .useSite(DatadogSite.EU1)
-                .trackInteractions()
-                .trackLongTasks()
-                .setFirstPartyHosts(tracedHosts)
-                .useViewTrackingStrategy(
-                    ActivityViewTrackingStrategy(
-                        trackExtras = false,
-                        componentPredicate = FlutterExcludingComponentPredicate(),
-                    ),
-                )
-                .build()
-
-            val datadogCredentials = Credentials(
-                clientToken = BuildConfig.DD_CLIENT_TOKEN,
-                envName = "prod",
-                rumApplicationId = BuildConfig.DD_APPLICATION_ID,
-                variant = "release",
-                serviceName = "lantern-android",
+                ),
+                null,
             )
-
-            Datadog.initialize(
-                LanternApp.getAppContext(),
-                credentials = datadogCredentials,
-                configuration = datadogConfig,
-                TrackingConsent.GRANTED,
+            .sampleRumSessions(100f)
+            .setUploadFrequency(UploadFrequency.FREQUENT)
+            .useSite(DatadogSite.EU1)
+            .trackInteractions()
+            .trackLongTasks()
+            .useViewTrackingStrategy(ActivityViewTrackingStrategy(
+                trackExtras = false,
+                componentPredicate = FlutterExcludingComponentPredicate()
+            ))
+            .setFirstPartyHosts(tracedHosts)
+            .useViewTrackingStrategy(
+                ActivityViewTrackingStrategy(
+                    trackExtras = false,
+                    componentPredicate = FlutterExcludingComponentPredicate(),
+                ),
             )
-
-            Datadog.setUserInfo(
-                id = LanternApp.getSession().userId().toString(),
-            )
-
-            val monitor = RumMonitor.Builder().build()
-            GlobalRum.registerIfAbsent(monitor)
-        }
+            .build()
     }
 }
