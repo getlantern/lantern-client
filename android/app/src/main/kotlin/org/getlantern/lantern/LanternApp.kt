6e448e2ac5e9f039f0da1b85cb0593aa53bea029
atavism <todd@lana.io>
2023-09-06 05:44:30 -0700

    only enable datadog with release builds

--- android/app/src/main/kotlin/org/getlantern/lantern/LanternApp.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/LanternApp.kt
@@ -48,9 +48,8 @@ open class LanternApp : Application() {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         appContext = applicationContext
         session = LanternSessionManager(this)
-        if (!BuildConfig.DEBUG) {
-            Datadog.initialize()
-        }
+        if (!BuildConfig.DEBUG) Datadog.initialize()
+        
         LanternProxySelector(session)
 
         if (session.isPlayVersion) inAppBilling = InAppBilling(this)
