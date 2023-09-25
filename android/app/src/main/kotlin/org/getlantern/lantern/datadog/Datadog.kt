38f532a7a7baf3864edc3e1a409988395a365507
lumavpn <admin@lumavpn.com>
2023-09-25 14:28:26 -0700

    run ktlint, remove unused imports

--- android/app/src/main/kotlin/org/getlantern/lantern/datadog/Datadog.kt
+++ android/app/src/main/kotlin/org/getlantern/lantern/datadog/Datadog.kt
@@ -1,7 +1,6 @@
 package org.getlantern.lantern.datadog
 
 import android.util.Log
-import com.datadog.android.Datadog as DatadogMain
 import com.datadog.android.DatadogSite
 import com.datadog.android.core.configuration.BatchSize
 import com.datadog.android.core.configuration.Configuration
@@ -14,18 +13,18 @@ import com.datadog.android.rum.RumErrorSource
 import com.datadog.android.rum.RumMonitor
 import com.datadog.android.rum.tracking.ActivityViewTrackingStrategy
 import org.getlantern.lantern.LanternApp
-import org.getlantern.lantern.BuildConfig
 import org.getlantern.mobilesdk.Logger
-import org.getlantern.mobilesdk.model.SessionManager
 import java.net.InetSocketAddress
 import java.net.Proxy
 import java.util.concurrent.atomic.AtomicBoolean
+import com.datadog.android.Datadog as DatadogMain
 
 object Datadog {
-    private val tracedHosts = listOf(
-        "datadoghq.eu",
-        "127.0.0.1",
-    )
+    private val tracedHosts =
+        listOf(
+            "datadoghq.eu",
+            "127.0.0.1",
+        )
     private val initialized = AtomicBoolean()
     private lateinit var datadogConfig: Configuration
 
@@ -35,13 +34,14 @@ object Datadog {
         DatadogMain.setVerbosity(Log.VERBOSE)
         datadogConfig = createDatadogConfiguration()
 
-        val datadogCredentials = Credentials(
-            clientToken = "puba617ab01333a95a25a9d3709f04e1654",
-            envName = "prod",
-            rumApplicationId = "f8eabf3c-5db3-4f7e-8e6a-5a72433b46d2",
-            variant = "release",
-            serviceName = "lantern-android",
-        )
+        val datadogCredentials =
+            Credentials(
+                clientToken = "puba617ab01333a95a25a9d3709f04e1654",
+                envName = "prod",
+                rumApplicationId = "f8eabf3c-5db3-4f7e-8e6a-5a72433b46d2",
+                variant = "release",
+                serviceName = "lantern-android",
+            )
 
         DatadogMain.initialize(
             LanternApp.getAppContext(),
@@ -82,7 +82,7 @@ object Datadog {
         name: String,
         actionAttributes: Map<String, Any?> = emptyMap(),
     ) {
-         GlobalRum.get().addUserAction(actionType, name, actionAttributes)
+        GlobalRum.get().addUserAction(actionType, name, actionAttributes)
     }
 
     // trackUserClick is used to track user clicks with RumMonitor
