a0fe72e94224ecb7a8f2bd7f1430a49c64392ba3
Ox Cart <ox.to.a.cart@gmail.com>
2023-09-26 08:53:37 -0500

    Added testList to DBModuleTests

--- DBModule/Tests/DBModuleTests/DBModuleTests.swift
+++ DBModule/Tests/DBModuleTests/DBModuleTests.swift
@@ -1,12 +1,38 @@
 import XCTest
+import Internalsdk
 @testable import DBModule
 
-final class DBModuleTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documenation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class DBModuleTests: XCTestCase, TestsupportTestingTProtocol {
+    func errorf(_ text: String?) {
+        XCTFail(text!)
+    }
+    
+    func failNow() {
+        XCTFail("failing now!")
+    }
+    
+    func testList() throws {
+        let db = try newDB()
+        TestsupportTestList(self, db)
+    }
+    
+    private func newDB() throws -> MinisqlDBProtocol {
+        return try DatabaseFactory.getDbManager(databasePath: newDBPath())
+    }
+    
+    private func newDBPath() -> String {
+        let fileManager = FileManager.default
+        let directory = fileManager.temporaryDirectory
+        let subdirectory = UUID().uuidString
+        let dbDir = directory.appendingPathComponent(subdirectory)
+        
+        do {
+            try fileManager.createDirectory(at: dbDir, withIntermediateDirectories: true, attributes: nil)
+            let dbLocation = dbDir.appendingPathComponent("db").path
+            return dbLocation
+        } catch {
+            print("Error creating directory: \(error)")
+            return ""  // Return an empty string or handle the error accordingly.
+        }
     }
 }
