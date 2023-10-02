02494cdf4e95117623e8a2be61f01591d04d7d8a
Jigar-f <jigar@getlantern.org>
2023-10-02 13:23:08 +0530

    Removed not used files.

--- ios/LanternTests/LanternTests.swift
+++ ios/LanternTests/LanternTests.swift
@@ -10,48 +10,15 @@ import SQLite
 @testable import Runner
 import XCTest
 
-final class TestingT: NSObject, TestsupportTestingTProtocol {
-    var test = "unknown"
-    
-    func errorf(_ text: String?) {
-        XCTFail("Test \(test) failed with message \(text)")
-    }
-    
-    func failNow() {
-        XCTFail("Test \(test) failed")
-    }
-}
 
 final class LanternTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testList() throws {
-        try TestsupportTestList(TestingT(), getDB())
-    }
-
-    private func getDB() throws -> MinisqlDBProtocol {
-        let dbPath = getDatabasePath()
-        let db = try Connection(dbPath)
-        return DatabaseManager(database: db)
-    }
-            
-    private func getDatabasePath() -> String {
-        let fileManager = FileManager.default
-        let dbDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("masterDBv2")
-        do {
-            try fileManager.createDirectory(at: dbDir, withIntermediateDirectories: true, attributes: nil)
-            let dbLocation = dbDir.appendingPathComponent("db").path
-            return dbLocation
-        } catch {
-            print("Error creating directory: \(error)")
-            return ""  // Return an empty string or handle the error accordingly.
-        }
-    }
+    
 }
