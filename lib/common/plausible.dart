304a2a9789bc2c6a3e5f4dd61a2b2fd50dc4cf0a
atavism <paul@getlantern.org>
2023-10-10 08:36:30 -0700

    add Plausible class

---
+++ lib/common/plausible.dart
@@ -0,0 +1,9 @@
+import 'package:plausible_analytics/plausible_analytics.dart';
+
+class PlausibleUtils {
+  static trackUserAction(String name, [Map<String, String> props = const {}]) {
+      Plausible plausible = Plausible("https://plausible.io", "android.lantern.io");
+  	  // Send goal
+  	  plausible.event(name: name, props: props);
+  }
+}
