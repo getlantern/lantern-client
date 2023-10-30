31f0c619731c832c88efbfa93eb9f45efba33f5c
atavism <paul@getlantern.org>
2023-10-30 05:13:47 -0700

    apply formatting

--- appium_kotlin/app/src/test/java/appium_kotlin/tests/MobileUtils.kt
+++ appium_kotlin/app/src/test/java/appium_kotlin/tests/MobileUtils.kt
@@ -1,18 +1,20 @@
 package appium_kotlin.tests
 
+import appium_kotlin.LANTERN_PACKAGE_ID
 import io.appium.java_client.MobileBy
 import org.openqa.selenium.By
-import appium_kotlin.LANTERN_PACKAGE_ID
 
 class MobileUtils {
-	companion object {
-		fun id(id: String): By {
-			// MobileBy is optimised for for Android and IOS UIs instead of the generic By mainly for web locators.
-			return MobileBy.id(addPackageId(id))
-		}
+    companion object {
+        fun id(id: String): By {
+            // MobileBy is optimised for for Android and IOS UIs instead of the generic By mainly for web locators.
+            return MobileBy.id(addPackageId(id))
+        }
 
-		fun androidTextByBuilder(
-            text: String, scroll: Boolean = true, searchType: SearchTextOperator = SearchTextOperator.EXACT
+        fun androidTextByBuilder(
+            text: String,
+            scroll: Boolean = true,
+            searchType: SearchTextOperator = SearchTextOperator.EXACT,
         ): By {
             val elementByText = "UiSelector().${searchType.androidUiSelectorTextMethodName}(\"$text\")"
             val scrollableElement = "UiScrollable(UiSelector().scrollable(true).instance(0)).scrollIntoView($elementByText)"
@@ -25,15 +27,15 @@ class MobileUtils {
             return MobileBy.AndroidUIAutomator(selector)
         }
 
-		enum class SearchTextOperator(val androidUiSelectorTextMethodName: String, val iOSOperator: String) {
-	        MATCHES("textMatches", "MATCHES"),
-	        EXACT("text", "=="),
-	        CONTAINS("textContains", "CONTAINS"),
-	        STARTS_WITH("textStartsWith", "BEGINSWITH")
-	    }
+        enum class SearchTextOperator(val androidUiSelectorTextMethodName: String, val iOSOperator: String) {
+            MATCHES("textMatches", "MATCHES"),
+            EXACT("text", "=="),
+            CONTAINS("textContains", "CONTAINS"),
+            STARTS_WITH("textStartsWith", "BEGINSWITH"),
+        }
 
         private fun addPackageId(id: String): String {
             return if (id.contains(":")) id else "${LANTERN_PACKAGE_ID}$id"
         }
-	}
+    }
 }
