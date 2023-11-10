f1ee8aeaf7b52fdf7e16a030399220eb18480c74
atavism <paul@getlantern.org>
2023-11-10 15:06:09 -0800

    Add system tray code

--- lib/main_desktop.dart
+++ lib/main_desktop.dart
@@ -5,10 +5,46 @@ import 'package:flutter/services.dart';
 import 'package:lantern/app.dart';
 import 'package:lantern/app_desktop.dart';
 import 'package:lantern/ffi.dart';
+import 'package:system_tray/system_tray.dart';
 
 Future<void> main() async {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   WidgetsFlutterBinding.ensureInitialized();
   loadLibrary();
   runApp(DesktopApp());
+}
+
+Future<void> initSystemTray() async {
+  String path =
+      Platform.isWindows ? 'assets/app_icon.ico' : 'assets/app_icon.png';
+
+  final AppWindow appWindow = AppWindow();
+  final SystemTray systemTray = SystemTray();
+
+  // We first init the systray menu
+  await systemTray.initSystemTray(
+    title: "system tray",
+    iconPath: path,
+  );
+
+  // create context menu
+  final Menu menu = Menu();
+  await menu.buildFrom([
+    MenuItemLabel(label: 'Show', onClicked: (menuItem) => appWindow.show()),
+    MenuItemLabel(label: 'Hide', onClicked: (menuItem) => appWindow.hide()),
+    MenuItemLabel(label: 'Exit', onClicked: (menuItem) => appWindow.close()),
+  ]);
+
+  // set context menu
+  await systemTray.setContextMenu(menu);
+
+  // handle system tray event
+  systemTray.registerSystemTrayEventHandler((eventName) {
+    debugPrint("eventName: $eventName");
+    if (eventName == kSystemTrayEventClick) {
+       Platform.isWindows ? appWindow.show() : systemTray.popUpContextMenu();
+    } else if (eventName == kSystemTrayEventRightClick) {
+       Platform.isWindows ? systemTray.popUpContextMenu() : appWindow.show();
+    }
+  });
 }
