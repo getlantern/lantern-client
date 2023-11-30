b93e073cc6ac4f98fbd25a4f41552616ab445228
Jigar-f <jigar@getlantern.org>
2023-11-30 18:53:00 +0530

    Added input validation across multiple screens

--- lib/app.dart
+++ lib/app.dart
@@ -121,8 +121,18 @@ class _LanternAppState extends State<LanternApp> {
           (context, lang, child) {
             Localization.locale = lang;
             return GlobalLoaderOverlay(
-              overlayColor: Colors.black,
-              overlayOpacity: 0.6,
+              useDefaultLoading: false,
+              overlayColor: Colors.black.withOpacity(0.5),
+              overlayWidget: Center(
+                child: AnimatedLoadingBorder(
+                  borderWidth: 5,
+                  borderColor: yellow3,
+                  cornerRadius: 100,
+                  child: SvgPicture.asset(
+                    ImagePaths.lantern_logo,
+                  ),
+                ),
+              ),
               child: I18n(
                 initialLocale: currentLocale(lang),
                 child: MaterialApp.router(
