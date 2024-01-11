7c5af521330bca45fee57c2c060ca25a89c2f48f
atavism <paul@getlantern.org>
2024-01-11 13:54:49 -0800

    Update notarize-darwin.py

--- installer-resources-lantern/tools/notarize-darwin.py
+++ installer-resources-lantern/tools/notarize-darwin.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-import enum
 import json
 import os
 import subprocess
 import sys
 import time
-import urllib.request
-
-import xml.etree.ElementTree as ET
 
 
 description = """
 Notarizes a dmg file and staples the resulting notarization ticket to the file. The arguments are
-passed to the xcrun altool utility, so more info can be obtained through 'xcrun altool --help'.
+passed to the xcrun notarytool utility, so more info can be obtained through 'xcrun notarytool --help'.
 
 Requires Python 3.
 """
@@ -35,137 +31,68 @@ POLL_MAX_RETRIES = 3
 
 DEV_NULL = open(os.devnull, 'w')
 
-
-class ParsingException(Exception):
-    pass
-
-
-class NotarizationResult(enum.Enum):
-    in_progress = 1
-    success = 2
-    failure = 3
-
-
 def print_stderr(*objects):
     print("%s:" % os.path.basename(__file__), *objects, file=sys.stderr)
 
 
-def xml_dict_find(xml_dict, key):
-    """ Returns the value associated with the given key in an XML dictionary structure. """
+def get_notary_call(command, param_list):
 
-    if len(xml_dict) % 2 != 0:
-        print(len(xml_dict))
-        raise ParsingException("Expected dictionary to have an even number of elements (key, value pairs)")
-    dict_iter = iter(xml_dict)
-    for child in dict_iter:
-        value = next(dict_iter)
-        if child.text == key:
-            return value
-    return None
+    cmd = ["xcrun", "notarytool", command] + param_list + ["--output-format", "json"]
+    com_process = subprocess.run(cmd, text=True, capture_output=True)
 
+    com_result = com_process.returncode
+    com_stdout = com_process.stdout
 
-def get_error_message(upload_xml):
-    """ Parses the error message out of the XML output from a notarization upload request. """
+    if com_result != 0:
+        print_stderr(f"Error results: {com_process.stderr}")
+    print_stderr(f"{command} results: {com_stdout}")
 
     try:
-        root = ET.fromstring(upload_xml)
-    except Exception as e:
-        print(upload_xml, file=sys.stderr)
-        raise ParsingException(e)
-    d = root.find("dict")
-    if d == None:
-        raise ParsingException("Expected element 'dict' in root")
-    errors = xml_dict_find(d, "product-errors")
-    if errors == None:
-        raise ParsingException("Expected key 'product-errors' in root.dict")
-    errors_dict = errors.find("dict")
-    if errors_dict == None:
-        raise ParsingException("Expected element 'dict' in root.dict.product-errors")
-    message = xml_dict_find(errors_dict, "message")
-    if message == None:
-        raise ParsingException("Expected key 'message' in root.dict.product-errors.dict")
-    return message.text
+        output_json = json.loads(com_stdout)
+        return com_result, output_json
+    except json.decoder.JSONDecodeError as e:
+        print(com_stdout, file=sys.stderr)
+        print_stderr(f"Error running {command}, please check your credentials and inputs")
+        print(e)
+        raise e
 
 
-def get_request_id(upload_xml):
-    """ Parses the request ID out of the XML output from a notarization upload request. """
-
+def get_info(credentials, request_id):
     try:
-        root = ET.fromstring(upload_xml)
-    except Exception as e:
-        raise ParsingException(e)
-    d = root.find("dict")
-    if d == None:
-        raise ParsingException("Expected element 'dict' in root")
-    upload = xml_dict_find(d, "notarization-upload")
-    if upload == None:
-        raise ParsingException("Expected key 'notarization-upload' in root.dict")
-    request_id = xml_dict_find(upload, "RequestUUID")
-    if request_id == None:
-        raise ParsingException("Expected key 'RequestUUID' in root.dict.notarization-upload")
-    return request_id.text
+        return get_notary_call("info",[ request_id, 
+                "--apple-id", credentials["username"],
+                "--password", credentials["password"],
+                "--team-id", credentials["team-id"]
+                ]
+            )
+    except json.decoder.JSONDecodeError as e:
+        print_stderr(f"Error fetching request info for {request_id}")
+        exit(1)
 
 
-def get_already_uploaded_request_id(upload_xml):
-    """ Parses the request ID out of the XML output from a redundant upload request. """
-
-    search_string = "The software asset has already been uploaded. The upload ID is "
-    message = get_error_message(upload_xml)
-    start = message.index(search_string) + len(search_string)
-    end = start + message[start:].index("\"")
-    return message[start:end]
-
-def get_notarization_result(info_xml):
-    """ Parses a notarization result out of the XML output from an info request. """
+def upload_file(credentials, filepath):
     try:
-        root = ET.fromstring(info_xml)
-    except Exception as e:
-        raise ParsingException(e)
-    d = root.find("dict")
-    if d == None:
-        raise ParsingException("Expected element 'dict' in root")
-    notarization_info = xml_dict_find(d, "notarization-info")
-    if notarization_info == None:
-        raise ParsingException("Expected key 'notarization-info' in root.dict")
-    status = xml_dict_find(notarization_info, "Status")
-    if status == None:
-        raise ParsingException("Expected key 'Status' in root.dict.notarization-info")
-    if status.text == "success":
-        return NotarizationResult.success
-    if status.text == "in progress":
-        return NotarizationResult.in_progress
-    if status.text == "invalid":
-        return NotarizationResult.failure
-    raise ParsingException("Unexpected value for root.dict.notarization-info.Status: %s" % status.text)
+        return get_notary_call("submit",[ 
+                "--apple-id", credentials["username"],
+                "--password", credentials["password"],
+                "--team-id", credentials["team-id"], filepath]
+            )
+    except json.decoder.JSONDecodeError as e:
+        print_stderr(f"Error uploading {filepath}")
+        exit(1)
 
 
-def get_log_url(info_xml):
-    """ Parses the log file URL out of the XML output from an info request. """
+def get_log(credentials, request_id):
     try:
-        root = ET.fromstring(info_xml)
-    except Exception as e:
-        raise ParsingException(e)
-    d = root.find("dict")
-    if d == None:
-        raise ParsingException("Expected element 'dict' in root")
-    notarization_info = xml_dict_find(d, "notarization-info")
-    if notarization_info == None:
-        raise ParsingException("Expected key 'notarization-info' in root.dict")
-    log_url = xml_dict_find(notarization_info, "LogFileURL")
-    if log_url == None:
-        raise ParsingException("Expected key 'LogFileURL' in root.dict.notarization-info")
-    return log_url.text
-
-
-def retrieve_and_parse_log(info_xml):
-    """ Retrieves the notarization log file and parses out any issues (including warnings). """
-    log_url = get_log_url(info_stdout)
-    log_json = urllib.request.urlopen(log_url).read()
-    log = json.loads(log_json)
-    try:
-        return log['issues']
-    except KeyError:
-        raise ParsingException("Expected key 'issues' in log JSON")
+        return get_notary_call("log",[ request_id, 
+                "--apple-id", credentials["username"],
+                "--password", credentials["password"],
+                "--team-id", credentials["team-id"]
+                ]
+            )
+    except json.decoder.JSONDecodeError as e:
+        print_stderr(f"Error fetching log for {request_id}")
+        exit(1)
 
 
 if __name__ == '__main__':
@@ -174,14 +101,12 @@ if __name__ == '__main__':
         epilog=epilog,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("dmgfile")
-    parser.add_argument("-b", "--primary-bundle-id", dest="bundle_id", required=True,
-        help="Used to uniquely identify the package.")
     parser.add_argument("-u", "--username", dest="username", required=True,
         help="Apple ID used for the Apple Developer Program.")
+    parser.add_argument("-a", "--team-id ", dest="team_id", required=True,
+        help="Developer Team ID used for the Apple Developer Program.")
     parser.add_argument("-p", "--password", dest="password", required=True,
-        help="May be provided through keychain or env var; see 'xcrun altool --help'.")
-    parser.add_argument("-a", "--asc-provider", dest="asc_provider", required=True,
-        help="Used to determine which provider to associate the file with.")
+        help="May be provided through keychain or env var; see 'xcrun notarytool --help'.")
     parser.add_argument("-t", "--max-wait-time", dest="max_wait_time", type=int, default=120,
         help="The maximum amount of time (in minutes) to wait for a notarization result.")
     args = parser.parse_args()
@@ -193,33 +118,30 @@ if __name__ == '__main__':
         exit(0)
 
     print_stderr("uploading to notary servers...")
-    upload_process = subprocess.Popen(["xcrun", "altool",
-        "--notarize-app",
-        "--username", args.username,
-        "--password", args.password,
-        "--primary-bundle-id", args.bundle_id,
-        "--asc-provider", args.asc_provider,
-        "--file", args.dmgfile,
-        "--output-format", "xml"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    upload_result = upload_process.wait()
-    upload_stdout = upload_process.stdout.read(-1).decode("utf-8")
+
+    credentials = { "username": args.username,
+                    "password": args.password,
+                    "team-id": args.team_id}
+
+    upload_result, upload_json = upload_file(credentials, args.dmgfile)
+    message = upload_json.get("message")
+
+    print_stderr(f"Upload process complete, message: {message}")
 
     if upload_result == 0:
         print_stderr("upload complete")
-        try:
-            request_id = get_request_id(upload_stdout)
-        except ParsingException as e:
-            print(upload_stdout, file=sys.stderr)
-            print_stderr("failed to parse output of upload process:", e)
+        request_id = upload_json.get("id")
+        if not request_id:
+            print_stderr("Request id not found in upload output, aborting")
             exit(1)
+
     else:
-        if "already been uploaded" in get_error_message(upload_stdout):
+
+        if "already been uploaded" in message:
             print_stderr("already uploaded")
-            request_id = get_already_uploaded_request_id(upload_stdout)
+            exit(1)
         else:
-            print(upload_stdout, file=sys.stderr)
+            print(upload_json, file=sys.stderr)
             print_stderr("upload failed")
             exit(upload_result)
 
@@ -229,63 +151,64 @@ if __name__ == '__main__':
     consecutive_retries = 0
     processing_complete = False
     while not processing_complete and time.time() < poll_end:
-        info_process = subprocess.Popen(["xcrun", "altool",
-            "--notarization-info", request_id,
-            "--username", args.username,
-            "--password", args.password,
-            "--output-format", "xml"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        if info_process.wait() != 0:
+
+        print_stderr("polling results: ")
+        info_result, info_json = get_info(credentials, request_id)
+
+        if info_result != 0:
             consecutive_retries += 1
             if consecutive_retries >= POLL_MAX_RETRIES:
-                print(info_process.stdout.read(-1).decode("utf-8"), file=sys.stderr)
+                print(info_json, file=sys.stderr)
                 print_stderr("%d consecutive failures polling for status; giving up" % POLL_MAX_RETRIES)
                 exit(1)
             time.sleep(POLL_WAIT_SECONDS)
             continue
         consecutive_retries = 0
-        info_stdout = info_process.stdout.read(-1).decode("utf-8")
-        try:
-            status = get_notarization_result(info_stdout)
-            if status == NotarizationResult.success:
-                print_stderr("notarization succeeded")
-                processing_complete = True
-            elif status == NotarizationResult.failure:
-                print_stderr("notarization failed")
-                try:
-                    issues = retrieve_and_parse_log(info_stdout)
-                    if len(issues) == 0:
-                        print_stderr("found no issues in log file")
-                        print_stderr("url:", get_log_url(info_stdout))
-                        exit(1)
-                    print_stderr("issues:")
-                    for issue in issues:
-                        print(issue, file=sys.stderr)
-                except Exception as e:
-                    print(info_stdout, file=sys.stderr)
-                    print_stderr("failed to retrieve log file", e)
-                    exit(1)
-                exit(2)
-            elif status == NotarizationResult.in_progress:
-                time.sleep(POLL_WAIT_SECONDS)
-        except ParsingException as e:
-            print(info_stdout, file=sys.stderr)
-            print_stderr("failed to parse output of info process:", e)
+
+        status = info_json.get("status").lower()
+        if not status:
+            print(info_json, file=sys.stderr)
+            print_stderr("Status not present, malformed info response")
+            exit(1)
+        
+        if status == "success" or status == "accepted":
+            print_stderr("notarization succeeded")
+            processing_complete = True
+
+        elif status == "invalid":
+            print_stderr("notarization failed")
+
+            log_result, log_json = get_log(credentials, request_id)
+            issues = log_json.get("issues")
+            if not issues:
+                print_stderr("found no issues in log file")
+                print_stderr("request_id:", request_id)
+                exit(1)
+            print_stderr("issues:")
+            for issue in issues:
+                print(issue, file=sys.stderr)
+    
+        elif status == "in progress":
+            time.sleep(POLL_WAIT_SECONDS)
+        else:
+            print_stderr("Unknown Status in log")
+            print_stderr(info_json)
             exit(1)
 
     print_stderr("pulling log file...")
+
     try:
-        issues = retrieve_and_parse_log(info_stdout)
-        if len(issues) == 0:
+        log_result, log_json = get_log(credentials, request_id)
+        issues = info_json.get("issues")
+
+        if issues is None or len(issues) == 0:
             print_stderr("found no issues in log file")
-            print_stderr("url:", get_log_url(info_stdout))
         else:
             print_stderr("found issues in log file:")
             for issue in issues:
                 print(issue, file=sys.stderr)
     except Exception as e:
-        print(info_stdout, file=sys.stderr)
+        print(info_json, file=sys.stderr)
         print_stderr("failed to retrieve log file", e)
 
     print_stderr("stapling notarization ticket to file")
