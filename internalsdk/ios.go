869fb089e7e3f9154a90cd87e66ada00119d4b47
atavism <paul@getlantern.org>
2024-03-14 15:26:38 -0700

    Update ios.go

--- internalsdk/ios.go
+++ internalsdk/ios.go
@@ -1,8 +1,12 @@
 package internalsdk
 
 import (
+	"net"
 	"path/filepath"
+	"strconv"
+	"time"
 
+	"github.com/getlantern/flashlight/v7/client"
 	"github.com/getlantern/flashlight/v7/common"
 	"github.com/getlantern/flashlight/v7/logging"
 )
@@ -17,10 +21,21 @@ func NewService(sessionModel *SessionModel) *LanternService {
 	}
 }
 
-func (s *LanternService) start(configDir string, locale string, settings Settings) {
+func (s *LanternService) Start(configDir string, locale string, settings Settings) {
 	logging.EnableFileLogging(common.DefaultAppName, filepath.Join(configDir, "logs"))
 	session := &panickingSessionImpl{s.sessionModel}
 	startOnce.Do(func() {
 		go run(configDir, locale, settings, session)
 	})
+}
+
+func HTTPProxyPort() int {
+	if addr, ok := client.Addr(6 * time.Second); ok {
+		if _, p, err := net.SplitHostPort(addr.(string)); err == nil {
+			port, _ := strconv.Atoi(p)
+			return port
+		}
+	}
+	log.Errorf("Couldn't retrieve HTTP proxy addr in time")
+	return 0
 }
