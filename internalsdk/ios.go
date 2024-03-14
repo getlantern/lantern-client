dd6de052f2cd5b4519f62a08f602708594470226
atavism <paul@getlantern.org>
2024-03-14 15:13:06 -0700

    Add ios.go

--- internalsdk/ios.go
+++ internalsdk/ios.go
@@ -1,5 +1,12 @@
 package internalsdk
 
+import (
+	"path/filepath"
+
+	"github.com/getlantern/flashlight/v7/common"
+	"github.com/getlantern/flashlight/v7/logging"
+)
+
 type LanternService struct {
 	sessionModel *SessionModel
 }
@@ -8,4 +15,12 @@ func NewService(sessionModel *SessionModel) *LanternService {
 	return &LanternService{
 		sessionModel,
 	}
+}
+
+func (s *LanternService) start(configDir string, locale string, settings Settings) {
+	logging.EnableFileLogging(common.DefaultAppName, filepath.Join(configDir, "logs"))
+	session := &panickingSessionImpl{s.sessionModel}
+	startOnce.Do(func() {
+		go run(configDir, locale, settings, session)
+	})
 }
