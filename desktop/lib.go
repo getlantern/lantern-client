b9b7b673ca7e93c4bc57cb83bbaeb33b5e3df486
atavism <paul@getlantern.org>
2024-03-24 18:33:42 -0700

    clean-ups, do not fetch user data when checking email address

--- desktop/lib.go
+++ desktop/lib.go
@@ -175,34 +175,46 @@ func paymentMethods() *C.char {
 	return C.CString(string(b))
 }
 
+func getUserData() (*client.User, error) {
+	resp, err := proClient.UserData(userConfig())
+	if err != nil {
+		return nil, err
+	}
+	user := resp.User
+	if user.Email != "" {
+		a.Settings().SetEmailAddress(user.Email)
+	}
+	return &user, nil
+}
+
 //export devices
 func devices() *C.char {
-	resp, err := proClient.UserData(userConfig())
+	user, err := getUserData()
 	if err != nil {
 		return sendError(err)
 	}
-	b, _ := json.Marshal(resp.User.Devices)
+	b, _ := json.Marshal(user.Devices)
 	return C.CString(string(b))
 }
 
 //export expiryDate
 func expiryDate() *C.char {
-	resp, err := proClient.UserData(userConfig())
+	user, err := getUserData()
 	if err != nil {
 		return sendError(err)
 	}
-	tm := time.Unix(resp.User.Expiration, 0)
+	tm := time.Unix(user.Expiration, 0)
 	exp := tm.Format("01/02/2006")
 	return C.CString(string(exp))
 }
 
 //export userData
 func userData() *C.char {
-	resp, err := proClient.UserData(userConfig())
+	user, err := getUserData()
 	if err != nil {
 		return sendError(err)
 	}
-	b, _ := json.Marshal(resp.User)
+	b, _ := json.Marshal(user)
 	return C.CString(string(b))
 }
 
@@ -223,17 +235,7 @@ func serverInfo() *C.char {
 
 //export emailAddress
 func emailAddress() *C.char {
-	settings := a.Settings()
-	emailAddress := settings.GetEmailAddress()
-	if emailAddress == "" {
-		resp, err := proClient.UserData(userConfig())
-		if err != nil {
-			return sendError(err)
-		}
-		emailAddress = resp.User.Email
-		settings.SetEmailAddress(emailAddress)
-	}
-	return C.CString(emailAddress)
+	return C.CString(a.Settings().GetEmailAddress())
 }
 
 //export emailExists
