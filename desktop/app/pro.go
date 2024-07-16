4138e701932d8500593a97fb431353858ab55249
atavism <paul@getlantern.org>
2024-07-16 09:47:58 -0700

    remove commented lines

--- desktop/app/pro.go
+++ desktop/app/pro.go
@@ -203,17 +203,6 @@ func (app *App) servePro(channel ws.UIChannel) error {
 		}
 	}()
 
-	// helloFn := func(write func(interface{})) {
-	// 	if user, known := GetUserDataFast(ctx, app.settings.GetUserID()); known {
-	// 		log.Debugf("Sending current user data to new client: %v", user)
-	// 		write(user)
-	// 	}
-	// 	log.Debugf("Fetching user data again to see if any changes")
-	// 	select {
-	// 	case chFetch <- true:
-	// 	default: // fetching in progress, skipping
-	// 	}
-	// }
 	service, err := channel.Register("pro", nil)
 	if err != nil {
 		return err
