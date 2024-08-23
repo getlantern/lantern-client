bfb1a598e39a4aa64ed00c92f55658a524dc1732
atavism <paul@getlantern.org>
2024-08-22 17:58:03 -0700

    payment methods updates

---
+++ desktop/app/config.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"strconv"
+	"sync"
+
+	fcommon "github.com/getlantern/flashlight/v7/common"
+	"github.com/getlantern/flashlight/v7/config"
+	"github.com/getlantern/lantern-client/desktop/ws"
+	"github.com/getlantern/lantern-client/internalsdk/common"
+	"github.com/getlantern/lantern-client/internalsdk/protos"
+)
+
+type configService struct {
+	service     *ws.Service
+	listeners   []func(ConfigOptions)
+	listenersMx sync.RWMutex
+}
+
+// ConfigOptions are the config options that Lantern is running with
+type ConfigOptions struct {
+	DevelopmentMode      bool                   `json:"developmentMode"`
+	ReplicaAddr          string                 `json:"replicaAddr"`
+	AuthEnabled          bool                   `json:"authEnabled"`
+	HasSucceedingProxy   bool                   `json:"hasSucceedingProxy"`
+	FetchedGlobalConfig  bool                   `json:"fetchedGlobalConfig"`
+	FetchedProxiesConfig bool                   `json:"fetchedProxiesConfig"`
+	Plans                []protos.Plan          `json:"plans"`
+	PaymentMethods       []protos.PaymentMethod `json:"paymentMethods"`
+	SdkVersion           string                 `json:"sdkVersion"`
+	AppVersion           string                 `json:"appVersion"`
+}
+
+func (s *configService) StartService(channel ws.UIChannel) (err error) {
+	s.service, err = channel.Register("config", nil)
+	return err
+}
+
+func (s *configService) sendConfigOptions(cfg ConfigOptions) {
+	b, _ := json.Marshal(&cfg)
+	log.Debugf("Sending config options to client %s", string(b))
+	s.service.Out <- cfg
+}
+
+// AddListener adds a listener for any updates to the startup
+func (s *configService) AddListener(f func(ConfigOptions)) {
+	s.listenersMx.Lock()
+	s.listeners = append(s.listeners, f)
+	s.listenersMx.Unlock()
+}
+
+func (app *App) sendConfigOptions() {
+	authEnabled := func(a *App) bool {
+		authEnabled := a.IsFeatureEnabled(config.FeatureAuth)
+		if ok, err := strconv.ParseBool(os.Getenv("ENABLE_AUTH_FEATURE")); err == nil && ok {
+			authEnabled = true
+		}
+		log.Debugf("DEBUG: Auth enabled: %v", authEnabled)
+		return authEnabled
+	}
+	ctx := context.Background()
+	plans, _ := app.Plans(ctx)
+	paymentMethods, _ := app.PaymentMethods(ctx)
+
+	app.configService.sendConfigOptions(ConfigOptions{
+		DevelopmentMode:      common.IsDevEnvironment(),
+		ReplicaAddr:          "",
+		AuthEnabled:          authEnabled(app),
+		HasSucceedingProxy:   app.HasSucceedingProxy(),
+		Plans:                plans,
+		PaymentMethods:       paymentMethods,
+		FetchedGlobalConfig:  app.fetchedGlobalConfig.Load(),
+		FetchedProxiesConfig: app.fetchedProxiesConfig.Load(),
+		SdkVersion:           fcommon.LibraryVersion,
+	})
+}
---
+++ lib/common/config.dart
@@ -0,0 +1,63 @@
+import 'common.dart';
+import 'common_desktop.dart';
+import 'package:collection/collection.dart';
+import 'package:lantern/plans/utils.dart';
+import 'package:fixnum/fixnum.dart';
+
+extension BoolParsing on String {
+  bool parseBool() {
+    return this.toLowerCase() == 'true';
+  }
+}
+
+class ConfigOptions {
+  final bool developmentMode;
+  final String replicaAddr;
+  final bool authEnabled;
+  final bool hasSucceedingProxy;
+  final bool fetchedGlobalConfig;
+  final bool fetchedProxiesConfig;
+  final String sdkVersion;
+  final Map<String, Plan>? plans;
+  final Map<String, PaymentMethod>? paymentMethods;
+
+  ConfigOptions({
+    this.developmentMode = false,
+    this.replicaAddr = '',
+    this.authEnabled = false,
+    this.hasSucceedingProxy = false, 
+    this.fetchedGlobalConfig = false, 
+    this.fetchedProxiesConfig = false, 
+    this.sdkVersion = '',
+    this.plans = null,
+    this.paymentMethods = null,
+  });
+
+  bool get startupReady => hasSucceedingProxy && fetchedGlobalConfig && fetchedProxiesConfig;
+
+  factory ConfigOptions.fromJson(Map<String, dynamic> parsedJson) {
+    final Map<String, Plan> plans = {};
+    final plansResponse = parsedJson['plans'];
+    if (plansResponse is List<dynamic>) {
+      for (var item in plansResponse) {
+        var id = item['id'] ?? item['name'];
+        plans[id] = planFromJson(item) as Plan;
+      }
+    }
+    print("payment methods are ${parsedJson['paymentMethods']}");
+    final paymentMethods = paymentMethodsFromJson(parsedJson['paymentMethods']);
+    print("plans are $plans");
+    print("payment methods are $paymentMethods");
+    return ConfigOptions(
+      developmentMode: parsedJson['developmentMode'],
+      authEnabled: parsedJson['authEnabled'],
+      hasSucceedingProxy: parsedJson['hasSucceedingProxy'],
+      fetchedGlobalConfig: parsedJson['fetchedGlobalConfig'],
+      fetchedProxiesConfig: parsedJson['fetchedProxiesConfig'],
+      plans: plans,
+      paymentMethods: paymentMethods,
+      replicaAddr : parsedJson['replicaAddr'].toString(),
+      sdkVersion: parsedJson['sdkVersion'].toString() 
+    );
+  }
+}
