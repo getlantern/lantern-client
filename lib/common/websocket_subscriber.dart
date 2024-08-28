6c6322098ac5cb3b45f1dae4dcb3e298d0083776
Jigar-f <jigar@getlantern.org>
2024-08-28 14:45:41 +0530

    Updated debug messages.

--- lib/common/websocket_subscriber.dart
+++ lib/common/websocket_subscriber.dart
@@ -1,10 +1,18 @@
+import 'package:collection/collection.dart';
+import 'package:logger/logger.dart';
+
 import 'common.dart';
 import 'common_desktop.dart';
-import 'package:collection/collection.dart';
-import 'package:lantern/plans/utils.dart';
-import 'package:fixnum/fixnum.dart';
 
-typedef void WebsocketChange();
+var _webSocketLogger = Logger(
+  printer: PrettyPrinter(
+    printEmojis: true,
+    methodCount: 0,
+    errorMethodCount: 8,
+    colors: true,
+
+  ),
+);
 
 enum WebsocketMessage {
   config,
@@ -25,39 +33,46 @@ class WebsocketSubscriber {
     listenMessages();
   }
 
-  listenMessages() =>
+  void listenMessages() {
+    _webSocketLogger.i("Listening to websocket messages");
     _ws.messageStream.listen(
       (json) {
         if (json["type"] == null) return;
         final message = json["message"];
-        WebsocketMessage? messageType = WebsocketMessage.values.firstWhereOrNull((e) => e.name == json["type"]);
+        WebsocketMessage? messageType = WebsocketMessage.values
+            .firstWhereOrNull((e) => e.name == json["type"]);
         if (message == null || messageType == null) return;
         switch (messageType) {
           case WebsocketMessage.settings:
-            print("settings websocket message: $json");
+            _webSocketLogger.i("websocket message[Setting]: $json");
             final referralCode = message['referralCode'];
-            if (referralCode != null) sessionModel.referralNotifier.value = referralCode;
+            if (referralCode != null)
+              sessionModel.referralNotifier.value = referralCode;
           case WebsocketMessage.stats:
             if (message['countryCode'] != null) {
-              sessionModel.serverInfoNotifier.value = ServerInfo.create()..mergeFromProto3Json({
-                'city': message['city'],
-                'country': message['country'],
-                'countryCode': message['countryCode'],
-              });
+              sessionModel.serverInfoNotifier.value = ServerInfo.create()
+                ..mergeFromProto3Json({
+                  'city': message['city'],
+                  'country': message['country'],
+                  'countryCode': message['countryCode'],
+                });
             }
           case WebsocketMessage.pro:
-            print("pro websocket message: $message");
+            _webSocketLogger.i("Websocket message[Pro]: $json");
             final userStatus = message['userStatus'];
             final userLevel = message['userLevel'];
-            if (userStatus != null && userStatus == 'active') sessionModel.proUserNotifier.value = true;
-            if (userLevel != null && userLevel == 'pro') sessionModel.proUserNotifier.value = true;
+            if (userStatus != null && userStatus == 'active')
+              sessionModel.proUserNotifier.value = true;
+            if (userLevel != null && userLevel == 'pro')
+              sessionModel.proUserNotifier.value = true;
           case WebsocketMessage.bandwidth:
-            print("bandwidth websocket message: $message");
+            _webSocketLogger.i("Websocket message[Bandwidth]: $json");
             final Map res = jsonDecode(jsonEncode(message));
-            vpnModel.bandwidthNotifier.value = Bandwidth.create()..mergeFromProto3Json({
-              'allowed': res['mibAllowed'],
-              'remaining': res['mibUsed'],
-            });
+            vpnModel.bandwidthNotifier.value = Bandwidth.create()
+              ..mergeFromProto3Json({
+                'allowed': res['mibAllowed'],
+                'remaining': res['mibUsed'],
+              });
           case WebsocketMessage.config:
             final ConfigOptions config = ConfigOptions.fromJson(message);
             sessionModel.configNotifier.value = config;
@@ -66,7 +81,8 @@ class WebsocketSubscriber {
               sessionModel.plansNotifier.value.clearPaths();
               for (String key in plansMessage!.keys) {
                 final plan = config.plans?[key];
-                if (plan != null) sessionModel.plansNotifier.value.map[key] = plan;
+                if (plan != null)
+                  sessionModel.plansNotifier.value.map[key] = plan;
               }
             }
             final paymentMethods = config.paymentMethods;
@@ -74,21 +90,27 @@ class WebsocketSubscriber {
               sessionModel.paymentMethodsNotifier.value.clearPaths();
               for (String key in paymentMethods!.keys) {
                 final paymentMethod = config.paymentMethods?[key];
-                if (paymentMethod != null) sessionModel.paymentMethodsNotifier.value.map[key] = paymentMethod;
+                if (paymentMethod != null)
+                  sessionModel.paymentMethodsNotifier.value.map[key] =
+                      paymentMethod;
               }
             }
-            if (config.authEnabled) sessionModel.isAuthEnabled.value = config.authEnabled;
+            if (config.authEnabled)
+              sessionModel.isAuthEnabled.value = config.authEnabled;
           case WebsocketMessage.vpnstatus:
             final res = message["connected"];
             if (res != null) {
-              final vpnStatus = res.toString() == "true" ? "connected" : "disconnected";
-              print("vpn status $vpnStatus");
+              final vpnStatus =
+                  res.toString() == "true" ? "connected" : "disconnected";
+              _webSocketLogger.i("Websocket message[VPNStatus]: $vpnStatus");
               vpnModel.vpnStatusNotifier.value = vpnStatus;
             }
         }
       },
-      onError: (error) => appLogger.i("websocket error: ${error.description}"),
+      onError: (error) => _webSocketLogger
+          .e("websocket error: ${error.description}", error: error),
     );
+  }
 
   Future<void> connect() async => await _ws.connect();
 
