6c0e0e7e7fef34113e22635ff5319cc111a70ad4
Jigar-f <jigar@getlantern.org>
2024-09-03 11:35:01 +0530

    Update vpn_bandwidth.dart

--- lib/vpn/vpn_bandwidth.dart
+++ lib/vpn/vpn_bandwidth.dart
@@ -9,58 +9,59 @@ class VPNBandwidth extends StatelessWidget {
         .bandwidth((BuildContext context, Bandwidth bandwidth, Widget? child) {
       // User does not have bandwidth cap off
       if (bandwidth.remaining > 0) {
+        return Column(
+          children: [
+            Container(
+              margin: const EdgeInsetsDirectional.only(
+                top: 4.0,
+                bottom: 16.0,
+              ),
+              child: const CDivider(height: 10),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CText(
+                  'Daily Data Usage'.i18n,
+                  style: tsSubtitle3.copiedWith(
+                    color: unselectedTabIconColor,
+                  ),
+                ),
+                Expanded(
+                  child: CText(
+                    '${(bandwidth.allowed - bandwidth.remaining)}/${bandwidth.allowed} MB',
+                    textAlign: TextAlign.end,
+                    style: tsSubtitle4,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Container(
+              decoration: BoxDecoration(
+                color: unselectedTabColor,
+                border: Border.all(
+                  color: borderColor,
+                  width: 1,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(borderRadius),
+                ),
+              ),
+              child: LinearProgressIndicator(
+                value: (bandwidth.percent.toDouble() / 100).toDouble(),
+                minHeight: 12,
+                borderRadius:
+                    const BorderRadius.all(Radius.circular(borderRadius)),
+                backgroundColor: unselectedTabColor,
+                valueColor: AlwaysStoppedAnimation(usedDataBarColor),
+              ),
+            ),
+          ],
+        );
+      } else {
         return const SizedBox();
       }
-      return Column(
-        children: [
-          Container(
-            margin: const EdgeInsetsDirectional.only(
-              top: 4.0,
-              bottom: 16.0,
-            ),
-            child: const CDivider(height: 10),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CText(
-                'Daily Data Usage'.i18n,
-                style: tsSubtitle3.copiedWith(
-                  color: unselectedTabIconColor,
-                ),
-              ),
-              Expanded(
-                child: CText(
-                  '${(bandwidth.allowed - bandwidth.remaining)}/${bandwidth.allowed} MB',
-                  textAlign: TextAlign.end,
-                  style: tsSubtitle4,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Container(
-            decoration: BoxDecoration(
-              color: unselectedTabColor,
-              border: Border.all(
-                color: borderColor,
-                width: 1,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(borderRadius),
-              ),
-            ),
-            child: LinearProgressIndicator(
-              value: (bandwidth.percent.toDouble() / 100).toDouble(),
-              minHeight: 12,
-              borderRadius:
-                  const BorderRadius.all(Radius.circular(borderRadius)),
-              backgroundColor: unselectedTabColor,
-              valueColor: AlwaysStoppedAnimation(usedDataBarColor),
-            ),
-          ),
-        ],
-      );
     });
   }
 }
