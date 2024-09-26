506d5d52e0868b21e905504d7bd9be814f177b14
Jigar-f <jigar@getlantern.org>
2024-09-26 17:09:45 +0530

    use stateless widget.

--- lib/account/proxies_setting.dart
+++ lib/account/proxies_setting.dart
@@ -1,49 +1,35 @@
 import '../common/common.dart';
 
 @RoutePage(name: 'ProxiesSetting')
-class ProxiesSetting extends StatefulWidget {
+class ProxiesSetting extends StatelessWidget {
   const ProxiesSetting({super.key});
 
-  @override
-  State<ProxiesSetting> createState() => _ProxiesSettingState();
-}
-
-class _ProxiesSettingState extends State<ProxiesSetting> {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
       title: 'proxy_settings'.i18n,
-      body: _buildBody(),
+      body: _buildBody(context),
       padHorizontal: true,
       padVertical: true,
     );
   }
 
-  Widget _buildBody() {
+  Widget _buildBody(BuildContext context) {
     final config = sessionModel.configNotifier.value;
     return Column(
       children: <Widget>[
         ListItemFactory.settingsItem(
-          header: 'http_proxy'.i18n,
-          content: CText(config?.httpProxyAddr ?? '', style: tsBody1),
-          trailingArray: [SvgPicture.asset(ImagePaths.copy)],
-          onTap: () => copyHttpProxy(config?.httpProxyAddr ?? ''),
-        ),
+            header: 'http_proxy'.i18n,
+            content: CText(config?.httpProxyAddr ?? '', style: tsBody1),
+            trailingArray: [SvgPicture.asset(ImagePaths.copy)],
+            onTap: () => copyText(context, config?.httpProxyAddr ?? '')),
         ListItemFactory.settingsItem(
           header: 'socks_proxy'.i18n,
           content: CText(config?.socksProxyAddr ?? '', style: tsBody1),
-          onTap: () => copySocksProxy(config?.httpProxyAddr ?? ''),
+          onTap: () => copyText(context, config?.socksProxyAddr ?? ''),
           trailingArray: [SvgPicture.asset(ImagePaths.copy)],
         )
       ],
     );
-  }
-
-  void copyHttpProxy(String address) {
-    copyText(context, address);
-  }
-
-  void copySocksProxy(String address) {
-    copyText(context, address);
   }
 }
