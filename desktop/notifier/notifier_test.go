0b3c1c3afb1958a5380f7afcd63692c3853da2a4
Adam Fisk <myles@getlantern.org>
2024-10-18 11:53:17 -0600

    Use internalsdk analytics

--- desktop/notifier/notifier_test.go
+++ desktop/notifier/notifier_test.go
@@ -6,11 +6,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/getlantern/golog"
 	notify "github.com/getlantern/notifier"
 
-	"github.com/getlantern/flashlight/v7/common"
-	"github.com/getlantern/lantern-client/desktop/analytics"
+	"github.com/getlantern/lantern-client/internalsdk/analytics"
 )
 
 func TestNotify(t *testing.T) {
@@ -29,28 +27,4 @@ func TestNotify(t *testing.T) {
 
 	assert.True(t, shown)
 	stop()
-}
-
-func TestNormalizeClickURL(t *testing.T) {
-	log := golog.LoggerFor("lantern-desktop.notifier-test")
-	note := &notify.Notification{
-		Title:    "test",
-		Message:  "test",
-		ClickURL: "https://test.com",
-		IconURL:  "https://test.com",
-	}
-
-	err := normalizeClickURL(note, "test-campaign")
-	assert.NoError(t, err, "unexpected error")
-	log.Debugf("url: %v", note.ClickURL)
-	assert.Equal(t, "https://test.com?utm_campaign=test-campaign&utm_content=test-test&utm_medium=notification&utm_source="+common.Platform, note.ClickURL)
-
-	note.ClickURL = ":"
-	log.Debugf("url: %v", note.ClickURL)
-	err = normalizeClickURL(note, "test-campaign")
-	assert.Error(t, err, "expected an error")
-
-	stop := loopFor(10*time.Millisecond, analytics.NullSession{})
-	defer stop()
-	assert.False(t, ShowNotification(note, "test-campaign"))
 }
