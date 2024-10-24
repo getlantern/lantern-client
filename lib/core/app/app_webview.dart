fd0ff22823a609d49697d370111f12874cbc121a
atavism <paul@getlantern.org>
2024-10-23 18:32:27 -0700

    Windows CI updates

--- lib/core/app/app_webview.dart
+++ lib/core/app/app_webview.dart
@@ -56,14 +56,7 @@ class _AppWebViewState extends State<AppWebView> {
         initialSettings: InAppWebViewSettings(
           isInspectable: true,
           javaScriptEnabled: true,
-          supportZoom: true,
-          useWideViewPort: true,
-          loadWithOverviewMode: true,
-          builtInZoomControls: true,
-          displayZoomControls: false,
-          mediaPlaybackRequiresUserGesture: false,
-          allowsInlineMediaPlayback: true,
-          underPageBackgroundColor: Colors.white,
+          transparentBackground: true,
         ),
         onProgressChanged: (controller, progress) {
           appLogger.i("Loading progress: $progress%");
