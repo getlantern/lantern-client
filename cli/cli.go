0437c99294afc7b8135fd4434158a7a83d125a9f
atavism <paul@getlantern.org>
2024-12-10 08:35:16 -0800

    use flashlight flags

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"sync"
 	"syscall"
 	"time"
@@ -13,31 +14,24 @@ import (
 	"github.com/getlantern/flashlight/v7/client"
 	"github.com/getlantern/flashlight/v7/stats"
 	"github.com/getlantern/golog"
+	"github.com/getlantern/lantern-client/desktop/settings"
 	"github.com/getlantern/lantern-client/internalsdk/common"
 
-	"github.com/alexflint/go-arg"
 	"github.com/pterm/pterm"
 )
 
 var (
+	// lanternClient holds the reference to the running Lantern client instance
 	lanternClient *client.Client
 	mu            sync.Mutex
 	log           = golog.LoggerFor("lantern")
 )
 
-type args struct {
-	HttpProxyAddress  string `arg:"--http-proxy" help:"The HTTP proxy address to use" default:":0"`
-	SocksProxyAddress string `arg:"--socks-proxy" help:"The SOCKS proxy address to use" default:":0"`
-	StickyConfig      bool   `arg:"--sticky-config" help:"Whether to use sticky config" default:"false"`
-}
-
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	var args args
-	arg.MustParse(&args)
-
+	flags := flashlight.ParseFlags()
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -46,38 +40,38 @@ func main() {
 		cancel()
 	}()
 
-	startLantern(args)
+	// Start Lantern
+	startLantern(flags)
 	defer stopLantern()
 	<-ctx.Done()
 }
 
-func startLantern(args args) {
-	configDir := appdir.General(common.DefaultAppName)
-	pterm.Info.Println("Starting lantern: configDir", configDir)
+func startLantern(flags flashlight.Flags) {
+	cdir := configDir(&flags)
+	pterm.Info.Println("Starting lantern: configDir", cdir)
 
-	if args.HttpProxyAddress != "" {
-		pterm.Info.Println("Using http proxy address:", args.HttpProxyAddress)
-	}
-	if args.SocksProxyAddress != "" {
-		pterm.Info.Println("Using socks proxy address:", args.SocksProxyAddress)
-	}
-	if args.StickyConfig {
-		pterm.Info.Println("Using sticky config...")
+	// Log provided CLI arguments
+	httpProxyAddr := "127.0.0.1:0"
+	if flags.ForceProxyAddr != "" {
+		pterm.Info.Println("Using http proxy address:", flags.ForceProxyAddr)
+		httpProxyAddr = flags.ForceProxyAddr
 	}
 
-	userConfig := common.NewUserConfig("", "a34113", 3456344, "tok123", map[string]string{}, "")
+	settings := loadSettings(cdir)
+	userConfig := common.NewUserConfig(common.DefaultAppName, settings.GetDeviceID(), settings.GetUserID(),
+		settings.GetToken(), map[string]string{}, settings.GetLanguage())
 
 	runner, err := flashlight.New(
 		common.DefaultAppName,
 		common.ApplicationVersion,
 		common.RevisionDate,
-		configDir, // place to store lantern configuration
+		cdir, // place to store lantern configuration
 		false,
 		func() bool { return false }, // always connected
 		func() bool { return true },
 		func() bool { return false }, // do not proxy private hosts on Android
 		func() bool { return true },  // auto report
-		map[string]interface{}{},
+		flags.AsMap(),
 		userConfig,
 		stats.NewTracker(),
 		func() bool { return false },
@@ -88,10 +82,11 @@ func startLantern(args args) {
 	if err != nil {
 		log.Fatalf("failed to start flashlight: %v", err)
 	}
+	// Run Lantern in the background
 	go func() {
 		runner.Run(
-			args.HttpProxyAddress,  // listen for HTTP on provided address
-			args.SocksProxyAddress, // listen for SOCKS on random address
+			httpProxyAddr, // listen for HTTP on provided address
+			"127.0.0.1:0", // listen for SOCKS on random address
 			func(c *client.Client) {
 				mu.Lock()
 				lanternClient = c
@@ -110,6 +105,7 @@ func stopLantern() {
 	defer mu.Unlock()
 
 	if lanternClient == nil {
+		// Lantern is not running, no cleanup needed
 		return
 	}
 
@@ -122,4 +118,27 @@ func stopLantern() {
 	// small delay to give Lantern time to cleanup
 	time.Sleep(1 * time.Second)
 	pterm.Success.Println("Lantern stopped successfully.")
+}
+
+// loadSettings loads the initial settings at startup, either from disk or using defaults.
+func loadSettings(configDir string) *settings.Settings {
+	path := filepath.Join(configDir, "settings.yaml")
+	return settings.LoadSettingsFrom(common.ApplicationVersion, common.RevisionDate, common.BuildDate, path)
+}
+
+func configDir(flags *flashlight.Flags) string {
+	cdir := flags.ConfigDir
+	if cdir == "" {
+		cdir = appdir.General(common.DefaultAppName)
+	}
+	log.Debugf("Using config dir %v", cdir)
+	if _, err := os.Stat(cdir); err != nil {
+		if os.IsNotExist(err) {
+			// Create config dir
+			if err := os.MkdirAll(cdir, 0750); err != nil {
+				log.Errorf("Unable to create configdir at %s: %s", configDir, err)
+			}
+		}
+	}
+	return cdir
 }
