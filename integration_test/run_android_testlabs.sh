22fbba26f4dd35649ed020071bc552ebc473609d
Jigar-f <jigar@getlantern.org>
2024-12-31 15:28:10 +0530

    Update run_android_testlabs.sh

--- integration_test/run_android_testlabs.sh
+++ integration_test/run_android_testlabs.sh
@@ -7,11 +7,11 @@
 #If you want to find device model, version, using gcloud firebase test android models list
 #add --device model=MODEL_NAME,version=OS_VERSION_IDS,locale=en,orientation=portrait
 #   --device model=redfin,version=30,locale=en_US,orientation=portrait \
-#    --device model=panther,version=33,locale=en_US,orientation=portrait \
+#   --device model=panther,version=33,locale=en_US,orientation=portrait \
 gcloud firebase test android run \
     --type instrumentation \
-    --app build/app/outputs/apk/prod/debug/app-prod-debug.apk \
-    --test build/app/outputs/apk/androidTest/prod/debug/app-prod-debug-androidTest.apk \
+    --app build/app/outputs/apk/appiumTest/debug/app-appiumTest-debug.apk \
+    --test build/app/outputs/apk/androidTest/appiumTest/debug/app-appiumTest-debug-androidTest.apk \
     --device model=lynx,version=33,locale=en_US,orientation=portrait \
     --num-flaky-test-attempts=2 \
     --timeout 10m \
