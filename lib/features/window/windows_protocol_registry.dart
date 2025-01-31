0abbe618d869c5ce3260f6e67f599cb32c292ade
Jigar-f <jigar@getlantern.org>
2025-01-31 11:23:51 +0530

    Close registry key.

--- lib/features/window/windows_protocol_registry.dart
+++ lib/features/window/windows_protocol_registry.dart
@@ -1,16 +1,11 @@
-// Anything related to windows service will be here
-import 'dart:io';
-
 import 'package:lantern/core/utils/common.dart';
 import 'package:win32_registry/win32_registry.dart';
-
 
 class ProtocolRegistrar {
   ProtocolRegistrar._();
 
   /// The shared instance of [ProtocolRegistrar].
-  static final ProtocolRegistrar instance =
-  ProtocolRegistrar._();
+  static final ProtocolRegistrar instance = ProtocolRegistrar._();
 
   /// Registers the given [scheme] as a protocol handler.
   Future<void> register(String scheme) async {
@@ -34,9 +29,9 @@ class ProtocolRegistrar {
       final regKey = Registry.currentUser.createKey(protocolRegKey);
       regKey.createValue(protocolRegValue);
       regKey.createKey(protocolCmdRegKey).createValue(protocolCmdRegValue);
-    appLogger.i('Windows protocol registration for $scheme completed');
-    }
-    catch (e) {
+      appLogger.i('Windows protocol registration for $scheme completed');
+      regKey.close();
+    } catch (e) {
       appLogger.e("Error registering protocol: $e");
     }
   }
