4e27e431feee950bcb699574e351e6030b130237
atavism <paul@getlantern.org>
2025-02-11 12:28:57 -0800

    update app_links on macos

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,27 +1,10 @@
 import Cocoa
 import FlutterMacOS
-import app_links
 
 @main
 class AppDelegate: FlutterAppDelegate {
-
-  public override func application(
-    _ application: NSApplication,
-    continue userActivity: NSUserActivity,
-    restorationHandler: @escaping ([any NSUserActivityRestoring]) -> Void
-  ) -> Bool {
-
-    guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
-      return false
-    }
-
-    AppLinks.shared.handleLink(link: url.absoluteString)
-
-    return false
-  }
-
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return false
+    return true
   }
 
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
