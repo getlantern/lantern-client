f79b5ce24ce00c8c1c4102a3761dd1ebbc5507ac
atavism <paul@getlantern.org>
2025-02-11 17:37:36 -0800

    re-enable app_links

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: FlutterAppDelegate {
   }
 
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    return false
   }
 
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
