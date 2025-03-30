b1b8d0c7df919fc9aff0cfdb4fcb47188f25ad04
atavism <paul@getlantern.org>
2025-03-30 10:56:58 -0700

    Remove import

--- lib/core/utils/common_desktop.dart
+++ lib/core/utils/common_desktop.dart
@@ -1,7 +1,6 @@
 export 'dart:convert';
 export 'dart:ffi'; // For FFI
 export 'package:ffi/src/utf8.dart';
-export 'package:lantern/core/utils/config.dart';
 export 'package:lantern/core/native/model.dart';
 export 'package:lantern/core/service/websocket.dart';
 export 'package:lantern/core/service/websocket_subscriber.dart';
